c0016fb563008694bfebf56fb7ee2bc4233fd4bb
Aman Kubanychbek <aman.kubanychbek@timelysoft.net>
2016-10-25 17:39:38 +0600

    move main function

--- src/server/Startup.cs
+++ src/server/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
@@ -6,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Server.Models;
+// ReSharper disable All
 
 namespace Server
 {
@@ -14,9 +16,9 @@ namespace Server
         public Startup(IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
-                         .SetBasePath(env.ContentRootPath)
-                         .AddJsonFile("appsettings.json", true)
-                         .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true);
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true);
             if (env.IsDevelopment()) builder.AddUserSecrets();
             builder.AddEnvironmentVariables();
             Configuration = builder.Build();
@@ -39,7 +41,7 @@ namespace Server
                 app.UseDeveloperExceptionPage();
             else
                 app.UseExceptionHandler(new ExceptionHandlerOptions
-                    { ExceptionHandler = async ctx => await ctx.Response.WriteAsync("Oops!") });
+                {ExceptionHandler = async ctx => await ctx.Response.WriteAsync("Oops!")});
 
             app.UseFileServer();
             app.UseMvc(routeBuilder =>
@@ -47,6 +49,19 @@ namespace Server
                 routeBuilder.MapRoute("Default", "{controller=Home}/{action=Index}/{id?}");
             });
             app.Run(async ctx => await ctx.Response.WriteAsync("Not found!"));
+        }
+
+        public static void Main()
+        {
+            var cwd = Directory.GetCurrentDirectory();
+            new WebHostBuilder()
+                .UseContentRoot(cwd)
+                .UseWebRoot(Path.GetFileName(cwd) == "server" ? "../public" : "public")
+                .UseKestrel()
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build()
+                .Run();
         }
     }
 }
