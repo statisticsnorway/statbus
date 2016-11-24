4861c91ba5a1937249711f2fbeff0394ec58d044
Alexander Philippov <alexphil@gmail.com>
2016-11-24 12:10:32 +0600

    #253 Added UnitType. add method create statistical unit in controller and service

--- nscreg.Server/Controllers/StatisticalUnitController.cs
+++ nscreg.Server/Controllers/StatisticalUnitController.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 using nscreg.Data;
 using System.Linq;
 using nscreg.Server.Services;
+using nscreg.Server.Models.StatisticalUnit;
+using nscreg.Data.Entities;
+using nscreg.Utilities;
 
 namespace nscreg.Server.Controllers
 {
@@ -11,15 +14,37 @@ namespace nscreg.Server.Controllers
     {
         private readonly NSCRegDbContext _context;
 
+        public StatisticalUnitController(NSCRegDbContext context)
+        {
+            _context = context;
+        }
+
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
             var resp = StatisticalUnitServices.Delete(_context, id);
             if (resp.Equals("OK"))
             {
-                return (IActionResult) NoContent();
+                return (IActionResult)NoContent();
             }
             return BadRequest(new { message = "Error while deleting Statistical Unit" });
         }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] StatisticalUnitSubmitM data)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            try
+            {
+                new StatisticalUnitServices().Create(_context, data);
+                return Ok();
+            }
+            catch (StatisticalUnitCreateException e)
+            {
+                return BadRequest(new {e.Message});
+            }
+        }
+
     }
 }
--- nscreg.Server/Services/StatisticalUnitServices.cs
+++ nscreg.Server/Services/StatisticalUnitServices.cs
@@ -1,10 +1,25 @@
 ﻿using System.Linq;
 using nscreg.Data;
+using nscreg.Data.Entities;
+using nscreg.Server.Models.StatisticalUnit;
+using System.Collections.Generic;
+using System;
+using nscreg.Utilities;
 
 namespace nscreg.Server.Services
 {
     public class StatisticalUnitServices
     {
+        Dictionary<UnitType, Action<NSCRegDbContext, StatisticalUnitSubmitM>> _createActions;
+
+        public StatisticalUnitServices()
+        {
+            _createActions = new Dictionary<UnitType, Action<NSCRegDbContext, StatisticalUnitSubmitM>>();
+            _createActions.Add(UnitType.Legal, CreateLegalUnit);
+            _createActions.Add(UnitType.Local, CreateLocalUnit);
+            _createActions.Add(UnitType.Enterprise, CreateEnterpriseUnit);
+            _createActions.Add(UnitType.EnterpriseGroup, CreateEnterpriseGroupUnit);
+        }
         public static string Delete(NSCRegDbContext context, int id)
         {
             var localStatUnit = context.LocalUnits.FirstOrDefault(x => x.RegId == id);
@@ -35,5 +50,53 @@ namespace nscreg.Server.Services
 
             return "NotFound";
         }
+
+        public void Create(NSCRegDbContext context, StatisticalUnitSubmitM data)
+        {
+            try
+            {
+                _createActions[data.UnitType](context, data);
+            }
+            catch (Exception e)
+            {
+                throw new StatisticalUnitCreateException("Error while create Statistical Unit", e);
+            }
+
+
+        }
+
+        private void CreateLegalUnit(NSCRegDbContext context, StatisticalUnitSubmitM data)
+        {
+            var unit = new LegalUnit()
+            {
+                Name = data.Name
+            };
+            context.LegalUnits.Add(unit);
+        }
+        private void CreateLocalUnit(NSCRegDbContext context, StatisticalUnitSubmitM data)
+        {
+            var unit = new LocalUnit()
+            {
+                Name = data.Name
+            };
+            context.LocalUnits.Add(unit);
+        }
+        private void CreateEnterpriseUnit(NSCRegDbContext context, StatisticalUnitSubmitM data)
+        {
+            var unit = new EnterpriseUnit()
+            {
+                Name = data.Name
+            };
+            context.EnterpriseUnits.Add(unit);
+        }
+        private void CreateEnterpriseGroupUnit(NSCRegDbContext context, StatisticalUnitSubmitM data)
+        {
+            var unit = new EnterpriseGroup()
+            {
+                Name = data.Name
+            };
+            context.EnterpriseGroups.Add(unit);
+        }
+
     }
 }
