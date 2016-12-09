1559533ba4b02c6d19dd09b52b4201cd774a3b33
Alexander Philippov <alexphil@gmail.com>
2016-12-09 10:32:10 +0600

    updte dotnet core 1.0 to 1.1 update migration

--- nscreg.Data/Startup.cs
+++ nscreg.Data/Startup.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.IO;
