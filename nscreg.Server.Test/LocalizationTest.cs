49e7a41e8bad839c4b961ee9a02525956d8fa979
Alexander Philippov <alexphil@gmail.com>
2017-01-03 12:48:56 +0600

    #463 add unit tests

---
+++ nscreg.Server.Test/LocalizationTest.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace nscreg.Server.Test
+{
+    public class LocalizationTest
+    {
+        [Theory]
+        [InlineData("en-GB")]
+        [InlineData("ru-RU")]
+        [InlineData("ky-KG")]
+        private void HasLanguage(string key)
+        {
+            const string standart = "en-GB";
+            var dics = Localization.AllResources;
+            Assert.Contains(key, dics.Keys);
+            Assert.NotEmpty(dics[key]);
+            Assert.True(dics[key].Keys.Count > 0);
+            Assert.All(dics[standart].Keys, x => Assert.Contains(x, dics[key].Keys));
+            Assert.All(dics[key].Keys, x => Assert.Contains(x, dics[standart].Keys));
+            Assert.All(dics[key], x => Assert.False(string.IsNullOrEmpty(x.Value)));
+        }
+    }
+}
