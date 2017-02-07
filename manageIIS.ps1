72e6f2f57192ef51594cbe1f8de23abe40318ebe
Aman Kubanychbek <aman.kubanychbek@timelysoft.net>
2017-02-07 06:56:21 +0000

    PR 249: update manageIIS.ps1
     - update manageIIS.ps1
    
    Related work items: #1205

--- manageIIS.ps1
+++ manageIIS.ps1
@@ -1,16 +1,16 @@
 Param(
-  # iis website name
-  [String]$sitename,
-  # iis website command: stop | whatever
+  # iis web app pool name
+  [String]$poolname,
+  # iis command: stop | whatever
   [String]$command
 )
 
 Import-Module WebAdministration
 
 if ($command -eq "stop") {
-  Write-Warning "stopping website..."
-  Stop-WebSite $sitename
+  Write-Warning "stopping web app pool..."
+  Stop-WebAppPool $poolname
 } else {
-  Write-Warning "starting website..."
-  Start-WebSite $sitename
+  Write-Warning "starting web app pool..."
+  Start-WebAppPool $poolname
 }
