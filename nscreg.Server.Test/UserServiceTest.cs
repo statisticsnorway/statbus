987c6a6a236c82f45863fcc7079d99f2072c700d
fedotovsergey <wdr.jens@gmail.com>
2017-03-10 11:53:25 +0600

    Task 1524: Users Search, Sorting, Paging(UI + back) Unit tests

--- nscreg.Server.Test/UserServiceTest.cs
+++ nscreg.Server.Test/UserServiceTest.cs
@@ -35,6 +35,48 @@ namespace nscreg.Server.Test
             }
         }
 
+        [Theory]
+        [InlineData("me_1", false, null, 11)]
+        [InlineData("me_1", "Region 7", null, 1)]
+        [InlineData(null, "Region 7", null, 4)]
+        public async void GetFiltered(string username, string regionName, string roleId, int expectedRows)
+        {
+            using (var context = CreateContext())
+            {
+                var regionsService = new RegionsService(context);
+                foreach (var name in new[] { "Test Region 1", "Test Region 2", "Region 7" })
+                {
+                    await regionsService.AddAsync(new Region() {Name = name});
+                }
+                var regions2 = await regionsService.ListAsync(v => v.Name != regionName);
+                var targetRegions = await regionsService.ListAsync(v => v.Name == regionName);
+                var target = targetRegions.SingleOrDefault();
+
+                for (var i = 0; i <= 21; i++)
+                {
+                    context.Users.Add(new User
+                    {
+                        Name = "Name_" + i,
+                        Status = UserStatuses.Active,
+                        Region = i%7 == 0 ? target : regions2[i%2]
+                    });
+                    
+                }
+                await context.SaveChangesAsync();
+
+                var userList = new UserService(context).GetAllPaged(new UserListFilter()
+                {
+                    UserName = username,
+                    RegionId = target?.Id,
+                    RoleId = roleId,
+                    Page = 1,
+                    PageSize = 50,
+                });
+
+                Assert.Equal(expectedRows, userList.Result.Count()); //UserName_14
+            }
+        }
+
         [Fact]
         public void GetById()
         {
