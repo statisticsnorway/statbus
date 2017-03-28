4f716440b2333e07852f85238299980473c9dcc5
Aman Kubanychbek <aman.kubanychbek@timelysoft.net>
2017-03-28 15:10:04 +0600

    #1667 add auth to account view and edit

--- client/pages/Account/Routes.jsx
+++ client/pages/Account/Routes.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 
+import { systemFunction as sF } from 'helpers/checkPermissions'
+import ViewPage from './ViewPage'
 import Edit from './Edit'
 
 const Layout = props => <div>{props.children}</div>
 
 export default (
   <Route path="account" component={Layout}>
-    <IndexRoute component={Edit} />
+    <IndexRoute component={sF('AccountEdit') ? Edit : ViewPage} />
   </Route>
 )
