26c55b80c4b006941284dc5cb54626e580a73935
Aman Kubanychbek <aman.kubanychbek@timelysoft.net>
2017-07-20 12:04:57 +0000

    Merged PR 1200: #3027 add create link button in orglinks view
    #3027 add create link button in orglinks view
    
    Related work items: #3027

--- client/pages/StatUnits/Links/Create/CreateLink.jsx
+++ client/pages/StatUnits/Links/Create/CreateLink.jsx
@@ -1,22 +1,38 @@
 import React from 'react'
-import { func, array, bool } from 'prop-types'
+import { func, arrayOf, shape, string, bool } from 'prop-types'
 
 import { wrapper } from 'helpers/locale'
 import LinksGrid from '../components/LinksGrid'
 import LinksForm from '../components/LinkForm'
-
+import { defaultUnitSearchResult } from '../components/UnitSearch'
 
 class CreateLink extends React.Component {
   static propTypes = {
-    localize: func.isRequired,
+    links: arrayOf(shape({})).isRequired,
+    isLoading: bool.isRequired,
+    params: shape({
+      id: string,
+      type: string,
+    }),
     createLink: func.isRequired,
     deleteLink: func.isRequired,
-    links: array.isRequired,
-    isLoading: bool.isRequired,
+    localize: func.isRequired,
+  }
+
+  static defaultProps = {
+    params: undefined,
   }
 
   state = {
-    data: undefined,
+    data: {
+      source1: {
+        ...defaultUnitSearchResult,
+        id: this.props.params ? Number(this.props.params.id) : undefined,
+        type: this.props.params ? Number(this.props.params.type) : undefined,
+      },
+      source2: defaultUnitSearchResult,
+      comment: '',
+    },
   }
 
   onChange = (value) => {
--- client/pages/StatUnits/View/tabs/Links.jsx
+++ client/pages/StatUnits/View/tabs/Links.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import { func, shape, oneOfType, number, string } from 'prop-types'
+import { Link } from 'react-router'
+import { Button, Segment } from 'semantic-ui-react'
 
-import Tree from '../../Links/components/LinksTree'
+import { systemFunction as sF } from 'helpers/checkPermissions'
+import LinksTree from '../../Links/components/LinksTree'
 
 const Links = ({ filter, fetchData, localize }) => (
-  <Tree
-    filter={filter}
-    getUnitsTree={fetchData}
-    localize={localize}
-  />
+  <div>
+    <Segment>
+      <LinksTree
+        filter={filter}
+        getUnitsTree={fetchData}
+        localize={localize}
+      />
+    </Segment>
+    {sF('LinksCreate') && <Button
+      as={Link}
+      to={`/statunits/links/create?id=${filter.id}&type=${filter.type}`}
+      content={localize('LinksViewAddLinkBtn')}
+      positive
+    />}
+  </div>
 )
 
 Links.propTypes = {
