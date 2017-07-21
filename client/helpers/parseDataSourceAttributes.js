9d77e832d9eec0acf48d35e6ff4ce6f2aa0bb591
Aman Kubanychbek <aman.kubanychbek@timelysoft.net>
2017-07-21 17:17:22 +0600

    #1927 update parser to parse single entity xml

--- client/helpers/parseDataSourceAttributes.js
+++ client/helpers/parseDataSourceAttributes.js
@@ -1,17 +1,20 @@
 import { parse as parseXml } from 'fast-xml-parser'
 import { parse as parseCsv } from 'papaparse'
-import { pipe, union, keys } from 'ramda'
+import { pipe, union, keys, values } from 'ramda'
 
-const polishParsedCsv = result => result.data
+const isObject = data => data && typeof (data) === 'object'
+const hasNested = data => values(data).some(isObject)
+const unionBy = asFn => (prev, curr) => union(prev, asFn(curr))
 
 const getAttributes = data => Array.isArray(data)
-  ? data.reduce((prev, curr) => union(prev, keys(curr)), [])
-  : data && typeof (data) === 'object'
-    ? Object.entries(data).reduce(
-      (acc, [, value]) => [...acc, ...getAttributes(value)],
-      [],
-    )
+  ? data.reduce(unionBy(keys), [])
+  : isObject(data)
+    ? hasNested(data)
+      ? values(data).reduce(unionBy(getAttributes), [])
+      : keys(data)
     : []
+
+const polishParsedCsv = result => result.data
 
 export const parseXML = pipe(parseXml, getAttributes)
 export const parseCSV = pipe(parseCsv, polishParsedCsv, getAttributes)
