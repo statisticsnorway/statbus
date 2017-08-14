7943e252ad9796aaa7b1cd304dd28e0aa4760d1c
bktest <bktest@mail.ru>
2017-08-14 14:07:56 +0600

    Configuration File for Data project

--- nscreg.Data/Startup.cs
+++ nscreg.Data/Startup.cs
@@ -3,6 +3,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.IO;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Options;
+using nscreg.ConfigurationSettings.CommonSettings;
+
 // ReSharper disable UnusedMember.Global
 
 namespace nscreg.Data
@@ -10,21 +13,27 @@ namespace nscreg.Data
     // ReSharper disable once ClassNeverInstantiated.Global
     public class Startup
     {
+        private IConfiguration Configuration { get; }
+
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("../appsettings.json", true, true)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddUserSecrets<Startup>();
+
+            Configuration = builder.Build();
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
-            var config = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("../appsettings.json", true)
-                .AddJsonFile("appsettings.json", true)
-                .AddUserSecrets<Startup>()
-                .Build();
-
-            //services.AddOptions();
-            //services.Configure<ConnectionStrings>(cs => Configuration.GetSection(nameof(CommonSettings)).Bind(cs));
-            //services.AddScoped(cfg => cfg.GetService<IOptions<CommonSettings>>().Value);
+            services.AddOptions();
+            services.Configure<ConnectionStrings>(cs => Configuration.GetSection(nameof(ConnectionStrings)).Bind(cs));
+            services.AddScoped(cfg => cfg.GetService<IOptions<ConnectionStrings>>().Value);
 
             services.AddDbContext<NSCRegDbContext>(op =>
-                op.UseNpgsql(config.GetConnectionString("DefaultConnection")));
+                op.UseNpgsql(Configuration.GetConnectionString(nameof(ConnectionStrings.DefaultConnection))));
         }
 
         public static void Main()
