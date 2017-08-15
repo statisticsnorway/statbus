f54864012f493f15b64a5b2c2e39ae51c0bf51e6
bktest <bktest@mail.ru>
2017-08-15 17:05:44 +0600

    CreateStatUnitFixes

--- nscreg.Data/Configuration/AnalysisErrorConfiguration.cs
+++ nscreg.Data/Configuration/AnalysisErrorConfiguration.cs
@@ -8,9 +8,9 @@ namespace nscreg.Data.Configuration
     {
         public override void Configure(EntityTypeBuilder<AnalysisError> builder)
         {
-            //builder.HasKey(x => x.Id);
-            //builder.Property(x => x.AnalysisLogId).IsRequired();
-            //builder.HasOne(x => x.AnalysisLog).WithMany(x => x.AnalysisErrors).HasForeignKey(x => x.AnalysisLogId);
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.AnalysisLogId).IsRequired();
+            builder.HasOne(x => x.AnalysisLog).WithMany(x => x.AnalysisErrors).HasForeignKey(x => x.AnalysisLogId);
         }
     }
 }
