a14727ed6f6d039c91fd8d57e66d0c43fb227958
bktest <bktest@mail.ru>
2017-09-19 11:44:25 +0600

    Fix sample frames location

--- nscreg.Server.Common/Services/SampleFrameService.cs
+++ nscreg.Server.Common/Services/SampleFrameService.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using AutoMapper;
+using nscreg.Business.SampleFrame;
 using nscreg.Data;
 using nscreg.Data.Entities;
 using nscreg.Server.Common.Models.SampleFrames;
 using nscreg.Server.Common.Services.Contracts;
 using nscreg.Utilities.Models.SampleFrame;
+using Newtonsoft.Json;
 
 namespace nscreg.Server.Common.Services
 {
@@ -14,11 +18,13 @@ namespace nscreg.Server.Common.Services
     /// </summary>
     public class SampleFrameService : ISampleFrameService
     {
-        private readonly nscreg.Services.SampleFrames.SampleFrameService _service;
+        private readonly NSCRegDbContext _context;
+        private readonly ExpressionParser _expressionParser;
 
         public SampleFrameService(NSCRegDbContext context)
         {
-            _service = new nscreg.Services.SampleFrames.SampleFrameService(context);
+            _context = context;
+            _expressionParser = new ExpressionParser();
         }
 
         /// <summary>
@@ -31,7 +37,9 @@ namespace nscreg.Server.Common.Services
         {
             var sampleFrame = new SampleFrame();
             Mapper.Map(data, sampleFrame);
-            await _service.CreateAsync(expressionTree, sampleFrame);
+            sampleFrame.Predicate = JsonConvert.SerializeObject(expressionTree);
+            await _context.SampleFrames.AddAsync(sampleFrame);
+            await _context.SaveChangesAsync();
         }
 
         /// <summary>
@@ -44,7 +52,13 @@ namespace nscreg.Server.Common.Services
         {
             var sampleFrame = new SampleFrame();
             Mapper.Map(data, sampleFrame);
-            await _service.EditAsync(expressionTree, sampleFrame);
+            var existingSampleFrame = _context.SampleFrames.FirstOrDefault(sf => sf.Id == sampleFrame.Id);
+            existingSampleFrame.Name = sampleFrame.Name;
+            existingSampleFrame.Predicate = JsonConvert.SerializeObject(expressionTree);
+            existingSampleFrame.Fields = sampleFrame.Fields;
+            existingSampleFrame.UserId = sampleFrame.UserId;
+
+            await _context.SaveChangesAsync();
         }
 
         /// <summary>
@@ -53,7 +67,10 @@ namespace nscreg.Server.Common.Services
         /// <param name="id"></param>
         public async Task DeleteAsync(int id)
         {
-            await _service.DeleteAsync(id);
+            var existingSampleFrame = _context.SampleFrames.FirstOrDefault(sf => sf.Id == id);
+            _context.SampleFrames.Remove(existingSampleFrame);
+
+            await _context.SaveChangesAsync();
         }
 
         /// <summary>
@@ -63,7 +80,37 @@ namespace nscreg.Server.Common.Services
         /// <returns></returns>
         public Dictionary<string, string[]> View(int id)
         {
-            return _service.View(id);
+            var sampleFrame = _context.SampleFrames.FirstOrDefault(x => x.Id == id);
+            if (sampleFrame == null) return null;
+
+            var sfExpression = JsonConvert.DeserializeObject<SFExpression>(sampleFrame.Predicate);
+            var predicate = _expressionParser.Parse(sfExpression);
+
+            var statUnits = _context.StatisticalUnits.Where(predicate).ToList();
+
+            var result = new Dictionary<string, string[]>();
+            var fields = sampleFrame.Fields.Split(';');
+            foreach (var unit in statUnits)
+            {
+                foreach (var field in fields)
+                {
+                    if (result.Any(x => x.Key == field))
+                    {
+                        var existed = result[field];
+                        result[field] = existed.Concat(GetPropValue(unit, field)).ToArray();
+                    }
+                    else
+                    {
+                        result.Add(field, GetPropValue(unit, field));
+                    }
+                }
+            }
+            return result;
+        }
+
+        private static string[] GetPropValue(StatisticalUnit src, string propName)
+        {
+            return new[] { src.GetType().GetProperty(propName).GetValue(src, null).ToString() };
         }
     }
 }
