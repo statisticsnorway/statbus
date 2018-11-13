1f1d4948b4732df3bd1607605de43d3a2e6cc917
s.sergeev <s.sergeev@no.no>
2018-11-13 17:26:30 +0600

    #14883 Sample frame create form => Predicates => Values
    - объединил сообщения об ошибках, теперь они показываются как список
    - написал фильтр для ошибок из "predicates"

--- client/pages/SampleFrames/FormBody.jsx
+++ client/pages/SampleFrames/FormBody.jsx
@@ -5,7 +5,7 @@ import { Grid, Message, Tab, Form } from 'semantic-ui-react'
 import { formBody as bodyPropTypes } from 'components/createSchemaFormHoc/propTypes'
 import { TextField as PlainTextField, withDebounce } from 'components/fields'
 import handlerFor from 'helpers/handleSetFieldValue'
-import { hasValue } from 'helpers/validation'
+import { hasValue, filterPredicateErrors } from 'helpers/validation'
 import PredicateEditor from './PredicateEditor'
 import FieldsEditor from './FieldsEditor'
 import { predicate as predicatePropTypes } from './propTypes'
@@ -36,6 +36,8 @@ const FormBody = ({
   })
   const predicateProps = propsFor('predicate')
   const fieldsProps = propsFor('fields')
+  const filteredPredicateErrors =
+    hasValue(predicateProps.errors) && filterPredicateErrors(predicateProps.errors[0].clauses)
   const renderPredicateEditor = () => (
     <PredicateEditor
       value={values.predicate}
@@ -79,14 +81,19 @@ const FormBody = ({
             { menuItem: localize('Fields'), render: renderFieldsEditor },
           ]}
         />
-        {hasValue(predicateProps.errors) && (
+
+        {(hasValue(predicateProps.errors) || hasValue(fieldsProps.errors)) && (
           <Message error>
-            {predicateProps.errors[0].clauses[0].value
-              ? localize(predicateProps.errors[0].clauses[0].value)
-              : null}
+            <Message.List>
+              {hasValue(predicateProps.errors) && (
+                <Message.Item content={filteredPredicateErrors.map(localize)} />
+              )}
+              {hasValue(fieldsProps.errors) && (
+                <Message.Item content={fieldsProps.errors.map(localize)} />
+              )}
+            </Message.List>
           </Message>
         )}
-        {hasValue(fieldsProps.errors) && <Message list={fieldsProps.errors.map(localize)} error />}
       </Grid>
     </div>
   )
