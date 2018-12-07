2384d8b7245123df4d99b72715387f4d31f7f3fc
k.almasbek <almasbek.kairat@timelysoft.net>
2018-12-07 16:14:10 +0600

    removed deprecated plugins in webpack_4
    Related work items: #15646

--- tools/webpack.config.js
+++ tools/webpack.config.js
@@ -40,10 +40,6 @@ const config = {
   },
   plugins: [
     new Webpack.optimize.OccurrenceOrderPlugin(),
-    // new Webpack.DefinePlugin({
-    //   'process.env.NODE_ENV': isDebug ? '"development"' : '"production"',
-    //   __DEV__: isDebug,
-    // }),
     new AssetsWebpackPlugin({
       path: Path.resolve(__dirname, '../src/nscreg.Server/wwwroot/dist'),
       filename: 'assets.json',
@@ -94,12 +90,7 @@ const config = {
 }
 
 if (!isDebug) {
-  config.plugins = [
-    ...config.plugins,
-    // new Webpack.optimize.ModuleConcatenationPlugin(),
-    // new Webpack.optimize.UglifyJsPlugin({ compress: { warnings: isVerbose } }),
-    new Webpack.optimize.AggressiveMergingPlugin(),
-  ]
+  config.plugins = [...config.plugins, new Webpack.optimize.AggressiveMergingPlugin()]
   config.mode = 'production'
 }
 
@@ -111,11 +102,7 @@ if (isDebug && useHMR) {
     'eventsource-polyfill',
     ...config.entry,
   ]
-  config.plugins = [
-    ...config.plugins,
-    new Webpack.HotModuleReplacementPlugin(),
-    // new Webpack.NoEmitOnErrorsPlugin(),
-  ]
+  config.plugins = [...config.plugins, new Webpack.HotModuleReplacementPlugin()]
   config.mode = 'development'
 }
 
