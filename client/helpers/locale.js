8dde6d22f344195082ba56ce65e7b60055b82848
s.sergeev <s.sergeev@no.no>
2018-12-26 14:02:03 +0600

    #15812 Client - catalogues
    fixed local function

--- client/helpers/locale.js
+++ client/helpers/locale.js
@@ -45,7 +45,7 @@ export const getNewName = (item, isUsersPage) => {
   let newName = ''
 
   if (defaultLocale === locale) {
-    newName = item.name
+    newName = item.name ? item.name : item.fullPath
   }
   if (language1 === locale) {
     newName = item.nameLanguage1
