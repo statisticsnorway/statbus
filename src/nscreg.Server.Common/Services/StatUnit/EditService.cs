7b7546da94a7bc20edc62ca03ea5c4986f24fc86
Евгений Дербичев <evgeny.derbichev@timelysoft.net>
2019-04-24 12:28:43 +0600

    #18284

--- src/nscreg.Server.Common/Services/StatUnit/EditService.cs
+++ src/nscreg.Server.Common/Services/StatUnit/EditService.cs
@@ -70,7 +70,7 @@ namespace nscreg.Server.Common.Services.StatUnit
                 {
                     if (Common.HasAccess<LegalUnit>(data.DataAccess, v => v.LocalUnits))
                     {
-                        var localUnits = _dbContext.LocalUnits.Where(x => data.LocalUnits.Contains(x.RegId));
+                        var localUnits = _dbContext.LocalUnits.Where(x => data.LocalUnits.Contains(x.RegId) && x.UnitStatusId != 7);
                         
                         unit.LocalUnits.Clear();
                         unit.HistoryLocalUnitIds = null;
