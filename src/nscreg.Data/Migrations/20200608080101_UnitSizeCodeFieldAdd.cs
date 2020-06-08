dd8e6f6de2165815a7e51e9b939e8f77d16f5908
l3itkg <l3iteg1997@gmail.com>
2020-06-08 17:17:50 +0600

    Innitialization

---
+++ src/nscreg.Data/Migrations/20200608080101_UnitSizeCodeFieldAdd.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace nscreg.Data.Migrations
+{
+    public partial class UnitSizeCodeFieldAdd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Code",
+                table: "UnitsSize",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.Sql(@"UPDATE dbo.UnitsSize SET Code = Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "UnitsSize");
+        }
+    }
+}
