d7134d896255f974ef5020dc32ee49edab73ae2a
Ilya Gorbunov <0551216050boss@gmail.com>
2020-07-24 13:19:05 +0600

    fixes

--- client/helpers/createStatUnitSchema.js
+++ client/helpers/createStatUnitSchema.js
@@ -156,7 +156,7 @@ const byType = {
     privCapitalShare: sureString,
     foreignCapitalShare: sureString,
     foreignCapitalCurrency: sureString,
-    entGroupRole: sureString,
+    entGroupRoleId: positiveNum,
     legalUnits: positiveNumArray,
     entGroupId: positiveNum,
     commercial: bool(),
