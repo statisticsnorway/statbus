6c930463f59531831991364680ecb6644ef60377
Ilya Gorbunov <0551216050boss@gmail.com>
2020-09-10 15:27:22 +0600

    Populate Service

--- src/nscreg.Server.Common/PopulateService.cs
+++ src/nscreg.Server.Common/PopulateService.cs
@@ -7,69 +7,151 @@ using nscreg.Business.DataSources;
 using nscreg.Data;
 using nscreg.Data.Constants;
 using nscreg.Data.Entities;
+using nscreg.Server.Common.Services.DataSources;
 using nscreg.Utilities.Extensions;
 
 namespace nscreg.Server.Common
-{ 
+{
+    /// <summary>
+    /// Service for populating unit
+    /// </summary>
     public class PopulateService
     {
-        private readonly Dictionary<string, string[]> _mappings;
+        //private readonly Dictionary<string, string[]> _mappings;
+        private readonly (string source, string target)[] _propMappings;
         private readonly DataSourceAllowedOperation _allowedOperation;
+        private readonly string _personRoleSource;
         private readonly DataSourceUploadTypes _uploadType;
         private readonly string _statIdSourceKey;
         private readonly StatUnitTypes _unitType;
         private readonly NSCRegDbContext _context;
         public PopulateService((string source, string target)[] propMapping, DataSourceAllowedOperation operation, DataSourceUploadTypes uploadType, StatUnitTypes unitType, NSCRegDbContext context)
         {
+            _personRoleSource = propMapping.FirstOrDefault(c => c.target == "Persons.Person.Role").source;
+            _propMappings = propMapping;
             _statIdSourceKey = StatUnitKeyValueParser.GetStatIdSourceKey(propMapping) ?? throw new ArgumentNullException(nameof(propMapping), "StatId doesn't have source field(header)");
             _context = context;
             _unitType = unitType;
             _allowedOperation = operation;
             _uploadType = uploadType;
-            _mappings = propMapping
-                .GroupBy(x => x.source)
-                .ToDictionary(x => x.Key, x => x.Select(y => y.target).ToArray());
+            //_mappings = propMapping
+            //    .GroupBy(x => x.source)
+            //    .ToDictionary(x => x.Key, x => x.Select(y => y.target).ToArray());
         }
 
-        public async Task<(StatisticalUnit, string)> PopulateAsync(IReadOnlyDictionary<string, object> raw)
+        /// <summary>
+        /// Method for populate unit
+        /// </summary>
+        /// <param name="raw">Parsed  data of a unit</param>
+        /// <returns></returns>
+        public async Task<(StatisticalUnit unit, bool isNew, string errors)> PopulateAsync(IReadOnlyDictionary<string, object> raw)
         {
-            var resultUnit = await GetStatUnitBase(_allowedOperation, raw);
+            var (resultUnit, isNew) = await GetStatUnitBase(_allowedOperation, raw);
 
-            raw = await TransformReferenceField(raw, _mappings, "Persons.Person.Role", (value) =>
+            raw = await TransformReferenceField(raw, "Persons.Person.Role", (value) =>
             {
                 return _context.PersonTypes.FirstOrDefaultAsync(x =>
                     x.Name == value || x.NameLanguage1 == value || x.NameLanguage2 == value);
             });
+            //ParseAndMutateStatUnit(_mappings, raw, resultUnit);
 
-            ParseAndMutateStatUnit(_mappings, raw, resultUnit);
+            //var errors = await _postProcessor.FillIncompleteDataOfStatUnit(resultUnit, _uploadType);
 
-            var errors = await _postProcessor.FillIncompleteDataOfStatUnit(resultUnit, _uploadType);
+            return (resultUnit, isNew, "!@3");
 
-            return (resultUnit, errors);
 
-            
         }
-
-        private async Task<StatisticalUnit> GetStatUnitBase(DataSourceAllowedOperation operation, IReadOnlyDictionary<string, object> raw)
+        /// <summary>
+        /// Returns existed or new (depending on <paramref name="operation"/>) stat unit
+        /// </summary>
+        /// <param name="operation">Data source operation(enum)</param>
+        /// <param name="raw">Parsed data of a unit</param>
+        /// <returns></returns>
+        private async Task<(StatisticalUnit unit, bool isNew)> GetStatUnitBase(DataSourceAllowedOperation operation, IReadOnlyDictionary<string, object> raw)
         {
-            StatisticalUnit existing = null;
             if (_statIdSourceKey.HasValue() &&
-            operation != DataSourceAllowedOperation.Create && raw.TryGetValue(_statIdSourceKey, out var statId))
+                operation != DataSourceAllowedOperation.Create && raw.TryGetValue(_statIdSourceKey, out var statId))
             {
-                existing = await _getStatUnitSet[_unitType]
+                var existing = await GetStatUnitSetHelper
+                    .GetStatUnitSet(_context, _unitType)
                     .FirstOrDefaultAsync(x => x.StatId == statId.ToString());
+                if (existing != null)
+                {
+                    _context.Entry(existing).State = EntityState.Detached;
+                    return (unit: existing, isNew: false);
+                }
             }
-
-            else if (_uploadType == DataSourceUploadTypes.Activities)
-                throw new InvalidOperationException("Missing statId required for activity upload");
-
-
-            if (existing == null) return CreateByType[_unitType]();
-
-            _context.Entry(existing).State = EntityState.Detached;
-            return existing;
+            return (GetStatUnitSetHelper.CreateByType(_unitType),true);
         }
 
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <param name="raw"></param>
+        /// <param name="referenceField"></param>
+        /// <param name="getEntityAction"></param>
+        /// <returns></returns>
+        private async Task<IReadOnlyDictionary<string, object>> TransformReferenceField<TEntity>(IReadOnlyDictionary<string, object> raw, string referenceField, Func<string, Task<TEntity>> getEntityAction)
+     where TEntity : LookupBase
+        {
+            ///todo Разобраться с нужностью этого метода.
+            Dictionary<string, object> result = new Dictionary<string, object>();
+            if (string.IsNullOrEmpty(_personRoleSource))
+            {
+                return raw;
+            }
+            var parts = referenceField.Split('.');
 
+            if (!raw.TryGetValue(_personRoleSource, out var propValue) ||
+                !(raw[_personRoleSource] is IList<KeyValuePair<string, Dictionary<string, string>>> parents))
+                return raw;
+
+            List<int> idsArray = new List<int>();
+            List<string> errorArray = new List<string>();
+            foreach (var par in parents)
+            {
+                var value = par.Value[parts.Last()];
+                var entity = await getEntityAction(value);
+                if (entity != null)
+                {
+                    idsArray.Add(entity.Id);
+                }
+                else
+                {
+                    errorArray.Add(value);
+                }
+            }
+            if (errorArray.Any()) throw new Exception($"Reference for {string.Join(",", errorArray)} was not found");
+            foreach (var keyValuePair in raw)
+            {
+                if (keyValuePair.Value is string)
+                {
+                    result[keyValuePair.Key] = keyValuePair.Value;
+                }
+                else
+                {
+                    var val = keyValuePair.Value as IList<KeyValuePair<string, Dictionary<string, string>>>;
+                    for (int i = 0; i < val.Count; i++)
+                    {
+                        var elem = new List<KeyValuePair<string, Dictionary<string, string>>>();
+                        if(keyValuePair.Value is IList<KeyValuePair<string, Dictionary<string, string>>> arrayKeyValuePair)
+                            foreach (var kv in arrayKeyValuePair)
+                            {
+                                var dic = new Dictionary<string, string>();
+                                foreach (var kvValue in kv.Value)
+                                {
+                                    dic.Add(kvValue.Key, kvValue.Key == parts.Last() ? idsArray[i].ToString() : kvValue.Value);
+                                }
+                                elem.Add(new KeyValuePair<string, Dictionary<string, string>>(kv.Key, dic));
+                            }
+
+                        result[keyValuePair.Key] = elem;
+                    }
+                }
+            }
+            return result;
+        }
     }
 }
--- src/nscreg.Server.Common/Services/DataSources/GetStatUnitSetHelper.cs
+++ src/nscreg.Server.Common/Services/DataSources/GetStatUnitSetHelper.cs
@@ -1,7 +1,7 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using Microsoft.EntityFrameworkCore;
+using nscreg.Data;
 using nscreg.Data.Constants;
 using nscreg.Data.Entities;
 
@@ -9,9 +9,64 @@ namespace nscreg.Server.Common.Services.DataSources
 {
     public static class GetStatUnitSetHelper
     {
-        public static Dictionary<StatUnitTypes, IQueryable<StatisticalUnit>> GetStatUnitSet()
+        public static IQueryable<StatisticalUnit> GetStatUnitSet(NSCRegDbContext context, StatUnitTypes type)
         {
+            switch (type)
+            {
+                case StatUnitTypes.LocalUnit:
+                    return context.LocalUnits
+                        .Include(x => x.Address)
+                        .ThenInclude(x => x.Region)
+                        .Include(x => x.PersonsUnits)
+                        .ThenInclude(x => x.Person)
+                        .Include(x => x.ActivitiesUnits)
+                        .ThenInclude(x => x.Activity)
+                        .Include(x => x.ForeignParticipationCountriesUnits)
+                        .ThenInclude(x => x.Country)
+                        .AsNoTracking();
 
+                case StatUnitTypes.LegalUnit:
+                    return context.EnterpriseUnits
+                        .Include(x => x.Address)
+                        .ThenInclude(x => x.Region)
+                        .Include(x => x.PersonsUnits)
+                        .ThenInclude(x => x.Person)
+                        .Include(x => x.ActivitiesUnits)
+                        .ThenInclude(x => x.Activity)
+                        .Include(x => x.ForeignParticipationCountriesUnits)
+                        .ThenInclude(x => x.Country)
+                        .AsNoTracking();
+
+                case StatUnitTypes.EnterpriseUnit:
+                    return context.EnterpriseUnits
+                        .Include(x => x.Address)
+                        .ThenInclude(x => x.Region)
+                        .Include(x => x.PersonsUnits)
+                        .ThenInclude(x => x.Person)
+                        .Include(x => x.ActivitiesUnits)
+                        .ThenInclude(x => x.Activity)
+                        .Include(x => x.ForeignParticipationCountriesUnits)
+                        .ThenInclude(x => x.Country)
+                        .AsNoTracking();
+                default:
+                    throw new InvalidOperationException("Unit type is not supported");
+
+            }
+        }
+
+        public static StatisticalUnit CreateByType(StatUnitTypes types)
+        {
+            switch (types)
+            {
+                case StatUnitTypes.LegalUnit:
+                    return  new LegalUnit();
+                case StatUnitTypes.EnterpriseUnit:
+                    return new EnterpriseUnit();
+                case StatUnitTypes.LocalUnit:
+                    return  new LocalUnit();
+                default:
+                    throw new InvalidOperationException("Unit type is not supported");
+            }
         }
     }
 }
