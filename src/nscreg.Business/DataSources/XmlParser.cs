f9f2e045692a90edc756cc4160904ccd22c81141
Ilya Gorbunov <0551216050boss@gmail.com>
2020-09-15 17:58:37 +0600

    XmlParser

--- src/nscreg.Business/DataSources/XmlParser.cs
+++ src/nscreg.Business/DataSources/XmlParser.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Linq;
 using nscreg.Data.Constants;
+using nscreg.Data.Entities;
+using nscreg.Utilities.Extensions;
 
 namespace nscreg.Business.DataSources
 {
     public static class XmlParser
     {
+        private readonly static string[] StatisticalUnitArrayPropertyNames = new[] { nameof(StatisticalUnit.Activities), nameof(StatisticalUnit.Persons), nameof(StatisticalUnit.ForeignParticipationCountriesUnits) };
         public static IEnumerable<XElement> GetRawEntities(XContainer doc)
         {
             var typeNames = Enum.GetNames(typeof(StatUnitTypes));
@@ -25,44 +28,61 @@ namespace nscreg.Business.DataSources
         public static IReadOnlyDictionary<string, object> ParseRawEntity(XElement el, (string source, string target)[] mappings)
         {
             var result = new Dictionary<string, object>();
-
-                foreach (var descendant in el.Elements())
+            var regularToNestedDictionary = new Dictionary<string, string>();
+            foreach (var descendant in el.Elements())
+            {
+                var mappingsByDescendantName = mappings.Where(x => x.source == descendant.Name.LocalName);
+                if (!descendant.HasElements)
                 {
-                    var targetMappingName = mappings.FirstOrDefault(x => x.source.Contains(descendant.Name.LocalName)).target;
-                    if (!descendant.HasElements)
+                    mappingsByDescendantName.ForEach(x =>
                     {
-                        result.Add(targetMappingName.Split('.', 3).Last(), descendant.Value);
-                        continue;
-                    }
+                        if(!StatisticalUnitArrayPropertyNames.Contains(x.target.Split(".",3)[0]))
+                            result.Add(x.target, descendant.Value);
+                        regularToNestedDictionary.Add(descendant.Name.LocalName, descendant.Value);
+                    });
+                    continue;
+                }
 
-                    var descendantElements = new List<KeyValuePair<string, object>>();
-
-                    foreach (var innerDescendant in descendant.Elements())
+                var descendantElements = new List<KeyValuePair<string, object>>();
+                string[] splittedTarget = null;
+                foreach (var innerDescendant in descendant.Elements())
+                {
+                    var keyValueDictionary = new Dictionary<string, string>();
+                  
+                    foreach (var innerInnerDescendant in innerDescendant.Elements())
                     {
+                        var fullPath = string.Join('.', descendant.Name.LocalName, innerDescendant.Name.LocalName,
+                            innerInnerDescendant.Name.LocalName);
 
-                        var array = new Dictionary<string, string>();
-
-                        foreach (var innerInnerDescendant in innerDescendant.Elements())
+                        foreach (var (source, target) in mappings.Where(x => x.source == fullPath || x.target == fullPath))
                         {
-                            var fullPath = string.Join('.', descendant.Name.LocalName, innerDescendant.Name.LocalName,
-                                innerInnerDescendant.Name.LocalName);
-                            foreach (var (source, target) in mappings)
+                            ////TODO Маппинг из обычного значения XML во вложенное
+                            splittedTarget = target.Split('.', 3);
+
+                            if (regularToNestedDictionary.TryGetValue(source, out var value) && StatisticalUnitArrayPropertyNames.Contains(target.Split(".", 3)[0]))
                             {
-                                if (fullPath == source)
-                                {
-                                    array[target.Split('.', 3).Last()] = innerInnerDescendant.Value;
-                                }
+                                keyValueDictionary[splittedTarget.Last()] = value;
+                            }
+                            else if (splittedTarget.Length > 2)
+                            {
+                                keyValueDictionary[splittedTarget.Last()] = innerInnerDescendant.Value;
                             }
                         }
-
-                        var sourceMappingName = mappings
-                            .FirstOrDefault(x => x.source.Contains(string.Join('.', descendant.Name.LocalName, innerDescendant.Name.LocalName))).target;
-
-                            descendantElements.Add(new KeyValuePair<string, object>(sourceMappingName.Split('.')[1], array));
                     }
-                    result.Add(targetMappingName.Split('.')[0], descendantElements);
+
+                    if (!keyValueDictionary.Any())
+                        continue;
+
+                    if(splittedTarget != null  && splittedTarget.Length > 2)
+                    {
+                        descendantElements.Add(new KeyValuePair<string, object>(splittedTarget[1], keyValueDictionary));
+                    }
                 }
-            
+                if (splittedTarget != null && splittedTarget.Any())
+                {
+                    result.Add(splittedTarget[0], descendantElements);
+                }
+            }
             return result;
         }
             
