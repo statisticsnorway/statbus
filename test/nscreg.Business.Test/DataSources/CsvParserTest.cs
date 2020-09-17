51a3ecf27bd64b373790c8f1bed7f7df2474b56a
r.mustafin <ruslan.mustafin@timelysoft.net>
2020-09-17 17:31:37 +0600

    New test for PopulateService. Some cleaning

--- test/nscreg.Business.Test/DataSources/CsvParserTest.cs
+++ test/nscreg.Business.Test/DataSources/CsvParserTest.cs
@@ -9,27 +9,6 @@ namespace nscreg.Business.Test.DataSources
 {
     public class CsvParserTest
     {
-        //[Theory]
-        //[InlineData(
-        //    "StatId;StatUnitName;parent_unit;organistaional_form;RegisterDate;dateOfCommencement;dateChangeOfOwner\n"
-        //    + "919228741;ALEX TRANSPORT;919018828;BEDR;2017-07-03;2017-06-04;\n"
-        //    + "919228768;UNISOL UTEGLASS AS;919146605;BEDR;;;\n"
-        //    + "919228776;PETBNB AS;919061227;BEDR;2017-07-03;2017-05-15;2017-05-25",
-        //    ";")]
-        //[InlineData(
-        //    "\"StatId\";\"StatUnitName\";\"parent_unit\";\"organistaional_form\";\"RegisterDate\";\"dateOfCommencement\";\"dateChangeOfOwner\"\n"
-        //    + "\"919228741\";\"ALEX TRANSPORT\";\"919018828\";\"BEDR\";\"2017-07-03\";\"2017-06-04\";\n"
-        //    + "\"919228768\";\"UNISOL UTEGLASS AS\";\"919146605\";\"BEDR\";;;\n"
-        //    + "\"919228776\";\"PETBNB AS\";\"919061227\";\"BEDR\";\"2017-07-03\";\"2017-05-15\";\"2017-05-25\"",
-        //    ";")]
-        //private void ShouldParseEntityFromUnquotedCommaString(string source, string delimiter)
-        //{
-        //    //var actual = CsvParser.GetParsedEntities(source, delimiter, (string.Empty, string.Empty)[]).ToArray();
-
-        //    Assert.Equal(3, actual.Length);
-        //    Assert.Equal("919061227", actual[2]["parent_unit"]);
-        //}
-
         [Theory]
         [InlineData(
 @"statId;name;addr1;activity1;employees;activityYear
--- test/nscreg.Business.Test/DataSources/PopulateServiceTest.cs
+++ test/nscreg.Business.Test/DataSources/PopulateServiceTest.cs
@@ -1,17 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using Moq;
-using Nest;
+using FluentAssertions;
 using nscreg.Business.Test.Base;
-using nscreg.Data;
 using nscreg.Data.Constants;
 using nscreg.Data.Entities;
 using nscreg.Resources.Languages;
 using nscreg.Server.Common;
 using nscreg.TestUtils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -30,6 +27,71 @@ namespace nscreg.Business.Test.DataSources
 
         public PopulateServiceTest(ITestOutputHelper helper) : base(helper)
         {
+        }
+
+        [Fact]
+        public async Task PopulateAsync_PersonMapping_Success()
+        {
+            var mappings =
+    "statId-StatId,name-Name,activity1-Activities.Activity.ActivityCategory.Code,employees-Activities.Activity.Employees,activityYear-Activities.Activity.ActivityYear,addr1-Address.AddressPart1,PersonRole-Persons.Person.Role";
+
+            var mappingsArray = mappings.Split(',').Select(vm =>
+            {
+                var pair = vm.Split('-');
+                return (pair[0], pair[1]);
+            }).ToArray();
+
+            using (var context = InMemoryDb.CreateDbContext())
+            {
+                context.PersonTypes.Add(new PersonType { Name = "DIRECTOR" });
+                await context.SaveChangesAsync();
+
+                var populateService = new PopulateService(mappingsArray, DataSourceAllowedOperation.Create, DataSourceUploadTypes.StatUnits, StatUnitTypes.LegalUnit, context);
+
+                var raw = new Dictionary<string, object>()
+                {
+                    { "StatId", "920951287"},
+                    { "Name", "LAST FRIDAY INVEST AS" },
+                    { "Address.AddressPart1", "TEST ADDRESS" },
+                    { "Persons", new List<KeyValuePair<string, Dictionary<string, string>>>(){
+                        new KeyValuePair<string, Dictionary<string, string>>("Person", new Dictionary<string, string>()
+                            {
+                                {"Role", "Director"},
+                            })
+                        }
+                    },
+                    { "Activities", new List<KeyValuePair<string, Dictionary<string, string>>>()
+                        {
+                            new KeyValuePair<string, Dictionary<string, string>>("Activity", new Dictionary<string, string>()
+                            {
+                                {"ActivityCategory.Code", "62.020"},
+                                {"Employees","100"},
+                                {"ActivityYear","2019"}
+                            }),
+                            new KeyValuePair<string, Dictionary<string, string>>("Activity", new Dictionary<string, string>()
+                            {
+                                {"ActivityCategory.Code", "70.220"},
+                                {"Employees","20"},
+                                {"ActivityYear","2018"}
+                            }),
+                            new KeyValuePair<string, Dictionary<string, string>>("Activity", new Dictionary<string, string>()
+                            {
+                                {"ActivityCategory.Code", "52.292"},
+                                {"Employees","10"}
+                            }),
+                            new KeyValuePair<string, Dictionary<string, string>>("Activity", new Dictionary<string, string>()
+                            {
+                                {"ActivityCategory.Code", "68.209"},
+                            })
+                        }
+                    }
+                };
+
+
+                var (popUnit, isNeW, errors) = await populateService.PopulateAsync(raw);
+
+
+            }
         }
 
         [Fact]
@@ -73,6 +135,8 @@ namespace nscreg.Business.Test.DataSources
             }
 
         }
+
+
 
 
     }
