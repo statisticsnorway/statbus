24a95373a6ad525c824b372a84d3b3e30516c319
Sirar Salih <sirar.salih@wintrytech.com>
2023-09-14 16:16:43 +0200

    Re-write component from recompose to hooks using ChatGPT.

--- client/components/fields/DateTimeField.jsx
+++ client/components/fields/DateTimeField.jsx
@@ -1,30 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { bool, arrayOf, func, string } from 'prop-types'
 import DatePicker from 'react-datepicker'
 import { Form, Message } from 'semantic-ui-react'
 import R from 'ramda'
-
 import * as dateFns from 'helpers/dateHelper'
 import { hasValue } from 'helpers/validation'
 
-class DateTimeField extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      isDateValid: true,
-      errorMessages: [],
-    }
+const DateTimeField = ({
+  id: ambiguousId,
+  name: ambiguousName,
+  value,
+  onChange,
+  label: labelKey,
+  title: titleKey,
+  placeholder: placeholderKey,
+  touched,
+  error,
+  required,
+  errors: errorKeys,
+  localize,
+  popuplocalizedKey,
+  disabled,
+  readOnly,
+  ...restProps
+}) => {
+  const [isDateValid, setIsDateValid] = useState(true)
+  const [errorMessages, setErrorMessages] = useState([])
+
+  const onChangeWrapper = (ambiguousValue) => {
+    const { name } = restProps
+    const nextValue = ensure(ambiguousValue)
+    setIsDateValid(true)
+    setErrorMessages([])
+    onChange({ target: { name, value: nextValue } }, { ...restProps, value: nextValue })
   }
 
-  onChangeWrapper = (ambiguousValue) => {
-    const { name, onChange } = this.props
-    const nextValue = this.ensure(ambiguousValue)
-    this.setState({ isDateValid: true, errorMessages: [] })
-    onChange({ target: { name, value: nextValue } }, { ...this.props, value: nextValue })
-  }
-
-  onChangeRawWrapper = (event) => {
-    const { name, onChange } = this.props
+  const onChangeRawWrapper = (event) => {
+    const { name } = restProps
     const isEmpty = event.target.value === ''
     const parsed = dateFns.parse(event.target.value && event.target.value.slice(0, 10))
     const isDateValid = (!!parsed && parsed.isValid() && dateFns.isDateInThePast(parsed)) || isEmpty
@@ -36,87 +48,61 @@ class DateTimeField extends React.Component {
           : !dateFns.isDateInThePast(parsed)
             ? ['DateCantBeInFuture']
             : ['DateNotValid']
-    this.setState({ isDateValid, errorMessages })
-    const nextValue = isEmpty ? '' : isDateValid ? this.ensure(parsed) : ''
-    onChange({ target: { name, value: nextValue } }, { ...this.props, value: nextValue })
+    setIsDateValid(isDateValid)
+    setErrorMessages(errorMessages)
+    const nextValue = isEmpty ? '' : isDateValid ? ensure(parsed) : ''
+    onChange({ target: { name, value: nextValue } }, { ...restProps, value: nextValue })
   }
 
-  format = x => dateFns.formatDate(x, this.props.dateFormat)
+  const format = x => dateFns.formatDate(x, restProps.dateFormat)
 
-  ensure = x =>
+  const ensure = x =>
     R.cond([
-      [
-        hasValue,
-        R.pipe(
-          this.format,
-          dateFns.toUtc,
-        ),
-      ],
+      [hasValue, R.pipe(format, dateFns.toUtc)],
       [R.T, R.identity],
     ])(x)
 
-  render() {
-    const {
-      id: ambiguousId,
-      name: ambiguousName,
-      value,
-      onChange,
-      label: labelKey,
-      title: titleKey,
-      placeholder: placeholderKey,
-      touched,
-      error,
-      required,
-      errors: errorKeys,
-      localize,
-      popuplocalizedKey,
-      disabled,
-      readOnly,
-      ...restProps
-    } = this.props
-    const hasErrors = touched !== false && hasValue(errorKeys)
+  const label = labelKey !== undefined ? localize(labelKey) : undefined
+  const title = titleKey ? localize(titleKey) : label
+  const id =
+    ambiguousId != null ? ambiguousId : ambiguousName != null ? ambiguousName : 'DateTimeField'
 
-    const label = labelKey !== undefined ? localize(labelKey) : undefined
-    const title = titleKey ? localize(titleKey) : label
-    const id =
-      ambiguousId != null ? ambiguousId : ambiguousName != null ? ambiguousName : 'DateTimeField'
-    const filteredErrorMessages = errorKeys.filter(erKey => this.state.errorMessages.filter(stateKey => stateKey === erKey).length !== 1)
-    filteredErrorMessages.forEach(el => this.state.errorMessages.push(el))
+  const hasErrors = touched !== false && hasValue(errorKeys)
 
-    const inputProps = {
-      ...restProps,
-      id,
-      name: ambiguousName,
-      title,
-      required,
-      as: DatePicker,
-      disabled: disabled || readOnly,
-      selected: dateFns.getDateOrNull(value),
-      error: error || hasErrors,
-      placeholder: placeholderKey ? localize(placeholderKey) : label,
-      className: 'ui input',
-      onChange: this.onChangeWrapper,
-      onChangeRaw: this.onChangeRawWrapper,
-      maxDate: dateFns.now(),
-      autoComplete: 'off',
-    }
-    return (
-      <div
-        className={`field datepicker${required ? ' required' : ''}
-          ${hasErrors || !this.state.isDateValid ? ' error' : ''}
+  const inputProps = {
+    ...restProps,
+    id,
+    name: ambiguousName,
+    title,
+    required,
+    as: DatePicker,
+    disabled: disabled || readOnly,
+    selected: dateFns.getDateOrNull(value),
+    error: error || hasErrors,
+    placeholder: placeholderKey ? localize(placeholderKey) : label,
+    className: 'ui input',
+    onChange: onChangeWrapper,
+    onChangeRaw: onChangeRawWrapper,
+    maxDate: dateFns.now(),
+    autoComplete: 'off',
+  }
+
+  return (
+    <div
+      className={`field datepicker${required ? ' required' : ''}
+          ${hasErrors || !isDateValid ? ' error' : ''}
           ${disabled ? 'disabled' : ''}
         `}
-        data-tooltip={popuplocalizedKey ? localize(popuplocalizedKey) : null}
-        data-position="top left"
-      >
-        {label !== undefined && <label htmlFor={id}>{label}</label>}
-        <Form.Input {...inputProps} />
-        {(hasErrors || !this.state.isDateValid) && (
-          <Message title={label} list={this.state.errorMessages.map(localize)} compact error />
-        )}
-      </div>
-    )
-  }
+      data-tooltip={popuplocalizedKey ? localize(popuplocalizedKey) : null}
+      data-position="top left"
+    >
+      {label !== undefined && <label htmlFor={id}>{label}</label>}
+      <Form.Input {...inputProps} />
+      {(hasErrors || !isDateValid) && (
+        <Message title={label} list={errorMessages.map(localize)} compact error />
+      )}
+    </div>
+  )
 }
 
 DateTimeField.propTypes = {
