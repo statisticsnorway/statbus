dcfcdc012a5cf3593e0d6a9f95aa905bd66f75a9
Sirar Salih <sirar.salih@wintrytech.com>
2023-09-18 14:37:24 +0200

    Re-write from recompose to hooks using ChatGPT.

--- client/pages/Roles/List/List.jsx
+++ client/pages/Roles/List/List.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { func, string, number, shape, arrayOf } from 'prop-types'
+import React, { useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
 import { Table, Confirm } from 'semantic-ui-react'
 import { equals } from 'ramda'
 
@@ -7,99 +7,90 @@ import Paginate from 'components/Paginate'
 import TableHeader from './Table/TableHeader'
 import ListItem from './ListItem'
 
-class RolesList extends React.Component {
-  static propTypes = {
-    localize: func.isRequired,
-    toggleRole: func.isRequired,
-    fetchRoles: func.isRequired,
-    totalCount: number.isRequired,
-    query: shape({}).isRequired,
-    roles: arrayOf(shape({
-      id: string.isRequired,
-      name: string.isRequired,
-      description: string.isRequired,
-      activeUsers: number.isRequired,
-      status: number.isRequired,
-    })).isRequired,
-  }
+function RolesList({ localize, toggleRole, fetchRoles, totalCount, query, roles: initialRoles }) {
+  const [showConfirm, setShowConfirm] = useState(false)
+  const [selectedId, setSelectedId] = useState(undefined)
+  const [selectedStatus, setSelectedStatus] = useState(undefined)
+  const [roles, setRoles] = useState(initialRoles)
 
-  state = {
-    showConfirm: false,
-    selectedId: undefined,
-    selectedStatus: undefined,
-  }
+  useEffect(() => {
+    fetchRoles(query)
+  }, [fetchRoles, query])
 
-  componentDidMount() {
-    this.props.fetchRoles(this.props.query)
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (!equals(nextProps.query, this.props.query)) {
-      nextProps.fetchRoles(nextProps.query)
+  useEffect(() => {
+    if (!equals(roles, initialRoles)) {
+      setRoles(initialRoles)
     }
+  }, [initialRoles])
+
+  const handleToggle = (id, status) => () => {
+    setSelectedId(id)
+    setSelectedStatus(status)
+    setShowConfirm(true)
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    return (
-      this.props.localize.lang !== nextProps.localize.lang ||
-      !equals(this.props, nextProps) ||
-      !equals(this.state, nextState)
-    )
+  const handleConfirm = () => {
+    const id = selectedId
+    const status = selectedStatus
+    setShowConfirm(false)
+    setSelectedId(undefined)
+    setSelectedStatus(undefined)
+    toggleRole(id, status ? 0 : 1)
   }
 
-  handleToggle = (id, status) => () => {
-    this.setState({ selectedId: id, selectedStatus: status, showConfirm: true })
+  const handleCancel = () => {
+    setShowConfirm(false)
   }
 
-  handleConfirm = () => {
-    const id = this.state.selectedId
-    const status = this.state.selectedStatus
-    this.setState({ showConfirm: false, selectedId: undefined, selectedStatus: undefined })
-    this.props.toggleRole(id, status ? 0 : 1)
-  }
-
-  handleCancel = () => {
-    this.setState({ showConfirm: false })
-  }
-
-  renderConfirm = () => {
-    const { localize, roles } = this.props
-    const { name: confirmName } = roles.find(r => r.id === this.state.selectedId)
-    const msgKey = this.state.selectedStatus ? 'DeleteRoleMessage' : 'UndeleteRoleMessage'
+  const renderConfirm = () => {
+    const { name: confirmName } = roles.find(r => r.id === selectedId)
+    const msgKey = selectedStatus ? 'DeleteRoleMessage' : 'UndeleteRoleMessage'
     return (
       <Confirm
-        open={this.state.showConfirm}
+        open={showConfirm}
         header={`${localize('AreYouSure')}?`}
         content={`${localize(msgKey)} "${confirmName}"?`}
-        onConfirm={this.handleConfirm}
-        onCancel={this.handleCancel}
+        onConfirm={handleConfirm}
+        onCancel={handleCancel}
       />
     )
   }
 
-  render() {
-    const { roles, totalCount, localize } = this.props
-    return (
-      <div>
-        {this.state.showConfirm && this.renderConfirm()}
-        <h2>{localize('RolesList')}</h2>
-        <Paginate totalCount={totalCount}>
-          <Table selectable>
-            <TableHeader localize={localize} />
-            {roles &&
-              roles.map(r => (
-                <ListItem
-                  key={r.id}
-                  {...r}
-                  onToggle={this.handleToggle(r.id, r.status)}
-                  localize={localize}
-                />
-              ))}
-          </Table>
-        </Paginate>
-      </div>
-    )
-  }
+  return (
+    <div>
+      {showConfirm && renderConfirm()}
+      <h2>{localize('RolesList')}</h2>
+      <Paginate totalCount={totalCount}>
+        <Table selectable>
+          <TableHeader localize={localize} />
+          {roles &&
+            roles.map(r => (
+              <ListItem
+                key={r.id}
+                {...r}
+                onToggle={handleToggle(r.id, r.status)}
+                localize={localize}
+              />
+            ))}
+        </Table>
+      </Paginate>
+    </div>
+  )
+}
+
+RolesList.propTypes = {
+  localize: PropTypes.func.isRequired,
+  toggleRole: PropTypes.func.isRequired,
+  fetchRoles: PropTypes.func.isRequired,
+  totalCount: PropTypes.number.isRequired,
+  query: PropTypes.shape({}).isRequired,
+  roles: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    activeUsers: PropTypes.number.isRequired,
+    status: PropTypes.number.isRequired,
+  })).isRequired,
 }
 
 export default RolesList
