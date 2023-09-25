8f1d190bd64ad872670e46eb70597f31a7cddc98
Sirar Salih <sirar.salih@wintrytech.com>
2023-09-25 12:30:30 +0200

    Refactor code, make it to fulfull seperation of concerns.

--- client/pages/DataFlowExamples/SharedStateExamples/LiftUpStateStatUnitExample/index.jsx
+++ client/pages/DataFlowExamples/SharedStateExamples/LiftUpStateStatUnitExample/index.jsx
@@ -2,9 +2,6 @@ import React, { useState } from 'react'
 
 function LiftUpStateStatUnitParent() {
   const [legalUnitId, setLegalUnitId] = useState(3)
-  const [legalUnitName] = useState('LegalUnitName')
-  const [localUnitId] = useState('5')
-  const [localUnitName] = useState('LocalUnitName')
   const [showLegalUnit, setShowLegalUnit] = useState(false)
   const [showLocalUnit, setShowLocalUnit] = useState(false)
 
@@ -29,80 +26,51 @@ function LiftUpStateStatUnitParent() {
       <br />
 
       <div>
-        <LiftUpStateLegalUnitChild
-          legalUnitId={legalUnitId}
-          legalUnitName={legalUnitName}
-          setLegalUnitId={setLegalUnitId}
-          showLegalUnit={showLegalUnit}
-        />
-        <LiftUpStateLocalUnitChild
-          localUnitId={localUnitId}
-          localUnitName={localUnitName}
-          legalUnitId={legalUnitId}
-          setLegalUnitId={setLegalUnitId}
-          showLocalUnit={showLocalUnit}
-        />
+        {showLegalUnit && <LegalUnit legalUnitId={legalUnitId} setLegalUnitId={setLegalUnitId} />}
+        {showLocalUnit && <LocalUnit legalUnitId={legalUnitId} setLegalUnitId={setLegalUnitId} />}
       </div>
     </div>
   )
 }
 
-function LiftUpStateLegalUnitChild({ legalUnitId, legalUnitName, setLegalUnitId, showLegalUnit }) {
+function LegalUnit({ legalUnitId, setLegalUnitId }) {
+  const [legalUnitName] = useState('LegalUnitName')
+
   return (
     <div>
-      {showLegalUnit && (
-        <div>
-          <strong>Legal unit</strong>
-          <div>
-            ID:
-            <br />
-            <input
-              type="text"
-              value={legalUnitId}
-              onChange={ev => setLegalUnitId(ev.target.value)}
-            />
-            <br />
-            Name:
-            <br />
-            <input type="text" value={legalUnitName} readOnly />
-          </div>
-        </div>
-      )}
+      <strong>Legal unit</strong>
+      <div>
+        ID:
+        <br />
+        <input type="text" value={legalUnitId} onChange={ev => setLegalUnitId(ev.target.value)} />
+        <br />
+        Name:
+        <br />
+        <input type="text" value={legalUnitName} readOnly />
+      </div>
     </div>
   )
 }
 
-function LiftUpStateLocalUnitChild({
-  localUnitId,
-  localUnitName,
-  legalUnitId,
-  setLegalUnitId,
-  showLocalUnit,
-}) {
+function LocalUnit({ legalUnitId, setLegalUnitId }) {
+  const [localUnitId] = useState('5')
+  const [localUnitName] = useState('LocalUnitName')
   return (
     <div>
-      {showLocalUnit && (
-        <div>
-          <strong>Local unit:</strong>
-          <div>
-            ID:
-            <br />
-            <input type="text" value={localUnitId} readOnly />
-            <br />
-            Name:
-            <br />
-            <input type="text" value={localUnitName} readOnly />
-            <br />
-            Legal unit ID:
-            <br />
-            <input
-              type="text"
-              value={legalUnitId}
-              onChange={ev => setLegalUnitId(ev.target.value)}
-            />
-          </div>
-        </div>
-      )}
+      <strong>Local unit:</strong>
+      <div>
+        ID:
+        <br />
+        <input type="text" value={localUnitId} readOnly />
+        <br />
+        Name:
+        <br />
+        <input type="text" value={localUnitName} readOnly />
+        <br />
+        Legal unit ID:
+        <br />
+        <input type="text" value={legalUnitId} onChange={ev => setLegalUnitId(ev.target.value)} />
+      </div>
     </div>
   )
 }
