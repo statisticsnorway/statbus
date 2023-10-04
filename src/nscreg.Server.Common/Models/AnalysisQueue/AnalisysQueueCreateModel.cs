07b3a0b82ee8015d59a381c5452c6ed58e7ea343
Sirar Salih <sirar.salih@wintrytech.com>
2023-10-04 14:44:57 +0200

    Change to DateTimeOffset.

--- src/nscreg.Server.Common/Models/AnalysisQueue/AnalisysQueueCreateModel.cs
+++ src/nscreg.Server.Common/Models/AnalysisQueue/AnalisysQueueCreateModel.cs
@@ -6,8 +6,8 @@ namespace nscreg.Server.Common.Models.AnalysisQueue
 {
     public class AnalisysQueueCreateModel
     {
-        public DateTime? DateFrom { get; set; }
-        public DateTime? DateTo { get; set; }
+        public DateTimeOffset? DateFrom { get; set; }
+        public DateTimeOffset? DateTo { get; set; }
         public string Comment { get; set; }
     }
 }
