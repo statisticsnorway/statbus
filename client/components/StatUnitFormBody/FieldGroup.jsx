4d18ed639309f745f5167d77fb23c2abc7831ad5
Sirar Salih <sirar.salih@wintrytech.com>
2023-10-13 14:17:14 +0200

    Render the form children properly, export the class.

--- client/components/StatUnitFormBody/FieldGroup.jsx
+++ client/components/StatUnitFormBody/FieldGroup.jsx
@@ -2,30 +2,12 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Form } from 'semantic-ui-react'
 
-class FieldGroup extends Component {
+export class FieldGroup extends Component {
   render() {
     const { isExtended, children } = this.props
     return (
       <Form.Group widths="equal">
-        {children[0].key == 'statId' && children}
-        {children[0].key == 'name' && children}
-        {children[0].key == 'taxRegId' && children}
-        {children[0].key == 'externalId' && children}
-        {children[0].key == 'registrationReasonId' && children}
-        {children[0].key == 'dataSourceClassificationId' && children}
-        {children[0].key == 'legalUnitId' && children}
-        {children[0].key == 'telephoneNo' && children}
-        {children[0].key == 'address' && children}
-        {children[0].key == 'actualAddress' && children}
-        {children[0].key == 'postalAddress' && children}
-        {/* {children[0].key == "activities" && children} */} {/* Does not work */}
-        {children[0].key == 'sizeId' && children}
-        {children[0].key == 'turnoverYear' && children}
-        {children[0].key == 'numOfPeopleEmp' && children}
-        {/* {children[0].key == "persons" && children} */} {/* Does not work */}
-        {children[0].key == 'reorgTypeId' && children}
-        {children[0].key == 'foreignParticipationId' && children}
-        {children[0].key == 'notes' && children}
+        {children}
         {!isExtended && children.length % 2 !== 0 && <div className="field" />}
       </Form.Group>
     )
