5536b1e6735ed38bd148acb6b191c1dfd5f486fe
Sirar Salih <sirar.salih@wintrytech.com>
2023-10-17 16:18:20 +0200

    Re-write back to using functional Hooks.

--- client/pages/StatUnits/View/tabs/Activity.jsx
+++ client/pages/StatUnits/View/tabs/Activity.jsx
@@ -1,31 +1,18 @@
 import React from 'react'
 import { shape, func, arrayOf, string } from 'prop-types'
 import { Header } from 'semantic-ui-react'
+
 import { ActivitiesList } from '/client/components/fields'
 import styles from './styles.scss'
 
-class Activity extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      data: props.data || [], // Initialize the data state with props or an empty array
-    }
-  }
-
-  render() {
-    const { localize, activeTab } = this.props
-    const { data } = this.state
-
-    return (
-      <div>
-        {activeTab !== 'activity' && (
-          <Header as="h5" className={styles.heigthHeader} content={localize('Activity')} />
-        )}
-        <ActivitiesList name="activities" value={data} localize={localize} readOnly />
-      </div>
-    )
-  }
-}
+const Activity = ({ data, localize, activeTab }) => (
+  <div>
+    {activeTab !== 'activity' && (
+      <Header as="h5" className={styles.heigthHeader} content={localize('Activity')} />
+    )}
+    <ActivitiesList name="activities" value={data} localize={localize} readOnly />
+  </div>
+)
 
 Activity.propTypes = {
   data: arrayOf(shape({})),
