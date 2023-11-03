6a431656d4e6dbab5d77bba9e053dc0d582d4841
JÃ¸rgen H. Fjeld <jorgen@veridit.no>
2023-11-03 10:03:25 +0100

    Demonstrate problem with periods
    This requires a fix, or another approach altogheter.

--- dbseed/periods_inconsistent.sql
+++ dbseed/periods_inconsistent.sql
@@ -24,7 +24,7 @@ TABLE uk;
 TABLE fk;
 
 --expected: fail,    behavior: deleted
-DELETE FROM uk WHERE (id, s, e) =(1, 1, 3);
+DELETE FROM uk WHERE (id, s, e) = (1, 1, 3);
 
 TABLE uk;
 TABLE fk;
@@ -43,5 +43,16 @@ UPDATE uk SET e = 3 WHERE (id, s, e) = (2, 1, 5);
 
 TABLE uk;
 TABLE fk;
+
+-- Create non contiguous time -- Should fail.
+INSERT INTO uk(id, s, e)        VALUES    (3, 1, 3),
+                                          (3, 4, 5);
+-- Reference over non contiguous time
+INSERT INTO fk(id, uk_id, s, e) VALUES (5, 3, 1, 5);
+
+
+-- Create overlappig range
+INSERT INTO uk(id, s, e)        VALUES    (4, 1, 4),
+                                          (4, 3, 5);
 
 \set ECHO :original_ECHO
