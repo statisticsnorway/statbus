beff1cdc318aab0c4fe5f4e0b5fa8db9df5550b6
JÃ¸rgen H. Fjeld <jorgen@veridit.no>
2023-11-03 10:04:18 +0100

    [ui] Add helpers from Supabase Svelte setup

--- ui/hooks.server.ts
+++ ui/hooks.server.ts
@@ -1,7 +1,28 @@
 // src/hooks.server.ts
 import { type Handle, redirect, error } from '@sveltejs/kit'
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
+import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit'
+
 
 export const handle: Handle = async ({ event, resolve }) => {
+  event.locals.supabase = createSupabaseServerClient({
+    supabaseUrl: PUBLIC_SUPABASE_URL,
+    supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
+    event,
+  })
+
+  /**
+   * a little helper that is written for convenience so that instead
+   * of calling `const { data: { session } } = await supabase.auth.getSession()`
+   * you just call this `await getSession()`
+   */
+  event.locals.getSession = async () => {
+    const {
+      data: { session },
+    } = await event.locals.supabase.auth.getSession()
+    return session
+  }
+
   // protect requests to all routes that start with /protected-routes
   if (event.url.pathname.startsWith('/protected-routes')) {
     const session = await event.locals.getSession()
@@ -20,5 +41,10 @@ export const handle: Handle = async ({ event, resolve }) => {
     }
   }
 
-  return resolve(event)
+  return resolve(event, {
+    filterSerializedResponseHeaders(name) {
+      return name === 'content-range'
+    },
+  })
+  //return resolve(event)
 }
