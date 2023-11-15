71bbfeb5e9c6935359f35680fb5836505b4d1efa
Jørgen H. Fjeld <jorgen@veridit.no>
2023-11-15 13:37:45 +0100

    [db] Improve checking of sql functions

--- dbseed/create-db-structure.sql
+++ dbseed/create-db-structure.sql
@@ -9,7 +9,7 @@ SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
+SET check_function_bodies = true;
 SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
