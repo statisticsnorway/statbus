7efeda20358db09338a5e781bdf0deef47e28232
Jørgen H. Fjeld <jorgen@veridit.no>
2023-11-20 12:02:42 +0100

    [ui] Use Skeleton AppShell

--- ui/src/routes/+layout.svelte
+++ ui/src/routes/+layout.svelte
@@ -4,6 +4,7 @@
   import { invalidate } from '$app/navigation'
   import { onMount } from 'svelte'
   import { themeStore } from '../stores'; // import the store
+  import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 
   export let data
 
@@ -22,6 +23,31 @@
     return () => subscription.unsubscribe()
   });
 
+  // Scroll to top when navigating between pages.
+  import type { AfterNavigate } from '@sveltejs/kit';
+  import { afterNavigate } from '$app/navigation';
+
+  afterNavigate((params: AfterNavigate) => {
+      const isNewPage: boolean = params.from?.route.id !== params.to?.route.id;
+      const elemPage = document.querySelector('#page');
+      if (isNewPage && elemPage !== null) {
+          elemPage.scrollTop = 0;
+      }
+  });
+
 </script>
 
-<slot></slot>
+
+<AppShell>
+  <svelte:fragment slot="header">
+    <AppBar>AppBar</AppBar>
+  </svelte:fragment>
+  <!-- (sidebarLeft) -->
+  <!-- (sidebarRight) -->
+  <!-- (pageHeader) -->
+  <!-- Router Slot -->
+  <slot />
+  <!-- ---- / ---- -->
+  <!-- (pageFooter) -->
+  <!-- (footer) -->
+</AppShell>
