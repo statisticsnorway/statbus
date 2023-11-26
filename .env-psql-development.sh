ed65be2085d9463e9c1abd3631506b25825597a1
Jørgen H. Fjeld <jorgen@veridit.no>
2023-11-26 11:17:40 +0100

    [dev] Adjust to local config

--- .env-psql-development.sh
+++ .env-psql-development.sh
@@ -1,7 +1,8 @@
 WORKSPACE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export PGHOST=localhost
-export PGPORT=$(awk -F '=' '/POSTGRES_PORT/{print $2}' $WORKSPACE/supabase/docker/.env)
+#export PGPORT=$(awk -F '=' '/POSTGRES_PORT/{print $2}' $WORKSPACE/supabase/docker/.env)
+export PGPORT=3432
 export PGUSER=postgres
 export PGDATABASE=$(awk -F '=' '/POSTGRES_DB/{print $2}' $WORKSPACE/supabase/docker/.env)
 export PGPASSWORD=$(awk -F '=' '/POSTGRES_PASSWORD/{print $2}' $WORKSPACE/supabase/docker/.env)
