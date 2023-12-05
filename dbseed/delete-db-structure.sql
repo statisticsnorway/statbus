b1d8c879931598c03a538f9d74377b3d82de8d86
Jørgen H. Fjeld <jorgen@veridit.no>
2023-12-05 18:13:02 +0100

    [db,seed] Load person_type via view
    Also add proper codes.

--- dbseed/delete-db-structure.sql
+++ dbseed/delete-db-structure.sql
@@ -31,6 +31,7 @@ SELECT admin.drop_table_views_for_batch_api('public.foreign_participation');
 SELECT admin.drop_table_views_for_batch_api('public.data_source_classification');
 SELECT admin.drop_table_views_for_batch_api('public.unit_status');
 SELECT admin.drop_table_views_for_batch_api('public.unit_size');
+SELECT admin.drop_table_views_for_batch_api('public.person_type');
 
 DROP FUNCTION admin.generate_view(regclass,admin.view_type_enum);
 DROP FUNCTION admin.generate_code_upsert_function(regclass,admin.view_type_enum);
