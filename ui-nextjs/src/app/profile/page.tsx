98fb2e10d31c0d85b2dbac7209911ece82bd3d09
Jonas Lundeland <jonas.lundeland@tv2.no>
2023-12-21 15:22:08 +0100

    [ui] Add example profile page
    [ui] Replace scaffolded nextjs index page with activity category listing demo example

---
+++ ui-nextjs/src/app/profile/page.tsx
@@ -0,0 +1,45 @@
+import {createClient} from "@/app/auth/lib/supabase.server.client";
+
+export default async function ProfilePage() {
+  const client = createClient()
+  const {data: { session}} = await client.auth.getSession()
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="mx-auto sm:w-full lg:max-w-4xl">
+        <div className="px-4 sm:px-0">
+          <h3 className="text-base font-semibold leading-7 text-gray-900">Profile Information</h3>
+          <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">This is what the system knows about you</p>
+        </div>
+        <div className="mt-6 border-t border-gray-100">
+          <dl className="divide-y divide-gray-100">
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">User ID</dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{session?.user?.id}</dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">Full name</dt>
+              <dd className="mt-1 text-sm capitalize leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{session?.user?.email?.split("@")[0].replace(/\./, " ")}</dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">Email address</dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{session?.user?.email}</dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">Phone</dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{session?.user.phone || "N/A"}</dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">Email confirmed</dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{session?.user.last_sign_in_at}</dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">Last sign in</dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{session?.user.email_confirmed_at}</dd>
+            </div>
+          </dl>
+        </div>
+      </div>
+    </main>
+  )
+}
