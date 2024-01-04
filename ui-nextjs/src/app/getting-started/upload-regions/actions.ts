e4b8d4c00da313b64e5c96c8dd3b89705c77ea7c
Jonas Lundeland <jonas.lundeland@tv2.no>
2024-01-04 13:27:49 +0100

    [ui] Post regions to region view for upsert logic

--- ui-nextjs/src/app/getting-started/upload-regions/actions.ts
+++ ui-nextjs/src/app/getting-started/upload-regions/actions.ts
@@ -8,7 +8,7 @@ export async function uploadRegions(formData: FormData) {
   const file = formData.get('regions') as File
   const session = await client.auth.getSession()
 
-  const response = await fetch(`${process.env.SUPABASE_URL}/rest/v1/region`, {
+  const response = await fetch(`${process.env.SUPABASE_URL}/rest/v1/region_view`, {
     method: 'POST',
     headers: {
       Authorization: `Bearer ${session.data.session?.access_token}`,
