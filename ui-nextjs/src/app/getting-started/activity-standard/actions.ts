2cb191b78ab9c5341e5e13fdc30eb80b8c3bcdeb
Jonas Lundeland <jonas.lundeland@tv2.no>
2024-01-05 10:14:18 +0100

    [ui] Upsert activity standard code
    We instruct supabase / postgrest to upsert on activity standard code by passing the following option to supabase client upsert method
    ```
    {
      onConflict: 'only_one_setting'
    }
    ```

--- ui-nextjs/src/app/getting-started/activity-standard/actions.ts
+++ ui-nextjs/src/app/getting-started/activity-standard/actions.ts
@@ -4,27 +4,36 @@ import {revalidatePath} from "next/cache";
 import {redirect} from "next/navigation";
 
 export async function setCategoryStandard(formData: FormData) {
-  "use server";
-  const client = createClient()
+    "use server";
+    const client = createClient()
 
-  const activityCategoryStandardIdFormEntry = formData.get('activity_category_standard_id')
-  if (!activityCategoryStandardIdFormEntry) {
-    return {error: 'No activity category standard provided'}
-  }
+    const activityCategoryStandardIdFormEntry = formData.get('activity_category_standard_id')
+    if (!activityCategoryStandardIdFormEntry) {
+        return {error: 'No activity category standard provided'}
+    }
 
-  const activityCategoryStandardId = parseInt(activityCategoryStandardIdFormEntry.toString(), 10);
-  if (isNaN(activityCategoryStandardId)) {
-    return {error: 'Invalid activity category standard provided'}
-  }
+    const activityCategoryStandardId = parseInt(activityCategoryStandardIdFormEntry.toString(), 10);
+    if (isNaN(activityCategoryStandardId)) {
+        return {error: 'Invalid activity category standard provided'}
+    }
 
-  try {
-    // TODO: this should be an upsert request (separate view) but this is as of yet not implemented
-    await client.from('settings').insert({activity_category_standard_id: activityCategoryStandardId})
-    revalidatePath('/getting-started')
-  } catch (error) {
-    return {error: "Error setting category standard"}
-  }
+    try {
+        const response = await client
+            .from('settings')
+            .upsert({activity_category_standard_id: activityCategoryStandardId}, {
+                onConflict: 'only_one_setting',
+            })
 
-  redirect('/getting-started/upload-regions')
+        if (response.status >= 400) {
+            return {error: response.statusText}
+        }
+
+        revalidatePath('/getting-started')
+
+    } catch (error) {
+        return {error: "Error setting category standard"}
+    }
+
+    redirect('/getting-started/upload-regions')
 }
 
