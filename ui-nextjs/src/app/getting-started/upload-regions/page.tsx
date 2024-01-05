808eed0800ead286046d1af11af518185d49e606
Jonas Lundeland <jonas.lundeland@tv2.no>
2024-01-05 15:24:59 +0100

    [ui] Add informative accordion component to regions upload page

--- ui-nextjs/src/app/getting-started/upload-regions/page.tsx
+++ ui-nextjs/src/app/getting-started/upload-regions/page.tsx
@@ -4,12 +4,14 @@ import {Button} from "@/components/ui/button";
 import {uploadRegions} from "@/app/getting-started/upload-regions/actions";
 import {createClient} from "@/lib/supabase.server.client";
 import Link from "next/link";
+import {Accordion, AccordionContent, AccordionItem, AccordionTrigger} from "@/components/ui/accordion";
+import React from "react";
 
 export default async function Home() {
     const client = createClient()
     const {data: regions} = await client.from('region').select('id, name')
     return (
-        <section className="space-y-6">
+        <section className="space-y-8">
             <h1 className="text-xl text-center">Upload regions</h1>
             <p>Upload a CSV file containing the regions you want to use in your analysis.</p>
             <form action={uploadRegions} className="space-y-3 bg-green-100 p-6">
@@ -27,6 +29,16 @@ export default async function Home() {
                     </>
                 ) : null
             }
+
+            <Accordion type="single" collapsible>
+                <AccordionItem value="Activity Category Standard">
+                    <AccordionTrigger>What is a Regions file?</AccordionTrigger>
+                    <AccordionContent>
+                        A regions file is a CSV file containing the regions you want to use in your analysis. The file
+                        must conform to a specific format in order to be processed correctly.
+                    </AccordionContent>
+                </AccordionItem>
+            </Accordion>
         </section>
     )
 }
