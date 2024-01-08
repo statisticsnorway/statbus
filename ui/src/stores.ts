580b8c30df31ed79ad62f0771ab71047cd53961d
Jonas Lundeland <jonas.lundeland@tv2.no>
2024-01-08 14:31:18 +0100

    [ui] Delete sveltekit POC directory ui

--- ui/src/stores.ts
+++
@@ -1,5 +0,0 @@
-// src/stores.ts
-import { writable } from 'svelte/store';
-
-// Possible options: "white" | "g10" | "g80" | "g90" | "g100"
-export const themeStore = writable("g100"); // default theme
