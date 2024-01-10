861914eb432b805309bf7a6f8982d562435ed43e
Jonas Lundeland <jonas.lundeland@tv2.no>
2024-01-10 14:23:57 +0100

    [ui] Add skip link on activity category standard wizard page

--- app/src/app/getting-started/activity-standard/_components/CategoryStandardForm.tsx
+++ app/src/app/getting-started/activity-standard/_components/CategoryStandardForm.tsx
@@ -2,69 +2,73 @@
 import {zodResolver} from "@hookform/resolvers/zod"
 import * as z from "zod"
 import {RadioGroup, RadioGroupItem} from "@/components/ui/radio-group"
-import {Button} from "@/components/ui/button";
+import {Button, buttonVariants} from "@/components/ui/button";
 import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage,} from "@/components/ui/form"
 import {useForm} from "react-hook-form";
 import {setCategoryStandard} from "@/app/getting-started/activity-standard/actions";
+import Link from "next/link";
 
 interface CategoryStandardFormProps {
-    readonly standards: { id: number, name: string }[] | null
-    readonly settings: { id: number, activity_category_standard: { id: number, name: string } | null }[] | null
+  readonly standards: { id: number, name: string }[] | null
+  readonly settings: { id: number, activity_category_standard: { id: number, name: string } | null }[] | null
 }
 
 const FormSchema = z.object({
-    activity_category_standard_id: z.coerce.number({
-        required_error: "You need to select an activity category standard",
-        invalid_type_error: "You need to select an activity category standard"
-    }),
+  activity_category_standard_id: z.coerce.number({
+    required_error: "You need to select an activity category standard",
+    invalid_type_error: "You need to select an activity category standard"
+  }),
 })
 
 export default function CategoryStandardForm({standards, settings}: CategoryStandardFormProps) {
 
-    const form = useForm<z.infer<typeof FormSchema>>({
-        resolver: zodResolver(FormSchema),
-        defaultValues: {
-            activity_category_standard_id: settings?.[0]?.activity_category_standard?.id
-        }
-    })
-
-    async function onSubmit({activity_category_standard_id}: z.infer<typeof FormSchema>) {
-        const formData = new FormData()
-        formData.append('activity_category_standard_id', activity_category_standard_id.toString(10))
-        await setCategoryStandard(formData)
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      activity_category_standard_id: settings?.[0]?.activity_category_standard?.id
     }
+  })
 
-    return (
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 bg-green-100 p-6">
-                <FormField control={form.control} name="activity_category_standard_id" render={({field}) => (
-                    <FormItem className="space-y-3">
-                        <FormLabel>Select activity category standard</FormLabel>
-                        <FormControl>
-                            <RadioGroup
-                                onValueChange={field.onChange}
-                                defaultValue={settings?.[0]?.activity_category_standard?.id?.toString(10)}
-                                className="flex flex-col space-y-1"
-                            >
-                                {
-                                    standards?.map(({id, name}) => (
-                                        <FormItem className="flex items-center space-x-3 space-y-0" key={id}>
-                                            <FormControl>
-                                                <RadioGroupItem value={id.toString(10)} id={id.toString(10)}/>
-                                            </FormControl>
-                                            <FormLabel className="text-md font-normal">
-                                                {name}
-                                            </FormLabel>
-                                        </FormItem>
-                                    ))
-                                }
-                            </RadioGroup>
-                        </FormControl>
-                        <FormMessage/>
+  async function onSubmit({activity_category_standard_id}: z.infer<typeof FormSchema>) {
+    const formData = new FormData()
+    formData.append('activity_category_standard_id', activity_category_standard_id.toString(10))
+    await setCategoryStandard(formData)
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 bg-green-100 p-6">
+        <FormField control={form.control} name="activity_category_standard_id" render={({field}) => (
+          <FormItem className="space-y-3">
+            <FormLabel>Select activity category standard</FormLabel>
+            <FormControl>
+              <RadioGroup
+                onValueChange={field.onChange}
+                defaultValue={settings?.[0]?.activity_category_standard?.id?.toString(10)}
+                className="flex flex-col space-y-1"
+              >
+                {
+                  standards?.map(({id, name}) => (
+                    <FormItem className="flex items-center space-x-3 space-y-0" key={id}>
+                      <FormControl>
+                        <RadioGroupItem value={id.toString(10)} id={id.toString(10)}/>
+                      </FormControl>
+                      <FormLabel className="text-md font-normal">
+                        {name}
+                      </FormLabel>
                     </FormItem>
-                )}/>
-                <Button type="submit">Next</Button>
-            </form>
-        </Form>
-    )
+                  ))
+                }
+              </RadioGroup>
+            </FormControl>
+            <FormMessage/>
+          </FormItem>
+        )}/>
+        <div className="space-x-3">
+          <Button type="submit">Confirm</Button>
+          <Link href="/getting-started/upload-regions" className={buttonVariants({variant: 'outline'})}>Skip</Link>
+        </div>
+      </form>
+    </Form>
+  )
 }
