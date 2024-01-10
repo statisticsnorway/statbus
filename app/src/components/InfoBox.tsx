23a4b16fa6ce0f3665a1959a6cacf2fd80b16942
Jonas Lundeland <jonas.lundeland@tv2.no>
2024-01-10 15:38:08 +0100

    [ui] Add start page placeholder with legal unit listing

--- app/src/components/InfoBox.tsx
+++ app/src/components/InfoBox.tsx
@@ -2,7 +2,7 @@ import React, {ReactNode} from "react";
 import {Info} from "lucide-react";
 
 export const InfoBox = ({children}: { readonly children: ReactNode }) => (
-  <div className="flex items-center space-x-6 p-6 bg-amber-100">
+  <div className="flex items-center space-x-3 p-6 bg-amber-100">
     <div>
       <Info/>
     </div>
