fc7f26530140209cf612e33de6d8ef632bd4b692
Jonas Lundeland <jonas.lundeland@tv2.no>
2024-01-10 13:33:43 +0100

    [ui] Add reusable component InfoBox to show info messages to user

---
+++ app/src/components/InfoBox.tsx
@@ -0,0 +1,11 @@
+import React, {ReactNode} from "react";
+import {Info} from "lucide-react";
+
+export const InfoBox = ({children}: { readonly children: ReactNode }) => (
+  <div className="flex items-center space-x-6 p-6 bg-amber-100">
+    <div>
+      <Info/>
+    </div>
+    {children}
+  </div>
+)
