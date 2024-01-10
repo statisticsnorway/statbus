bba2c42742fef18c38c5bbf58af641406cfb04bc
Jonas Lundeland <jonas.lundeland@tv2.no>
2024-01-10 13:01:13 +0100

    [ui] Add upload page for legal units
    On this page the user may upload a CSV file containing Legal Units. The page also offers a FAQ and an example CSV download link.

---
+++ app/src/app/getting-started/upload-legal-units/page.tsx
@@ -0,0 +1,57 @@
+import {Label} from "@/components/ui/label";
+import {Input} from "@/components/ui/input";
+import {Button} from "@/components/ui/button";
+import {createClient} from "@/lib/supabase.server.client";
+import {Accordion, AccordionContent, AccordionItem, AccordionTrigger} from "@/components/ui/accordion";
+import {uploadLegalUnits} from "@/app/getting-started/upload-legal-units/actions";
+import React from "react";
+import Link from "next/link";
+
+export default async function UploadRegionsPage() {
+  const client = createClient()
+  const {data: legalUnits} = await client.from('legal_unit').select('id, name')
+  return (
+    <section className="space-y-8">
+      <h1 className="text-xl text-center">Upload Legal Units</h1>
+      <p>Upload a CSV file containing legal units you want to use in your analysis.</p>
+      <form action={uploadLegalUnits} className="space-y-3 bg-green-100 p-6">
+        <Label className="block" htmlFor="regions-file">Select Legal Units file:</Label>
+        <Input required id="regions-file" type="file" name="regions"/>
+        <Button type="submit">Next</Button>
+      </form>
+
+      {
+        legalUnits?.length ? (
+          <p>There are <strong>{legalUnits.length}</strong> Legal Units already defined.&nbsp;
+            <Link className="underline" href="/getting-started/summary">Continue -&gt;</Link>
+          </p>
+        ) : null
+      }
+
+      <Accordion type="single" collapsible>
+        <AccordionItem value="Legal Unit">
+          <AccordionTrigger>What is a Legal Unit?</AccordionTrigger>
+          <AccordionContent>
+            <p className="mb-3">
+              A <strong>Legal Unit</strong> refers to an entity or establishment that is considered an individual economic unit engaged in economic activities.
+
+              Both NACE and ISIC are classification systems used to categorize economic activities and units for statistical and analytical purposes.
+              They provide a framework to classify different economic activities and units based on their primary activities.
+            </p>
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="Legal Units File">
+          <AccordionTrigger>What is a Legal Units file?</AccordionTrigger>
+          <AccordionContent>
+            <p className="mb-3">
+              A Legal Units file is a CSV file containing the Legal Units you want to use in your analysis. The file
+              must conform to a specific format in order to be processed correctly. Have a look at this example
+              CSV file to get an idea of how the file should be structured:
+            </p>
+            <a href="/100BREGUnits.csv" download="legalunits.example.csv" className="underline">Download example CSV file</a>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </section>
+  )
+}
