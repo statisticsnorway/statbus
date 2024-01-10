210e691384295d6b15748c86a5552bcf60d82219
Jonas Lundeland <jonas.lundeland@tv2.no>
2024-01-10 15:55:27 +0100

    [ui] Align logout button bottom right on profile page

--- app/src/app/profile/page.tsx
+++ app/src/app/profile/page.tsx
@@ -8,18 +8,9 @@ export default async function ProfilePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-8 md:p-24">
       <div className="mx-auto sm:w-full lg:max-w-5xl">
-        <div className="flex items-center justify-between">
-          <div>
-            <h3 className="text-base font-semibold leading-7 text-gray-900">Profile Information</h3>
-            <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">This is what the system knows about you</p>
-          </div>
-          <form action={logout}>
-            <button
-              type="submit"
-              className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-600 font-medium rounded-md text-sm px-5 py-2.5 me-2">
-              Log out
-            </button>
-          </form>
+        <div>
+          <h3 className="text-base font-semibold leading-7 text-gray-900">Profile Information</h3>
+          <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">This is what the system knows about you</p>
         </div>
         <div className="mt-6 border-t border-gray-100">
           <dl className="divide-y divide-gray-100">
@@ -53,6 +44,13 @@ export default async function ProfilePage() {
             </div>
           </dl>
         </div>
+        <form action={logout} className="bg-gray-100 p-6 flex justify-end">
+          <button
+            type="submit"
+            className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-600 font-medium rounded-md text-sm px-5 py-2.5 me-2">
+            Log out
+          </button>
+        </form>
       </div>
     </main>
   )
