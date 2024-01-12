5f4dc18c1579829377cdfe225e4b7978efe2b3e1
Jørgen H. Fjeld <jorgen@veridit.no>
2024-01-12 15:41:35 +0100

    [db] Remove redundant column
    The activity_category_id link can be used to find the standard.

--- dbseed/create-db-structure.sql
+++ dbseed/create-db-structure.sql
@@ -879,7 +879,6 @@ CREATE TABLE public.activity (
     valid_to date NOT NULL DEFAULT 'infinity',
     activity_type public.activity_type NOT NULL,
     activity_category_id integer NOT NULL REFERENCES public.activity_category(id) ON DELETE CASCADE,
-    activity_category_standard_id integer NOT NULL REFERENCES public.activity_category_standard(id) ON DELETE RESTRICT,
     updated_by_user_id integer NOT NULL REFERENCES public.statbus_user(id) ON DELETE CASCADE,
     updated_at timestamp with time zone DEFAULT statement_timestamp() NOT NULL,
     establishment_id integer REFERENCES public.establishment(id) ON DELETE CASCADE,
@@ -890,7 +889,6 @@ CREATE TABLE public.activity (
         )
 );
 CREATE INDEX ix_activity_activity_category_id ON public.activity USING btree (activity_category_id);
-CREATE INDEX ix_activity_activity_category_standard_id ON public.activity USING btree (activity_category_standard_id);
 CREATE INDEX ix_activity_establishment_id_id ON public.activity USING btree (establishment_id);
 CREATE INDEX ix_activity_legal_unit_id_id ON public.activity USING btree (legal_unit_id);
 CREATE INDEX ix_activity_updated_by_user_id ON public.activity USING btree (updated_by_user_id);
