674f448ac5c995a55408678bde8c76b96ebdaf98
Jonas Lundeland <jonas.lundeland@tv2.no>
2024-01-15 15:41:55 +0100

    [ui] Add UI for table filters with demo data

--- app/src/app/search/components/TableToolbar.tsx
+++ app/src/app/search/components/TableToolbar.tsx
@@ -8,6 +8,20 @@ interface TableToolbarProps<TData> {
 }
 
 export default function TableToolbar<TData>({table, onSearch}: TableToolbarProps<TData>) {
+
+  const options = {
+    categories: [
+      { label: "Fishing", value: "A011" },
+      { label: "Growing of tobacco", value: "A0115" },
+      { label: "Extraction of natural gas", value: "B0520" },
+      { label: "Manufacture of watches and clocks", value: "C1052" },
+    ],
+    regions: [
+      { label: "Vestland", value: "norway-w" },
+      { label: "Rogaland", value: "norway-w2" },
+    ]
+  }
+
   return (
     <div className="flex items-center justify-between">
       <div className="flex flex-1 items-center space-x-2 h-10">
@@ -18,8 +32,8 @@ export default function TableToolbar<TData>({table, onSearch}: TableToolbarProps
           className="w-[150px] lg:w-[250px] h-full"
           onChange={(e) => onSearch(e.target.value)}
         />
-        <TableFilter title="Activity Category" />
-        <TableFilter title="Region" />
+        <TableFilter title="Activity Category" options={options.categories} selected={new Set(["A0115", "B0520"])} />
+        <TableFilter title="Region" options={options.regions} selected={new Set(["norway-w"])} />
       </div>
     </div>
   )
