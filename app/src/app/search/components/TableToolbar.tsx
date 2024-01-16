363926589257dd2d4df09a6a13681f0ee912205d
Jonas Lundeland <jonas.lundeland@tv2.no>
2024-01-16 10:03:17 +0100

    [ui] Refactor Table Filter component to take onToggle and onReset cb as props

--- app/src/app/search/components/TableToolbar.tsx
+++ app/src/app/search/components/TableToolbar.tsx
@@ -1,6 +1,7 @@
 import {Input} from "@/components/ui/input";
 import {Table} from "@tanstack/table-core";
 import {TableFilter} from "@/app/search/components/TableFilter";
+import {useState} from "react";
 
 interface TableToolbarProps<TData> {
   table: Table<TData>
@@ -11,15 +12,50 @@ export default function TableToolbar<TData>({table, onSearch}: TableToolbarProps
 
   const options = {
     categories: [
-      { label: "Fishing", value: "A011" },
-      { label: "Growing of tobacco", value: "A0115" },
-      { label: "Extraction of natural gas", value: "B0520" },
-      { label: "Manufacture of watches and clocks", value: "C1052" },
+      {label: "Fishing", value: "A011"},
+      {label: "Growing of tobacco", value: "A0115"},
+      {label: "Extraction of natural gas", value: "B0520"},
+      {label: "Manufacture of watches and clocks", value: "C1052"},
     ],
     regions: [
-      { label: "Vestland", value: "norway-w" },
-      { label: "Rogaland", value: "norway-w2" },
+      {label: "Vestland", value: "norway-w"},
+      {label: "Rogaland", value: "norway-w2"},
     ]
+  }
+
+  const [regions, setRegions] = useState<Set<string>>(new Set(["norway-w"]))
+  const [activityCategories, setActivityCategories] = useState<Set<string>>(new Set(["A0115", "B0520"]))
+
+  const resetRegions = () => {
+    setRegions(new Set())
+  }
+
+  const resetActivityCategories = () => {
+    setActivityCategories(new Set())
+  }
+
+  const toggleRegion = (option: { label: string, value: string }) => {
+    setRegions((regions) => {
+      const set = new Set(regions)
+      if (set.has(option.value)) {
+        set.delete(option.value)
+      } else {
+        set.add(option.value)
+      }
+      return set
+    })
+  }
+
+  const toggleActivityCategory = (option: { label: string, value: string }) => {
+    setActivityCategories((activityCategories) => {
+      const set = new Set(activityCategories)
+      if (set.has(option.value)) {
+        set.delete(option.value)
+      } else {
+        set.add(option.value)
+      }
+      return set
+    })
   }
 
   return (
@@ -32,8 +68,20 @@ export default function TableToolbar<TData>({table, onSearch}: TableToolbarProps
           className="w-[150px] lg:w-[250px] h-full"
           onChange={(e) => onSearch(e.target.value)}
         />
-        <TableFilter title="Activity Category" options={options.categories} selected={new Set(["A0115", "B0520"])} />
-        <TableFilter title="Region" options={options.regions} selected={new Set(["norway-w"])} />
+        <TableFilter
+          title="Activity Category"
+          options={options.categories}
+          selectedOptionValues={activityCategories}
+          onToggle={toggleActivityCategory}
+          onReset={resetActivityCategories}
+        />
+        <TableFilter
+          title="Region"
+          options={options.regions}
+          selectedOptionValues={regions}
+          onToggle={toggleRegion}
+          onReset={resetRegions}
+        />
       </div>
     </div>
   )
