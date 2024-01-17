085980264c6bd339de645888f44136edf29c6eac
Jonas Lundeland <jonas.lundeland@tv2.no>
2024-01-17 10:32:19 +0100

    [ui] Make search toolbar more compact by only showing filter values (not label)

--- app/src/app/search/components/TableToolbar.tsx
+++ app/src/app/search/components/TableToolbar.tsx
@@ -24,35 +24,33 @@ export default function TableToolbar(
   const hasFilterSelected = selectedActivityCategories.length > 0 || selectedRegions.length > 0
 
   return (
-    <div className="flex items-center justify-between">
-      <div className="flex flex-1 items-center space-x-2 h-10">
-        <Input
-          type="text"
-          id="search-prompt"
-          placeholder="Find units by name"
-          className="w-[150px] lg:w-[250px] h-full"
-          onChange={(e) => onSearch(e.target.value)}
-        />
-        <TableFilter
-          title="Activity Category"
-          options={activityCategoryOptions}
-          selectedValues={selectedActivityCategories}
-          onToggle={({value}) => dispatch({type: "toggleActivityCategory", payload: value})}
-          onReset={() => dispatch({type: "resetActivityCategories", payload: ""})}
-        />
-        <TableFilter
-          title="Region"
-          options={regionOptions}
-          selectedValues={selectedRegions}
-          onToggle={({value}) => dispatch({type: "toggleRegion", payload: value})}
-          onReset={() => dispatch({type: "resetRegions", payload: ""})}
-        />
-        {
-          hasFilterSelected && (
-            <ResetFilterButton onReset={() => dispatch({type: "reset", payload: ""})}/>
-          )
-        }
-      </div>
+    <div className="flex items-center flex-wrap space-x-2 h-10">
+      <Input
+        type="text"
+        id="search-prompt"
+        placeholder="Find units by name"
+        className="w-[150px] h-full"
+        onChange={(e) => onSearch(e.target.value)}
+      />
+      <TableFilter
+        title="Activity Category"
+        options={activityCategoryOptions}
+        selectedValues={selectedActivityCategories}
+        onToggle={({value}) => dispatch({type: "toggleActivityCategory", payload: value})}
+        onReset={() => dispatch({type: "resetActivityCategories", payload: ""})}
+      />
+      <TableFilter
+        title="Region"
+        options={regionOptions}
+        selectedValues={selectedRegions}
+        onToggle={({value}) => dispatch({type: "toggleRegion", payload: value})}
+        onReset={() => dispatch({type: "resetRegions", payload: ""})}
+      />
+      {
+        hasFilterSelected && (
+          <ResetFilterButton onReset={() => dispatch({type: "reset", payload: ""})}/>
+        )
+      }
     </div>
   )
 }
