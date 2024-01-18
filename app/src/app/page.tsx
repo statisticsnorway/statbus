de52aa0de999588bbb10314bcfa3d92b3dca4f6d
Jonas Lundeland <jonas.lundeland@tv2.no>
2024-01-18 10:12:55 +0100

    [ui] Add error logging for when search page data fetch fail serverside

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -3,23 +3,31 @@ import Search from "@/app/search/components/Search";
 
 export default async function Home() {
   const client = createClient();
-  const {data: legalUnits, count, error} = await client
+  const {data: legalUnits, count, error: legalUnitsError} = await client
     .from('legal_unit')
     .select('tax_reg_ident, name', {count: 'exact'})
     .gt('id', 0)
     .limit(10);
 
-  const {data: regions} = await client
+  const {data: regions, error: regionsError} = await client
     .from('region')
     .select()
     .gt('id', 0);
 
-  const {data: activityCategories} = await client
+  const {data: activityCategories, error: activityCategoriesError} = await client
     .from('activity_category_available')
     .select();
 
-  if (error) {
-    console.error('⚠️failed to fetch legal units', error);
+  if (legalUnitsError) {
+    console.error('⚠️failed to fetch legal units', legalUnitsError);
+  }
+
+  if (regionsError) {
+    console.error('⚠️failed to fetch regions', regionsError);
+  }
+
+  if (activityCategoriesError) {
+    console.error('⚠️failed to fetch activity categories', activityCategoriesError);
   }
 
   return (
