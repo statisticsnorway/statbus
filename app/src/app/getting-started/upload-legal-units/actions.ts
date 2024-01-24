a0171f5394ce1b14db2b89847cd157629e73ebb1
Jonas Lundeland <jonas.lundeland@tv2.no>
2024-01-24 10:02:19 +0100

    [ui] Update legal unit csv upload view
    This commit also improve error handling and logging on CSV upload fail

--- app/src/app/getting-started/upload-legal-units/actions.ts
+++ app/src/app/getting-started/upload-legal-units/actions.ts
@@ -4,15 +4,28 @@ import {setupAuthorizedFetchFn} from "@/lib/supabase/request-helper";
 
 export async function uploadLegalUnits(formData: FormData) {
   "use server";
-  const file = formData.get('regions') as File
-  const authFetch = setupAuthorizedFetchFn()
-  const response = await authFetch(`${process.env.SUPABASE_URL}/rest/v1/legal_unit_region_activity_category_stats_view`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'text/csv'
-    },
-    body: file
-  })
 
-  return response.ok ? redirect('/getting-started/summary', RedirectType.push) : { error: response.statusText }
+  try {
+    const file = formData.get('regions') as File
+    const authFetch = setupAuthorizedFetchFn()
+    const response = await authFetch(`${process.env.SUPABASE_URL}/rest/v1/legal_unit_region_activity_category_stats_current`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'text/csv'
+      },
+      body: file
+    })
+
+    if (!response.ok) {
+      const data = await response.json()
+      console.error(`legal units upload failed with status ${response.status} ${response.statusText}`)
+      console.error(data)
+      return {error: data.message}
+    }
+
+  } catch (e) {
+    return {error: 'failed to upload legal units'}
+  }
+
+  return redirect('/getting-started/summary', RedirectType.push)
 }
--- app/src/app/getting-started/upload-regions/actions.ts
+++ app/src/app/getting-started/upload-regions/actions.ts
@@ -3,17 +3,29 @@ import {redirect, RedirectType} from "next/navigation";
 import {setupAuthorizedFetchFn} from "@/lib/supabase/request-helper";
 
 export async function uploadRegions(formData: FormData) {
-    "use server";
-    const file = formData.get('regions') as File
+  "use server";
 
+  try {
+    const file = formData.get('regions') as File
     const authFetch = setupAuthorizedFetchFn()
     const response = await authFetch(`${process.env.SUPABASE_URL}/rest/v1/region_view`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'text/csv'
-        },
-        body: file
+      method: 'POST',
+      headers: {
+        'Content-Type': 'text/csv'
+      },
+      body: file
     })
 
-    return response.ok ? redirect('/getting-started/upload-legal-units', RedirectType.push) : {error: response.statusText}
+    if (!response.ok) {
+      const data = await response.json()
+      console.error(`regions upload failed with status ${response.status} ${response.statusText}`)
+      console.error(data)
+      return {error: data.message}
+    }
+
+  } catch (e) {
+    return {error: 'failed to upload regions'}
+  }
+
+  return redirect('/getting-started/upload-legal-units', RedirectType.push)
 }
