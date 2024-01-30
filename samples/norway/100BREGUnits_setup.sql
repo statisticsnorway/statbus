d0e0fa55d06e03b3f905de59986d41a44c89ec96
Jørgen H. Fjeld <jorgen@veridit.no>
2024-01-30 14:23:54 +0100

    [dev] Ensure fresh data after loading

--- samples/norway/100BREGUnits_setup.sql
+++ samples/norway/100BREGUnits_setup.sql
@@ -2,3 +2,4 @@
 \i samples/norway/activity_category/activity_category_norway.sql
 \i samples/norway/regions/norway-regions-2024.sql
 \i samples/norway/100BREGUnits.sql
+SELECT public.statistical_unit_refresh_now();
