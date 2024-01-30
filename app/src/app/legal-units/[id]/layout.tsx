bb7f1550a9be9c63e1591cb0da7f62a3e0642450
Jonas Lundeland <jonas.lundeland@tv2.no>
2024-01-30 12:53:07 +0100

    [ui] Adjust legal units settings layout for mobile viewport

--- app/src/app/legal-units/[id]/layout.tsx
+++ app/src/app/legal-units/[id]/layout.tsx
@@ -1,5 +1,4 @@
 import {Metadata} from "next"
-import Image from "next/image"
 import {Separator} from "@/components/ui/separator";
 import {SidebarNav} from "@/app/legal-units/components/sidebar-nav";
 import {ReactNode, useCallback} from "react";
@@ -14,7 +13,7 @@ interface SettingsLayoutProps {
   params: { id: string }
 }
 
-export default function SettingsLayout({children, header, params: { id }}: SettingsLayoutProps) {
+export default function SettingsLayout({children, header, params: {id}}: SettingsLayoutProps) {
 
   const createSidebarNavItems = useCallback(() => {
     return [
@@ -32,33 +31,15 @@ export default function SettingsLayout({children, header, params: { id }}: Setti
   const sidebarNavItems = createSidebarNavItems()
 
   return (
-    <>
-      <div className="md:hidden">
-        <Image
-          src="/examples/forms-light.png"
-          width={1280}
-          height={791}
-          alt="Forms"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/forms-dark.png"
-          width={1280}
-          height={791}
-          alt="Forms"
-          className="hidden dark:block"
-        />
+    <main className="py-8 px-2 md:py-24 space-y-6 max-w-5xl mx-auto">
+      {header}
+      <Separator className="my-6"/>
+      <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+        <aside className="lg:-mx-4 lg:w-1/5">
+          <SidebarNav items={sidebarNavItems}/>
+        </aside>
+        <div className="flex-1 space-y-8">{children}</div>
       </div>
-      <div className="hidden space-y-6 p-10 pb-16 md:block">
-        {header}
-        <Separator className="my-6"/>
-        <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-          <aside className="-mx-4 lg:w-1/5">
-            <SidebarNav items={sidebarNavItems}/>
-          </aside>
-          <div className="flex-1 lg:max-w-2xl">{children}</div>
-        </div>
-      </div>
-    </>
+    </main>
   )
 }
