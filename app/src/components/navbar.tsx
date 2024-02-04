3513ea38e25a3718a93dd6c4865786a0d4aa10b3
Jørgen H. Fjeld <jorgen@veridit.no>
2024-02-04 12:51:41 +0100

    [ui] Fix logo loading
    There is a system for image loading for Next.js, and it should use
    either remote or local loading.
    The remote loading breaks on deploy, and I'm susure as to why, however
    the recommended approach for loading static images is using local
    loading with static analysis.
    Ref. https://nextjs.org/docs/app/building-your-application/optimizing/images#local-images

--- app/src/components/navbar.tsx
+++ app/src/components/navbar.tsx
@@ -1,12 +1,13 @@
 import ProfileAvatar from "@/components/profile-avatar";
 import Image from "next/image";
+import logo from '@/../public/statbus-logo.png'
 
 export default function Navbar() {
   return (
     <nav className="bg-gray-100">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <Image src="/statbus-logo.png" alt="Statbus Logo" width={32} height={32} className="h-8" />
+          <Image src={logo} alt="Statbus Logo" width={32} height={32} className="h-8" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Statbus</span>
         </a>
         <div className="items-center justify-between flex w-auto order-1" id="navbar-user">
