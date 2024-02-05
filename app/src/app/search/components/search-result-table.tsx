ae4e498983d44e144c54eda7c986b060a71cad78
Jonas Lundeland <jonas.lundeland@tv2.no>
2024-02-05 12:27:47 +0100

    [ui] Auto width statistical unit link in search result

--- app/src/app/search/components/search-result-table.tsx
+++ app/src/app/search/components/search-result-table.tsx
@@ -25,11 +25,13 @@ export default function SearchResultTable({searchResult: {statisticalUnits}, reg
         {
           statisticalUnits?.map(({legal_unit_id, tax_reg_ident, name, physical_region_path, primary_activity_category_path}) => (
               <TableRow key={legal_unit_id}>
-                  <TableCell className="p-2 flex flex-col space-y-1.5">
-                      <Link href={`/legal-units/${legal_unit_id}`} className="font-medium leading-none">
-                          {name}
-                      </Link>
-                      <small className="text-gray-700 leading-none">{tax_reg_ident}</small>
+                  <TableCell className="p-2 flex flex-col">
+                      <div>
+                          <Link href={`/legal-units/${legal_unit_id}`} className="font-medium">
+                              {name}
+                          </Link>
+                      </div>
+                      <small className="text-gray-700">{tax_reg_ident}</small>
                   </TableCell>
                   <TableCell className="p-2 text-right">{getRegionByPath(physical_region_path)?.name}</TableCell>
                   <TableCell className="text-right p-2 px-4">{primary_activity_category_path as string}</TableCell>
