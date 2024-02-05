6fad887fdd00fb7610b90570d316d780762287da
Jonas Lundeland <jonas.lundeland@tv2.no>
2024-02-05 12:44:23 +0100

    [ui] Improve data loading efficiency on getting-started summary page
    No need to fetch legal units or regions on this page. All we need is information that there exists any and the count.

--- app/src/app/getting-started/summary/page.tsx
+++ app/src/app/getting-started/summary/page.tsx
@@ -4,92 +4,92 @@ import {AlertCircle, Check} from "lucide-react";
 import {createClient} from "@/lib/supabase/server";
 
 export default async function OnboardingCompletedPage() {
-  const client = createClient()
-  const {data: settings} = await client.from('settings').select('id, activity_category_standard(id,name)')
-  const {data: regions} = await client.from('region').select('id, name')
-  const {data: legalUnits} = await client.from('legal_unit').select('id, name')
+    const client = createClient()
+    const {data: settings, count: numberOfSettings} = await client.from('settings').select('id, activity_category_standard(id,name)', {count: 'exact'}).limit(1)
+    const {count: numberOfRegions} = await client.from('region').select('id', {count: 'exact'}).limit(1)
+    const {count: numberOfLegalUnits} = await client.from('legal_unit').select('id', {count: 'exact'}).limit(1)
 
-  return (
-    <div className="space-y-8">
-      <h1 className="font-medium text-lg text-center">Summary</h1>
+    return (
+        <div className="space-y-8">
+            <h1 className="font-medium text-lg text-center">Summary</h1>
 
 
-      <div className="flex items-center space-x-3">
-        <div>
-          {
-            settings?.length ? <Check/> : <AlertCircle/>
-          }
+            <div className="flex items-center space-x-3">
+                <div>
+                    {
+                        numberOfSettings ? <Check/> : <AlertCircle/>
+                    }
+                </div>
+                <p>
+                    {
+                        numberOfSettings ? (
+                            <>
+                                You have configured StatBus to use
+                                the <strong>{settings?.[0]?.activity_category_standard?.name}</strong> activity category
+                                standard.
+                            </>
+                        ) : (
+                            <>
+                                You have not configured StatBus to use an activity category standard. You can configure
+                                activity category standards&nbsp;
+                                <Link className="underline" href={"/getting-started/activity-standard"}>here</Link>
+                            </>
+                        )
+                    }
+                </p>
+            </div>
+
+            <div className="flex items-center space-x-3">
+                <div>
+                    {
+                        numberOfRegions ? <Check/> : <AlertCircle/>
+                    }
+                </div>
+                <p>
+                    {
+                        numberOfRegions ? (
+                            <>
+                                You have uploaded <strong>{numberOfRegions}</strong> regions.
+                            </>
+                        ) : (
+                            <>
+                                You have not uploaded any regions. You can upload regions&nbsp;
+                                <Link className="underline" href={"/getting-started/upload-regions"}> here</Link>
+                            </>
+                        )
+                    }
+                </p>
+            </div>
+
+            <div className="flex items-center space-x-3">
+                <div>
+                    {
+                        numberOfLegalUnits ? <Check/> : <AlertCircle/>
+                    }
+                </div>
+                <p>
+                    {
+                        numberOfLegalUnits ? (
+                            <>
+                                You have uploaded <strong>{numberOfLegalUnits}</strong> legal units.
+                            </>
+                        ) : (
+                            <>
+                                You have not uploaded any legal units. You can do so&nbsp;
+                                <Link className="underline" href={"/getting-started/upload-legal-units"}>here</Link>
+                            </>
+                        )
+                    }
+                </p>
+            </div>
+
+            {
+                numberOfSettings && numberOfRegions && numberOfLegalUnits ? (
+                    <div className="text-center">
+                        <Link className="underline" href="/">Start using StatBus</Link>
+                    </div>
+                ) : null
+            }
         </div>
-        <p>
-          {
-            settings?.length ? (
-              <>
-                You have configured StatBus to use
-                the <strong>{settings?.[0]?.activity_category_standard?.name}</strong> activity category
-                standard.
-              </>
-            ) : (
-              <>
-                You have not configured StatBus to use an activity category standard. You can configure
-                activity category standards&nbsp;
-                <Link className="underline" href={"/getting-started/activity-standard"}>here</Link>
-              </>
-            )
-          }
-        </p>
-      </div>
-
-      <div className="flex items-center space-x-3">
-        <div>
-          {
-            regions?.length ? <Check/> : <AlertCircle/>
-          }
-        </div>
-        <p>
-          {
-            regions?.length ? (
-              <>
-                You have uploaded <strong>{regions.length}</strong> regions.
-              </>
-            ) : (
-              <>
-                You have not uploaded any regions. You can upload regions&nbsp;
-                <Link className="underline" href={"/getting-started/upload-regions"}>   here</Link>
-              </>
-            )
-          }
-        </p>
-      </div>
-
-      <div className="flex items-center space-x-3">
-        <div>
-          {
-            legalUnits?.length ? <Check/> : <AlertCircle/>
-          }
-        </div>
-        <p>
-          {
-            legalUnits?.length ? (
-              <>
-                You have uploaded <strong>{legalUnits.length}</strong> legal units.
-              </>
-            ) : (
-              <>
-                You have not uploaded any legal units. You can do so&nbsp;
-                <Link className="underline" href={"/getting-started/upload-legal-units"}>here</Link>
-              </>
-            )
-          }
-        </p>
-      </div>
-
-      {
-        settings?.length && regions?.length && legalUnits?.length ? (
-          <div className="text-center">
-            <Link className="underline" href="/">Start using StatBus</Link>
-          </div>
-        ) : null
-      }
-    </div>
-  )
+    )
 }
