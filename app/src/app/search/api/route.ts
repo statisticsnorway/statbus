07ef6e4d5473a7b04ba7990545703f0252306241
Jonas Lundeland <jonas.lundeland@tv2.no>
2024-02-07 10:41:54 +0100

    [ui] Refactor search server route function
    Extract data fetch method for cleaner GET request handler

--- app/src/app/search/api/route.ts
+++ app/src/app/search/api/route.ts
@@ -1,12 +1,19 @@
 import {NextResponse} from "next/server";
 import {setupAuthorizedFetchFn} from "@/lib/supabase/request-helper";
 
+async function getStatisticalUnits(searchParams: URLSearchParams) {
+    const authFetch = setupAuthorizedFetchFn()
+    return await authFetch(`${process.env.SUPABASE_URL}/rest/v1/statistical_unit?${searchParams}`, {
+        method: 'GET',
+        headers: {
+            'Prefer': 'count=exact',
+            'Range-Unit': 'items'
+        },
+    });
+}
+
 export async function GET(request: Request) {
     const {searchParams} = new URL(request.url)
-
-    if (!searchParams.has('limit')) {
-        searchParams.set('limit', '10')
-    }
 
     if (!searchParams.has('order')) {
         searchParams.set('order', 'enterprise_id.desc')
@@ -16,20 +23,17 @@ export async function GET(request: Request) {
         searchParams.set('select', 'name, tax_reg_ident, primary_activity_category_path, legal_unit_id, physical_region_path')
     }
 
-    const authFetch = setupAuthorizedFetchFn()
-    const response = await authFetch(`${process.env.SUPABASE_URL}/rest/v1/statistical_unit?${searchParams}`, {
-        method: 'GET',
-        headers: {
-            'Prefer': 'count=exact',
-            'Range-Unit': 'items'
-        },
-    })
-
-    if (!response.ok) {
-        return NextResponse.json({error: response.statusText})
+    if (!searchParams.has('limit')) {
+        searchParams.set('limit', '10')
     }
 
-    const statisticalUnits = await response.json()
-    const count = response.headers.get('content-range')?.split('/')[1]
+    const statisticalUnitsResponse = await getStatisticalUnits(searchParams);
+
+    if (!statisticalUnitsResponse.ok) {
+        return NextResponse.json({error: statisticalUnitsResponse.statusText})
+    }
+
+    const statisticalUnits = await statisticalUnitsResponse.json()
+    const count = statisticalUnitsResponse.headers.get('content-range')?.split('/')[1]
     return NextResponse.json({statisticalUnits, count: parseInt(count ?? '-1', 10)})
 }
