6c5eaa1faf33b37ad13428b32d35e33f714e854c
Jonas Lundeland <jonas.lundeland@tv2.no>
2024-02-07 13:08:29 +0100

    [ui] Refactor csv-utils
    Set readonly attributes and rename function escapeCSVString

--- app/src/lib/csv-utils.ts
+++ app/src/lib/csv-utils.ts
@@ -1,14 +1,14 @@
-function escapeCSVStringValue(str: string) {
+function escapeCSVString(str: string) {
     let value = str.replace(/"/g, '""')
     return /[,\n"]/g.test(value) ? `"${value}"` : value
 }
 
-export function toCSV(entries: Object[]) {
+export function toCSV(entries: readonly Object[]) {
     const header = Object.keys(entries[0]).join(',') + '\n'
 
     const body = entries
         .map(entry =>
-            Object.values(entry).map(val => typeof val === 'string' ? escapeCSVStringValue(val) : val).join(',')
+            Object.values(entry).map(val => typeof val === 'string' ? escapeCSVString(val) : val).join(',')
         ).join('\n')
 
     return {header, body}
