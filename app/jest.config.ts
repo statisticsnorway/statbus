9c571234887296683dd5ee26ca11c70fccb8dab6
Jonas Lundeland <jonas.lundeland@tv2.no>
2024-02-08 10:27:08 +0100

    [ui] Configure jest preset and environment
    The following configurations were set in jest.config.ts:
    
    ```
    {
      testEnvironment: "node",
      preset: 'ts-jest'
    }
    ```

--- app/jest.config.ts
+++ app/jest.config.ts
@@ -146,7 +146,7 @@ const config: Config = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: "jsdom",
+  testEnvironment: "node",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
