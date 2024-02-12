195aef87950fc05f9e3d0be9190449da09a79fcf
JÃ¸rgen H. Fjeld <jorgen@veridit.no>
2024-02-12 14:20:38 +0100

    [dev] Add helper to quickly load sample data

---
+++ samples/norway/brreg-samples.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+set -e # Exit on any failure for any command
+
+if test -n "$DEBUG"; then
+  set -x # Print all commands before running them - for easy debugging.
+fi
+
+WORKSPACE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../.. && pwd )"
+
+pushd $WORKSPACE
+
+source supabase_docker/.env
+export PGHOST=localhost
+export PGPORT=$DB_PUBLIC_LOCALHOST_PORT
+export PGDATABASE=$POSTGRES_DB
+export PGUSER=postgres
+export PGPASSWORD="$POSTGRES_PASSWORD"
+echo "Setting up Statbus for Norway"
+psql < samples/norway/setup.sql
+
+pushd cli
+echo "Loading legal_units"
+shards build && time ./bin/statbus import legal_unit -f ../samples/norway/enheter-selection-1000.csv --config ../samples/norway/enheter-mapping.json --strategy insert
+echo "Loading establishments"
+shards build && time ./bin/statbus import establishment -f ../samples/norway/underenheter-selection-1000.csv --config ../samples/norway/underenheter-mapping.json --strategy insert
+popd
+
+popd
