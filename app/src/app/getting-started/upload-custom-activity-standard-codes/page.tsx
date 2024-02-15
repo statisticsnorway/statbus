6ed287fbd0d1d1ba7e1cd67e8c15d8fcb61a43a7
Jonas Lundeland <jonas.lundeland@tv2.no>
2024-02-15 14:48:03 +0100

    [ui] Provide example CSV file for custom activity code upload

--- app/src/app/getting-started/upload-custom-activity-standard-codes/page.tsx
+++ app/src/app/getting-started/upload-custom-activity-standard-codes/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {InfoBox} from "@/components/info-box";
 import {createClient} from "@/lib/supabase/server";
 import {UploadCSVForm} from "@/app/getting-started/upload-csv-form";
+import {Accordion, AccordionContent, AccordionItem, AccordionTrigger} from "@/components/ui/accordion";
 
 export default async function UploadCustomActivityCategoryCodesPage() {
     const client = createClient()
@@ -23,10 +24,44 @@ export default async function UploadCustomActivityCategoryCodesPage() {
                     </InfoBox>
                 ) : null
             }
+
             <UploadCSVForm
                 uploadView="activity_category_available_custom"
                 nextPage="/getting-started/upload-legal-units"
             />
+
+            <Accordion type="single" collapsible>
+                <AccordionItem value="Legal Unit">
+                    <AccordionTrigger>What is a Custom Activity Category Code?</AccordionTrigger>
+                    <AccordionContent>
+                        <p className="mb-3">
+                            A <i>Custom Activity Category Code</i> is a code that you can use to categorize economic
+                            activities and units for statistical and analytical purposes. It is a category code
+                            that is not part of the standard classification systems like NACE or ISIC and may be
+                            specific to your country.
+                        </p>
+                    </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="Legal Units File">
+                    <AccordionTrigger>What is a Custom Activity Categories file?</AccordionTrigger>
+                    <AccordionContent>
+                        <p className="mb-3">
+                            A Custom Activity Categories file is a CSV file containing the Custom Activity Categories
+                            you want to use in your
+                            analysis. The file must conform to a specific format in order to be processed correctly.
+                            Have a look at this example
+                            CSV file to get an idea of how the file should be structured:
+                        </p>
+                        <a
+                            href="/activity_category_norway.csv"
+                            download="custom_activity_categories.example.csv"
+                            className="underline"
+                        >
+                            Download example CSV file
+                        </a>
+                    </AccordionContent>
+                </AccordionItem>
+            </Accordion>
         </section>
     )
 }
