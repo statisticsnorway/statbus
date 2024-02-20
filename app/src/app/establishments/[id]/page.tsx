b59004f037ee8e4ee6aa0e31d5b3771785051722
Jonas Lundeland <jonas.lundeland@tv2.no>
2024-02-20 12:49:28 +0100

    [ui] Add skeleton pages for enterprise, establishment and enterprise group details

---
+++ app/src/app/establishments/[id]/page.tsx
@@ -0,0 +1,8 @@
+export default function EstablishmentDetailsPage() {
+    return (
+        <div className="p-20 mx-auto text-center">
+            <h1>Establishment Details</h1>
+            <small>Coming soon</small>
+        </div>
+    )
+}
