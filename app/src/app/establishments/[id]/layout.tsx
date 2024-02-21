666ae6d2899be736f9eadb830bcfaf5bc21726c1
Jonas Lundeland <jonas.lundeland@tv2.no>
2024-02-21 10:28:25 +0100

    [ui] Reduce code duplication with common header layout between details pages

--- app/src/app/establishments/[id]/layout.tsx
+++ app/src/app/establishments/[id]/layout.tsx
@@ -1,33 +1,14 @@
 import {Metadata} from "next"
-import {ReactNode} from "react";
+import {DetailsPageLayout, DetailsPageLayoutProps} from "@/components/statistical-unit-details/details-page-layout";
 
 export const metadata: Metadata = {
-    title: "Details"
+  title: "Details"
 }
 
-interface SettingsLayoutProps {
-    readonly children: ReactNode,
-    readonly header: ReactNode,
-    readonly topology: ReactNode,
-    readonly nav: ReactNode,
-    readonly params: { id: string }
-}
-
-export default function SettingsLayout({children, header, topology, nav}: SettingsLayoutProps) {
-    return (
-        <main className="py-8 px-2 md:py-24 space-y-6 w-full max-w-5xl mx-auto">
-            {header}
-            <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-                <aside className="lg:w-4/12">
-                    {nav}
-                    <div className="p-2 mt-6">
-                        {topology}
-                    </div>
-                </aside>
-                <div className="flex-1">{children}</div>
-            </div>
-        </main>
-    )
+export default function SettingsLayout({children, header, topology, nav}: DetailsPageLayoutProps) {
+  return (
+    <DetailsPageLayout header={header} nav={nav} topology={topology}>{children}</DetailsPageLayout>
+  )
 }
 
 
---
+++ app/src/components/statistical-unit-details/details-page-layout.tsx
@@ -0,0 +1,23 @@
+import {ReactNode} from "react";
+
+export interface DetailsPageLayoutProps {
+  readonly children: ReactNode,
+  readonly header: ReactNode,
+  readonly topology: ReactNode,
+  readonly nav: ReactNode
+}
+
+export const DetailsPageLayout = ({children, header, topology, nav}: DetailsPageLayoutProps) => (
+  <main className="py-8 px-2 md:py-24 space-y-6 w-full max-w-5xl mx-auto">
+    {header}
+    <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+      <aside className="lg:w-4/12">
+        {nav}
+        <div className="p-2 mt-6">
+          {topology}
+        </div>
+      </aside>
+      <div className="flex-1">{children}</div>
+    </div>
+  </main>
+)
