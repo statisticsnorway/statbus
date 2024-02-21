4373157473b328b747b67475daacaeb1f1ea0d29
Jonas Lundeland <jonas.lundeland@tv2.no>
2024-02-21 13:31:35 +0100

    [ui] Add sub pages to establishment details page

--- app/src/app/establishments/[id]/contact/page.tsx
+++ app/src/app/establishments/[id]/contact/page.tsx
@@ -2,7 +2,7 @@ import {getEstablishmentById} from "@/app/establishments/[id]/establishment-requ
 import {DetailsPage} from "@/components/statistical-unit-details/details-page";
 import {notFound} from "next/navigation";
 
-export default async function EstablishmentGeneralInfoPage({params: {id}}: { readonly params: { id: string } }) {
+export default async function EstablishmentContactPage({params: {id}}: { readonly params: { id: string } }) {
   const unit = await getEstablishmentById(id);
 
   if (!unit) {
@@ -10,9 +10,9 @@ export default async function EstablishmentGeneralInfoPage({params: {id}}: { rea
   }
 
   return (
-    <DetailsPage title="General Info" subtitle="General establishment information such as name, sector">
+    <DetailsPage title="Contact Info" subtitle="Contact information such as email, phone, and addresses">
       <p className="bg-gray-50 p-12 text-sm text-center">
-        This section will show general information for {unit.name}
+        This section will show the contact information for {unit.name}
       </p>
     </DetailsPage>
   )
