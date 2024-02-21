d4b3a74bc8010c7a89769c31d7ae71783dbcca60
Jonas Lundeland <jonas.lundeland@tv2.no>
2024-02-21 10:52:31 +0100

    [ui] Refactor Topology components

--- app/src/components/statistical-unit-hierarchy/topology.tsx
+++ app/src/components/statistical-unit-hierarchy/topology.tsx
@@ -2,36 +2,34 @@ import {TopologyItem} from "@/components/statistical-unit-hierarchy/topology-ite
 import {StatisticalUnitHierarchy} from "@/components/statistical-unit-hierarchy/statistical-unit-hierarchy-types";
 
 interface TopologyProps {
-    readonly hierarchy: StatisticalUnitHierarchy;
-    readonly unitId: number;
-    readonly unitType: 'legal_unit' | 'establishment' | 'enterprise' | 'enterprise_group';
+  readonly hierarchy: StatisticalUnitHierarchy;
+  readonly unitId: number;
+  readonly unitType: 'legal_unit' | 'establishment' | 'enterprise' | 'enterprise_group';
 }
 
 export function Topology({hierarchy, unitId, unitType}: TopologyProps) {
-    return (
-        <ul className="text-xs">
-            {
-                hierarchy?.enterprise.legal_unit.map((legalUnit) => (
-                    <TopologyItem
-                        key={legalUnit.id}
-                        id={legalUnit.id}
-                        active={legalUnit.id === unitId && unitType === 'legal_unit'}
-                        name={legalUnit.name}
-                        type="legal_unit"
-                    >
-                        {legalUnit.establishment.map((establishment) =>
-                            <TopologyItem
-                                key={establishment.id}
-                                id={establishment.id}
-                                active={establishment.id === unitId && unitType === 'establishment'}
-                                name={establishment.name}
-                                type="establishment"
-                            />
-                        )}
-                    </TopologyItem>
-                ))
-            }
-        </ul>
-    )
+  return (
+    <ul className="text-xs">
+      {
+        hierarchy?.enterprise.legal_unit.map((legalUnit) => (
+          <TopologyItem
+            key={legalUnit.id}
+            unit={legalUnit}
+            type="legal_unit"
+            active={legalUnit.id === unitId && unitType === 'legal_unit'}
+          >
+            {legalUnit.establishment.map((establishment) =>
+              <TopologyItem
+                key={establishment.id}
+                unit={establishment}
+                active={establishment.id === unitId && unitType === 'establishment'}
+                type="establishment"
+              />
+            )}
+          </TopologyItem>
+        ))
+      }
+    </ul>
+  )
 }
 
