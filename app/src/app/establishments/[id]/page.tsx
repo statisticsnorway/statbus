17105e86bc0337a1294c240fa33d2fa80ca1174c
Jonas Lundeland <jonas.lundeland@tv2.no>
2024-02-21 12:46:56 +0100

    [ui] Add reusable DetailsPage component

--- app/src/app/establishments/[id]/page.tsx
+++ app/src/app/establishments/[id]/page.tsx
@@ -1,21 +1,19 @@
 import {getEstablishmentById} from "@/app/establishments/[id]/establishment-requests";
 import DataDump from "@/components/data-dump";
-import {Separator} from "@/components/ui/separator";
+import {DetailsPage} from "@/components/statistical-unit-details/details-page";
+import {notFound} from "next/navigation";
 
 export default async function EstablishmentDetailsPage({params: {id}}: { readonly params: { id: string } }) {
   const unit = await getEstablishmentById(id);
+
+  if (!unit) {
+    notFound()
+  }
+
   return (
-    <div className="space-y-6">
-      <div>
-        <h3 className="text-lg font-medium">Data dump</h3>
-        <p className="text-sm text-muted-foreground">
-          This section shows the raw data we have on {unit?.name}.
-        </p>
-      </div>
-      <Separator/>
-      {
-        unit != null ? <DataDump data={unit}/> : null
-      }
-    </div>
+    <DetailsPage title="Data dump" subtitle={`This section shows the raw data we have on ${unit?.name}`}>
+      <DataDump data={unit}/>
+    </DetailsPage>
   )
 }
+
