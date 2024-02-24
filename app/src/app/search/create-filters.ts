e70435c24a3f0d2ec985d8a43903be30c5bb59c5
Jonas Lundeland <jonas.lundeland@tv2.no>
2024-02-24 16:03:47 +0100

    [ui] Specify nullslast when sorting search results desc
    Default behavior is null values first when sorting desc, which from a user point of view does not make much sense.

--- app/src/app/search/create-filters.ts
+++ app/src/app/search/create-filters.ts
@@ -124,16 +124,26 @@ export function createFilters(opts: FilterOptions, urlSearchParams: URLSearchPar
       },
       {
         label: "Employees (Descending)",
-        value: "employees.desc",
+        value: "employees.desc.nullslast",
         humanReadableValue: "Employees (Descending)"
       },
       {
         label: "Employees (Ascending)",
         value: "employees.asc",
         humanReadableValue: "Employees (Ascending)"
+      },
+      {
+        label: "Activity (Ascending)",
+        value: "primary_activity_category_path.asc",
+        humanReadableValue: "Activity | Ascending"
+      },
+      {
+        label: "Activity (Descending)",
+        value: "primary_activity_category_path.desc.nullslast",
+        humanReadableValue: "Activity | Descending"
       }
     ],
-    selected: urlSearchParams?.has('order') ? [urlSearchParams?.get('order') as string] : ["employees.desc"],
+    selected: urlSearchParams?.has('order') ? [urlSearchParams?.get('order') as string] : ["employees.desc.nullslast"],
   }
 
   return [standardFilters, statisticalVariableFilters, [sortFilter]]
