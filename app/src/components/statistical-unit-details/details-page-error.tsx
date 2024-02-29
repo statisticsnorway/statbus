82885f8354c7adb489d1462dd59fa1f2577e32f3
Jonas Lundeland <jonas.lundeland@tv2.no>
2024-02-29 09:43:17 +0100

    [ui] Display error message, stack and digest whenever details page fail
    Also throw correct error type. Previously the PostgrestError was rethrown whenever stat unit details fetch failed. This did not serialize on error page. Now we instead throw a new Error, passing the postgres message as message and root cause.

--- app/src/components/statistical-unit-details/details-page-error.tsx
+++ app/src/components/statistical-unit-details/details-page-error.tsx
@@ -1,5 +1,4 @@
 import {DetailsPage} from "@/components/statistical-unit-details/details-page";
-import DataDump from "@/components/data-dump";
 
 interface ErrorPageParams {
   readonly error: Error & { digest?: string };
@@ -7,8 +6,28 @@ interface ErrorPageParams {
 
 export default function DetailsPageError({error}: ErrorPageParams) {
   return (
-    <DetailsPage title="This is what happened" subtitle="The following error happened while trying to get statistical unit information">
-      <DataDump data={error}/>
+    <DetailsPage
+      title="This is what happened"
+      subtitle="The following error happened while trying to get statistical unit information"
+    >
+      <div className="bg-red-100 p-6 space-y-6">
+        {
+          error.message && <ErrorSection title="Error Message" body={error.message}/>
+        }
+        {
+          error.stack && <ErrorSection title="Stack" body={error.stack}/>
+        }
+        {
+          error.digest && <ErrorSection title="Digest" body={error.digest}/>
+        }
+      </div>
     </DetailsPage>
   )
 }
+
+const ErrorSection = ({title, body}: { title: string, body: string }) => (
+  <div>
+    <h2 className="text-xs font-semibold uppercase mb-0.5">{title}:</h2>
+    <p>{body}</p>
+  </div>
+)
