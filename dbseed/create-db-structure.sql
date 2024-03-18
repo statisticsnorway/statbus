787a21e59ee50012b31c653a7917e5baa59029e1
JÃ¸rgen H. Fjeld <jorgen@veridit.no>
2024-03-18 23:00:22 +0100

    [api,db] Make connect_legal_unit_to_enterprise idempotent

--- dbseed/create-db-structure.sql
+++ dbseed/create-db-structure.sql
@@ -2096,7 +2096,7 @@ END;
 $$;
 
 
-CREATE OR REPLACE FUNCTION public.connect_legal_unit_to_enterprise(
+CREATE FUNCTION public.connect_legal_unit_to_enterprise(
     legal_unit_id integer,
     enterprise_id integer,
     valid_from date DEFAULT current_date,
@@ -2110,11 +2110,14 @@ DECLARE
     deleted_enterprise_id integer := NULL;
     is_primary BOOLEAN;
     other_legal_units_count INTEGER;
+    new_primary_legal_unit_id INTEGER;
 BEGIN
+    -- Check if the enterprise exists
     IF NOT EXISTS(SELECT 1 FROM public.enterprise WHERE id = enterprise_id) THEN
         RAISE EXCEPTION 'Enterprise does not exist.';
     END IF;
 
+    -- Retrieve current enterprise_id and if it's primary
     SELECT lu.enterprise_id, lu.primary_for_enterprise INTO old_enterprise_id, is_primary
     FROM public.legal_unit AS lu
     WHERE lu.id = legal_unit_id;
@@ -2122,14 +2125,33 @@ BEGIN
         RAISE EXCEPTION 'Legal Unit does not exist.';
     END IF;
 
-    -- Check if there are other legal units and none of them are primary
+    -- Idempotency check: If the legal unit is already connected to the given enterprise with the same parameters, do nothing.
+    IF old_enterprise_id = enterprise_id THEN
+        RETURN jsonb_build_object(
+            'message', 'No changes made as the legal unit is already connected to the specified enterprise.',
+            'legal_unit_id', legal_unit_id,
+            'enterprise_id', enterprise_id
+        );
+    END IF;
+
+    -- Check if there are other legal units and if none of them are primary
     IF is_primary THEN
         SELECT COUNT(*) INTO other_legal_units_count
         FROM public.legal_unit
-        WHERE legal_unit.enterprise_id = old_enterprise_id
-        AND legal_unit.id <> legal_unit_id;
+        WHERE enterprise_id = old_enterprise_id
+          AND id <> legal_unit_id;
 
-        IF other_legal_units_count > 0 THEN
+        -- If there is only one other legal unit, set it to primary.
+        IF other_legal_units_count = 1 THEN
+            SELECT id INTO new_primary_legal_unit_id
+            FROM public.legal_unit
+            WHERE enterprise_id = old_enterprise_id
+              AND id <> legal_unit_id;
+
+            UPDATE public.legal_unit
+            SET primary_for_enterprise = true
+            WHERE id = new_primary_legal_unit_id;
+        ELSIF other_legal_units_count > 1 THEN
             RAISE EXCEPTION 'Assign another primary legal_unit to existing enterprise first';
         END IF;
     END IF;
