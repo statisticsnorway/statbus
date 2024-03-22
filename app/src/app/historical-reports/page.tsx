1c445ca0104043911beacdd99c561741ebf42af7
Hedda Hubertz <hhz@ssb.no>
2024-03-22 12:19:41 +0100

    [ui] Add new history page

---
+++ app/src/app/historical-reports/page.tsx
@@ -0,0 +1,7 @@
+export default async function HistoryPage() {
+  return (
+    <main className="flex flex-col items-center justify-between px-2 py-8 md:py-24">
+      <h1>History</h1>
+    </main>
+  );
+}
