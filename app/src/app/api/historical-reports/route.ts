ff9dad992ada8e53cae5d6d7e31adf75022d934a
Hedda Hubertz <hhz@ssb.no>
2024-03-26 09:45:31 +0100

    [ui] Add drilldown functionality (year to year-month)

--- app/src/app/api/historical-reports/route.ts
+++ app/src/app/api/historical-reports/route.ts
@@ -1,80 +1,31 @@
 import { createClient } from "@/lib/supabase/server";
 import { NextResponse } from "next/server";
 
-const data = [
-  {
-    id: "2014",
-    name: "Establishments in 2014",
-    data: [120, 130, 125, 135, 140, 145, 150, 155, 160, 165, 170, 175],
-  },
-  {
-    id: "2015",
-    name: "Establishments in 2015",
-    data: [180, 185, 180, 175, 170, 165, 160, 155, 150, 145, 140, 135],
-  },
-  {
-    id: "2016",
-    name: "Establishments in 2016",
-    data: [130, 135, 140, 145, 150, 155, 160, 165, 170, 175, 180, 185],
-  },
-  {
-    id: "2017",
-    name: "Establishments in 2017",
-    data: [190, 185, 180, 175, 170, 165, 160, 155, 150, 145, 140, 135],
-  },
-  {
-    id: "2018",
-    name: "Establishments in 2018",
-    data: [130, 135, 130, 125, 120, 115, 110, 105, 100, 95, 90, 85],
-  },
-  {
-    id: "2019",
-    name: "Establishments in 2019",
-    data: [90, 95, 100, 105, 110, 115, 120, 125, 130, 135, 140, 145],
-  },
-  {
-    id: "2020",
-    name: "Establishments in 2020",
-    data: [150, 155, 160, 165, 170, 175, 180, 185, 190, 195, 200, 205],
-  },
-  {
-    id: "2021",
-    name: "Establishments in 2021",
-    data: [210, 205, 200, 195, 190, 185, 180, 175, 170, 165, 160, 155],
-  },
-  {
-    id: "2022",
-    name: "Establishments in 2022",
-    data: [150, 155, 160, 165, 170, 160, 150, 140, 130, 120, 110, 100],
-  },
-  {
-    id: "2023",
-    name: "Establishments in 2023",
-    data: [100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210],
-  },
-  {
-    id: "2024",
-    name: "Establishments in 2024",
-    data: [215, 210, 205, 200, 195, 190, 185, 180, 175, 170, 165, 160],
-  },
-];
-
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const client = createClient();
 
-  const year = searchParams.get("year");
+  let query = client.from("statistical_history").select();
 
-  const history = await client
-    .from("statistical_history")
-    .select("count(),primary_activity_category_path")
-    .eq("year", year);
-  console.log(history.data);
+  if (searchParams.has("year")) {
+    query = query.eq("year", searchParams.get("year"));
+  }
+  if (searchParams.has("unit_type")) {
+    query = query.eq("unit_type", searchParams.get("unit_type"));
+  }
+
+  if (searchParams.has("type")) {
+    query = query.eq("type", searchParams.get("type"));
+  }
+
+  const history = await query;
+
   const mappedHistoryData = history.data?.map(
-    // @ts-ignore
-    ({ count, primary_activity_category_path }) => ({
-      name: primary_activity_category_path,
+    ({ count, type, year, month }) => ({
+      type: type,
+      year: year,
       y: count,
+      name: `${year}${month ? `-${month}` : ""}`,
     })
   );
   return NextResponse.json(mappedHistoryData);
--- app/src/app/historical-reports/historical-report-chart.tsx
+++ app/src/app/historical-reports/historical-report-chart.tsx
@@ -1,21 +1,16 @@
 "use client";
 import { chart } from "highcharts";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { OptionsFilter } from "../search/components/options-filter";
-import useSWR, { Fetcher } from "swr";
-
-const fetcher: Fetcher<any[], string> = (...args) =>
-  fetch(...args).then((res) => res.json());
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+import useHistoricalData from "./use-historical-data";
 
 export const HistoricalReportChart = () => {
   const ref = useRef<HTMLDivElement>(null);
 
-  const [year, setYear] = useState<string | null>("2024");
-
-  const { data } = useSWR<any[]>(
-    `/api/historical-reports?year=${year}`,
-    fetcher
-  );
+  const { data, year, setYear, unitType, setUnitType, type, setType } =
+    useHistoricalData();
 
   useEffect(() => {
     if (ref.current) {
@@ -23,10 +18,42 @@ export const HistoricalReportChart = () => {
         chart: {
           renderTo: ref.current,
         },
+        xAxis: {
+          type: "category",
+        },
+        tooltip: {
+          headerFormat: "",
+          pointFormat: "{point.name}: <b>{point.y}</b>",
+          outside: true,
+          useHTML: true,
+        },
+        plotOptions: {
+          series: {
+            borderWidth: 0,
+            dataLabels: {
+              enabled: true,
+              format: "{point.y}",
+            },
+          },
+        },
+        credits: {
+          href: "",
+        },
         series: [
           {
             type: "bar",
+            showInLegend: false,
             data: data,
+            point: {
+              events: {
+                click: (e) => {
+                  // console.log(e);
+                  setType("year-month");
+                  // @ts-ignore
+                  setYear(`${e.point.options.year}`);
+                },
+              },
+            },
           },
         ],
       });
@@ -44,9 +71,58 @@ export const HistoricalReportChart = () => {
         selectedValues={[year]}
         onToggle={(option) => {
           setYear(option.value);
+          setType("year-month");
         }}
         onReset={() => {}}
       />
+      <OptionsFilter
+        title="Unit type"
+        options={[
+          {
+            label: "Enterprise",
+            value: "enterprise",
+            humanReadableValue: "Enterprise",
+            className: "bg-enterprise-100",
+          },
+          {
+            label: "Legal Unit",
+            value: "legal_unit",
+            humanReadableValue: "Legal Unit",
+            className: "bg-legal_unit-100",
+          },
+          {
+            label: "Establishment",
+            value: "establishment",
+            humanReadableValue: "Establishment",
+            className: "bg-establishment-100",
+          },
+        ]}
+        selectedValues={[unitType]}
+        onToggle={(option) => {
+          setUnitType(option.value);
+        }}
+        onReset={() => {}}
+      />
+      <div className="flex flex-wrap">
+        <Button
+          size="sm"
+          variant="ghost"
+          onClick={() => {
+            setType("year");
+            setYear(null);
+          }}
+        >
+          All years
+        </Button>
+        {year && (
+          <div className="flex items-center space-x-2">
+            <ArrowRight size={18} />
+            <Button size="sm" variant="ghost">
+              {year}
+            </Button>
+          </div>
+        )}
+      </div>
       <div ref={ref} />
     </section>
   );
