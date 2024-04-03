a04b93da47608389430280194ab0e1638f979ad4
Jonas Lundeland <jonas.lundeland@tv2.no>
2024-04-03 10:45:54 +0200

    [ui, refactor] Wrap dashboard cards in Suspense boundaries

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -3,13 +3,13 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
   AlertTriangle,
   BarChart3,
-  Building,
   Globe2,
+  Loader,
   ScrollText,
   Settings,
 } from "lucide-react";
 import { createClient } from "@/lib/supabase/server";
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { StatisticalUnitIcon } from "@/components/statistical-unit-icon";
@@ -19,167 +19,282 @@ export const metadata: Metadata = {
 };
 
 export default async function Dashboard() {
-  const client = createClient();
-
-  function countByUnitType(
-    unitType: "enterprise" | "legal_unit" | "establishment"
-  ) {
-    return client
-      .from("statistical_unit")
-      .select("", { count: "exact" })
-      .eq("unit_type", unitType)
-      .limit(0);
-  }
-
-  const [
-    { count: enterpriseCount, error: enterpriseError },
-    { count: legalUnitCount, error: legalUnitError },
-    { count: establishmentCount, error: establishmentError },
-    { count: invalidCodesCount, error: invalidCodesError },
-    { count: missingRegionCount, error: missingRegionError },
-    { count: missingACCount, error: missingACError },
-    { count: regionsCount, error: regionsError },
-    { count: statisticalVariablesCount, error: statisticalVariablesError },
-    { count: customACCount, error: customACError },
-    { data: settings, error: settingsError },
-  ] = await Promise.all([
-    countByUnitType("enterprise"),
-    countByUnitType("legal_unit"),
-    countByUnitType("establishment"),
-    client
-      .from("statistical_unit")
-      .select("", { count: "exact" })
-      .not("invalid_codes", "is", null)
-      .limit(0),
-    client
-      .from("statistical_unit")
-      .select("", { count: "exact" })
-      .is("physical_region_path", null)
-      .limit(0),
-    client
-      .from("statistical_unit")
-      .select("", { count: "exact" })
-      .is("primary_activity_category_path", null)
-      .limit(0),
-    client.from("region").select("", { count: "exact" }).limit(0),
-    client.from("stat_definition").select("", { count: "exact" }).limit(0),
-    client
-      .from("activity_category_available_custom")
-      .select("", { count: "exact" })
-      .limit(0),
-    client
-      .from("settings")
-      .select("activity_category_standard(id,name)")
-      .single(),
-  ]);
-
   return (
-    <main className="mx-auto flex max-w-5xl flex-col px-2 py-8 md:py-24">
+    <main className="mx-auto flex max-w-5xl flex-col px-2 py-8 md:py-24 w-full">
       <h1 className="mb-12 text-center text-2xl">StatBus Status Dashboard</h1>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
         <Link href="/search?unit_type=enterprise">
-          <DashboardCard
-            title="Enterprises"
-            icon={<StatisticalUnitIcon type="enterprise" className="h-4" />}
-            text={enterpriseCount?.toString() ?? "-"}
-            failed={!!enterpriseError}
-          />
+          <Suspense fallback={<FallBackCard title="Enterprises" />}>
+            <StatisticalUnitCountCard
+              unitType="enterprise"
+              title="Enterprises"
+            />
+          </Suspense>
         </Link>
 
         <Link href="/search?unit_type=legal_unit">
-          <DashboardCard
-            title="Legal Units"
-            icon={<StatisticalUnitIcon type="legal_unit" className="h-4" />}
-            text={legalUnitCount?.toString() ?? "-"}
-            failed={!!legalUnitError}
-          />
+          <Suspense fallback={<FallBackCard title="Legal Units" />}>
+            <StatisticalUnitCountCard
+              unitType="legal_unit"
+              title="Legal Units"
+            />
+          </Suspense>
         </Link>
 
         <Link href="/search?unit_type=establishment">
-          <DashboardCard
-            title="Establishments"
-            icon={<StatisticalUnitIcon type="establishment" className="h-4" />}
-            text={establishmentCount?.toString() ?? "-"}
-            failed={!!establishmentError}
-          />
+          <Suspense fallback={<FallBackCard title="Establishments" />}>
+            <StatisticalUnitCountCard
+              unitType="establishment"
+              title="Establishments"
+            />
+          </Suspense>
         </Link>
 
-        <DashboardCard
-          title="Regions"
-          icon={<Globe2 className="h-4" />}
-          text={regionsCount?.toString() ?? "-"}
-          failed={!!regionsError}
-        />
+        <Suspense fallback={<FallBackCard title="Regions" />}>
+          <RegionCard />
+        </Suspense>
 
         <Link href="/getting-started/activity-standard">
-          <DashboardCard
-            title="Activity Category Standard"
-            icon={<ScrollText className="h-4" />}
-            text={settings?.activity_category_standard?.name ?? "-"}
-            failed={!!settingsError}
-          />
+          <Suspense
+            fallback={<FallBackCard title="Activity Category Standard" />}
+          >
+            <ActivityCategoryCard />
+          </Suspense>
         </Link>
 
-        <DashboardCard
-          title="Statistical Variables"
-          icon={<BarChart3 className="h-4" />}
-          text={statisticalVariablesCount?.toString() ?? "-"}
-          failed={!!statisticalVariablesError}
-        />
+        <Suspense fallback={<FallBackCard title="Statistical Variables" />}>
+          <StatisticalVariableCountCard />
+        </Suspense>
 
         <Link href="/getting-started/upload-custom-activity-standard-codes">
-          <DashboardCard
-            title="Custom Activity Category Codes"
-            icon={<Settings className="h-4" />}
-            text={customACCount?.toString() ?? "-"}
-            failed={!!customACError}
-          />
+          <Suspense
+            fallback={<FallBackCard title="Custom Activity Category Codes" />}
+          >
+            <CustomActivityCategoryCard />
+          </Suspense>
         </Link>
 
         <Link href="/search?unit_type=enterprise,legal_unit,establishment&physical_region_path=null">
-          <DashboardCard
-            title="Units Missing Region"
-            icon={<AlertTriangle className="h-4" />}
-            text={missingRegionCount?.toString() ?? "-"}
-            failed={(missingRegionCount ?? 0) > 0 || !!missingRegionError}
-          />
+          <Suspense fallback={<FallBackCard title="Units Missing Region" />}>
+            <MissingRegionCard />
+          </Suspense>
         </Link>
 
         <Link href="/search?unit_type=enterprise,legal_unit,establishment&primary_activity_category_path=null">
-          <DashboardCard
-            title="Units Missing Activity Category"
-            icon={<AlertTriangle className="h-4" />}
-            text={missingACCount?.toString() ?? "-"}
-            failed={(missingACCount ?? 0) > 0 || !!missingACError}
-          />
+          <Suspense
+            fallback={<FallBackCard title="Units Missing Activity Category" />}
+          >
+            <MissingActivityCategoryCard />
+          </Suspense>
         </Link>
 
         <Link href="/search?unit_type=enterprise,legal_unit,establishment&invalid_codes=yes">
-          <DashboardCard
-            title="Units With Import Issues"
-            icon={<AlertTriangle className="h-4" />}
-            text={invalidCodesCount?.toString() ?? "-"}
-            failed={(invalidCodesCount ?? 0) > 0 || !!invalidCodesError}
-          />
+          <Suspense
+            fallback={<FallBackCard title="Units With Import Issues" />}
+          >
+            <InvalidCodesCard />
+          </Suspense>
         </Link>
       </div>
     </main>
   );
 }
 
+const StatisticalVariableCountCard = async () => {
+  const client = createClient();
+
+  const { count, error } = await client
+    .from("stat_definition")
+    .select("", { count: "exact" })
+    .limit(0);
+
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  return (
+    <DashboardCard
+      title="Statistical Variables"
+      icon={<BarChart3 className="h-4" />}
+      text={count?.toString() ?? "-"}
+      failed={!!error}
+    />
+  );
+};
+
+const ActivityCategoryCard = async () => {
+  const client = createClient();
+
+  const { data: settings, error } = await client
+    .from("settings")
+    .select("activity_category_standard(id,name)")
+    .single();
+
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  return (
+    <DashboardCard
+      title="Activity Category Standard"
+      icon={<ScrollText className="h-4" />}
+      text={settings?.activity_category_standard?.name ?? "-"}
+      failed={!!error}
+    />
+  );
+};
+
+const CustomActivityCategoryCard = async () => {
+  const client = createClient();
+
+  const { count, error } = await client
+    .from("activity_category_available_custom")
+    .select("", { count: "exact" })
+    .limit(0);
+
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  return (
+    <DashboardCard
+      title="Custom Activity Category Codes"
+      icon={<Settings className="h-4" />}
+      text={count?.toString() ?? "-"}
+      failed={!!error}
+    />
+  );
+};
+
+const MissingActivityCategoryCard = async () => {
+  const client = createClient();
+
+  const { count, error } = await client
+    .from("statistical_unit")
+    .select("", { count: "exact" })
+    .is("primary_activity_category_path", null)
+    .limit(0);
+
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  return (
+    <DashboardCard
+      title="Units Missing Activity Category"
+      icon={<AlertTriangle className="h-4" />}
+      text={count?.toString() ?? "-"}
+      failed={!!error || (count ?? 0) > 0}
+    />
+  );
+};
+
+const MissingRegionCard = async () => {
+  const client = createClient();
+
+  const { count, error } = await client
+    .from("statistical_unit")
+    .select("", { count: "exact" })
+    .is("physical_region_path", null)
+    .limit(0);
+
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  return (
+    <DashboardCard
+      title="Units Missing Region"
+      icon={<AlertTriangle className="h-4" />}
+      text={count?.toString() ?? "-"}
+      failed={!!error || (count ?? 0) > 0}
+    />
+  );
+};
+
+const InvalidCodesCard = async () => {
+  const client = createClient();
+
+  const { count, error } = await client
+    .from("statistical_unit")
+    .select("", { count: "exact" })
+    .not("invalid_codes", "is", null)
+    .limit(0);
+
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  return (
+    <DashboardCard
+      title="Units With Import Issues"
+      icon={<AlertTriangle className="h-4" />}
+      text={count?.toString() ?? "-"}
+      failed={!!error || (count ?? 0) > 0}
+    />
+  );
+};
+
+const RegionCard = async () => {
+  const client = createClient();
+
+  const { count, error } = await client
+    .from("region")
+    .select("", { count: "exact" })
+    .limit(0);
+
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  return (
+    <DashboardCard
+      title="Regions"
+      icon={<Globe2 className="h-4" />}
+      text={count?.toString() ?? "-"}
+      failed={!!error}
+    />
+  );
+};
+
+const StatisticalUnitCountCard = async ({
+  unitType,
+  title,
+}: {
+  readonly unitType: "enterprise" | "legal_unit" | "establishment";
+  readonly title: string;
+}) => {
+  const client = createClient();
+
+  const { count, error } = await client
+    .from("statistical_unit")
+    .select("", { count: "exact" })
+    .eq("unit_type", unitType)
+    .limit(0);
+
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  return (
+    <DashboardCard
+      title={title}
+      icon={<StatisticalUnitIcon type={unitType} className="h-4" />}
+      text={count?.toString() ?? "-"}
+      failed={!!error}
+    />
+  );
+};
+
+const FallBackCard = ({ title }: { readonly title: string }) => {
+  return (
+    <DashboardCard
+      title={title}
+      icon={<Loader className="h-4 animate-spin" />}
+      text="&nbsp;"
+    />
+  );
+};
+
 const DashboardCard = ({
   title,
   icon,
   text,
   failed,
+  className,
 }: {
   readonly title: string;
   readonly icon: ReactNode;
   readonly text: string;
-  readonly failed: boolean;
+  readonly failed?: boolean;
+  readonly className?: string;
 }) => {
   return (
-    <Card className="overflow-hidden">
+    <Card className={cn("overflow-hidden", className)}>
       <CardHeader
         className={cn(
           "flex flex-row items-center justify-between space-y-0 bg-gray-100 px-3 py-2",
