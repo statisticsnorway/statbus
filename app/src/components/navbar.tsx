a2d7016d6abea552c4a4112c2e728972cc18ef58
Jonas Lundeland <jonas.lundeland@tv2.no>
2024-04-05 09:44:29 +0200

    [ui] Hide menu elements if no session

--- app/src/components/navbar.tsx
+++ app/src/components/navbar.tsx
@@ -7,8 +7,11 @@ import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
 import { CommandPaletteTriggerMobileMenuButton } from "@/components/command-palette/command-palette-trigger-button";
 import TimeContextSelector from "@/components/time-context-selector";
+import { createClient } from "@/lib/supabase/server";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const supabase = createClient();
+  const session = await supabase.auth.getSession();
   return (
     <header className="bg-ssb-dark text-white">
       <div className="mx-auto flex max-w-screen-xl items-center justify-between gap-4 p-2 lg:px-4">
@@ -21,31 +24,33 @@ export default function Navbar() {
             className="h-8"
           />
         </a>
-        <div className="flex-1 space-x-3 flex items-center justify-end">
-          <TimeContextSelector />
-          <Link
-            href="/reports"
-            className={cn(
-              buttonVariants({ variant: "ghost", size: "sm" }),
-              "space-x-2 hidden lg:flex"
-            )}
-          >
-            <BarChartHorizontal size={16} />
-            <span>Reports</span>
-          </Link>
-          <Link
-            href="/search"
-            className={cn(
-              buttonVariants({ variant: "ghost", size: "sm" }),
-              "space-x-2 hidden lg:flex"
-            )}
-          >
-            <Search size={16} />
-            <span>Statistical Units</span>
-          </Link>
-          <ProfileAvatar className="w-8 h-8 text-ssb-dark hidden lg:flex" />
-          <CommandPaletteTriggerMobileMenuButton className="lg:hidden" />
-        </div>
+        {session.data.session?.user && (
+          <div className="flex-1 space-x-3 flex items-center justify-end">
+            <TimeContextSelector />
+            <Link
+              href="/reports"
+              className={cn(
+                buttonVariants({ variant: "ghost", size: "sm" }),
+                "space-x-2 hidden lg:flex"
+              )}
+            >
+              <BarChartHorizontal size={16} />
+              <span>Reports</span>
+            </Link>
+            <Link
+              href="/search"
+              className={cn(
+                buttonVariants({ variant: "ghost", size: "sm" }),
+                "space-x-2 hidden lg:flex"
+              )}
+            >
+              <Search size={16} />
+              <span>Statistical Units</span>
+            </Link>
+            <ProfileAvatar className="w-8 h-8 text-ssb-dark hidden lg:flex" />
+            <CommandPaletteTriggerMobileMenuButton className="lg:hidden" />
+          </div>
+        )}
       </div>
     </header>
   );
