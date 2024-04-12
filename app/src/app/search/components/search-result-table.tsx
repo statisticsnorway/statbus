59854c38d8d61566d580e7b5742519ce89b37ff9
Jonas Lundeland <jonas.lundeland@tv2.no>
2024-04-12 13:33:38 +0200

    [ui] Render table body skeleton as search loading indicator

--- app/src/app/search/components/search-result-table.tsx
+++ app/src/app/search/components/search-result-table.tsx
@@ -1,9 +1,8 @@
 "use client";
-import { Table, TableBody } from "@/components/ui/table";
+import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
 import { StatisticalUnitTableRow } from "@/app/search/components/statistical-unit-table-row";
 import { StatisticalUnitTableHeader } from "@/app/search/components/statistical-unit-table-header";
 import { useSearchContext } from "@/app/search/use-search-context";
-import { Loader } from "lucide-react";
 import { cn } from "@/lib/utils";
 
 export default function SearchResultTable() {
@@ -11,24 +10,65 @@ export default function SearchResultTable() {
 
   return (
     <div className="relative">
-      <Table className={cn("bg-white", isLoading && "blur-md")}>
+      <Table className={cn("bg-white", isLoading && "")}>
         <StatisticalUnitTableHeader />
-        <TableBody>
-          {searchResult?.statisticalUnits?.map((unit) => {
-            return (
-              <StatisticalUnitTableRow
-                key={`${unit.unit_id}-${unit.unit_type}`}
-                unit={unit}
-              />
-            );
-          })}
-        </TableBody>
+        {isLoading ? (
+          <TableBodySkeleton />
+        ) : (
+          <TableBody>
+            {searchResult?.statisticalUnits?.map((unit) => {
+              return (
+                <StatisticalUnitTableRow
+                  key={`${unit.unit_id}-${unit.unit_type}`}
+                  unit={unit}
+                />
+              );
+            })}
+          </TableBody>
+        )}
       </Table>
-      {isLoading && (
-        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-          <Loader className="animate-spin duration-1000 h-8 w-8 stroke-ssb-dark" />
-        </div>
-      )}
     </div>
+  );
+}
+
+function TableBodySkeleton() {
+  return (
+    <TableBody>
+      {[...Array(10)].map((_, index) => (
+        <TableRow key={index} className="animate-pulse">
+          <TableCell className="py-2">
+            <div className="flex items-center space-x-3">
+              <div className="w-5 h-5 bg-gray-200" />
+              <div className="flex flex-col space-y-1.5">
+                <div className="flex w-56 h-4 lg:w-48 bg-gray-200" />
+                <div className="flex w-20 h-3 lg:w-32 bg-gray-200" />
+              </div>
+            </div>
+          </TableCell>
+          <TableCell className="py-2 hidden lg:table-cell">
+            <div className="flex flex-col space-y-1.5">
+              <div className="flex w-16 h-4 bg-gray-200" />
+              <div className="flex w-20 h-2.5 bg-gray-200" />
+            </div>
+          </TableCell>
+          <TableCell />
+          <TableCell className="py-2 hidden lg:table-cell">
+            <div className="flex flex-col space-y-1.5">
+              <div className="flex w-16 h-4 bg-gray-200" />
+              <div className="flex w-28 h-2.5 bg-gray-200" />
+            </div>
+          </TableCell>
+          <TableCell className="py-2 hidden lg:table-cell">
+            <div className="flex  flex-col space-y-1.5">
+              <div className="flex w-16 h-4 bg-gray-200" />
+              <div className="flex w-36 h-2.5 bg-gray-200" />
+            </div>
+          </TableCell>
+          <TableCell className="py-2">
+            <div className="w-4 h-2 bg-gray-200" />
+          </TableCell>
+        </TableRow>
+      ))}
+    </TableBody>
   );
 }
