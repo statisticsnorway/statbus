2b7914d86ad18b799945b273127670b688ca020c
Jonas Lundeland <jonas.lundeland@tv2.no>
2024-04-12 14:02:36 +0200

    [ui, refactor] Extract SearchResultTableBodySkeleton as separate component

---
+++ app/src/app/search/components/search-result-table-body-skeleton.tsx
@@ -0,0 +1,43 @@
+import { TableBody, TableCell, TableRow } from "@/components/ui/table";
+
+export function SearchResultTableBodySkeleton() {
+  return (
+    <TableBody>
+      {[...Array(10)].map((_, index) => (
+        <TableRow key={index} className="animate-pulse">
+          <TableCell className="py-2">
+            <div className="flex items-center space-x-3">
+              <div className="w-5 h-5 bg-gray-200" />
+              <div className="flex flex-col space-y-1.5">
+                <div className="flex w-56 h-4 lg:w-48 bg-gray-200" />
+                <div className="flex w-20 h-3 lg:w-32 bg-gray-200" />
+              </div>
+            </div>
+          </TableCell>
+          <TableCell className="py-2 hidden lg:table-cell">
+            <div className="flex flex-col space-y-1.5">
+              <div className="flex w-16 h-4 bg-gray-200" />
+              <div className="flex w-20 h-2.5 bg-gray-200" />
+            </div>
+          </TableCell>
+          <TableCell />
+          <TableCell className="py-2 hidden lg:table-cell">
+            <div className="flex flex-col space-y-1.5">
+              <div className="flex w-16 h-4 bg-gray-200" />
+              <div className="flex w-28 h-2.5 bg-gray-200" />
+            </div>
+          </TableCell>
+          <TableCell className="py-2 hidden lg:table-cell">
+            <div className="flex  flex-col space-y-1.5">
+              <div className="flex w-16 h-4 bg-gray-200" />
+              <div className="flex w-36 h-2.5 bg-gray-200" />
+            </div>
+          </TableCell>
+          <TableCell className="py-2">
+            <div className="w-4 h-2 bg-gray-200" />
+          </TableCell>
+        </TableRow>
+      ))}
+    </TableBody>
+  );
+}
--- app/src/app/search/components/search-result-table.tsx
+++ app/src/app/search/components/search-result-table.tsx
@@ -1,9 +1,10 @@
 "use client";
-import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
+import { Table, TableBody } from "@/components/ui/table";
 import { StatisticalUnitTableRow } from "@/app/search/components/statistical-unit-table-row";
 import { StatisticalUnitTableHeader } from "@/app/search/components/statistical-unit-table-header";
 import { useSearchContext } from "@/app/search/use-search-context";
 import { cn } from "@/lib/utils";
+import { SearchResultTableBodySkeleton } from "@/app/search/components/search-result-table-body-skeleton";
 
 export default function SearchResultTable() {
   const { searchResult, isLoading } = useSearchContext();
@@ -13,7 +14,7 @@ export default function SearchResultTable() {
       <Table className={cn("bg-white", isLoading && "")}>
         <StatisticalUnitTableHeader />
         {isLoading ? (
-          <TableBodySkeleton />
+          <SearchResultTableBodySkeleton />
         ) : (
           <TableBody>
             {searchResult?.statisticalUnits?.map((unit) => {
@@ -28,47 +29,5 @@ export default function SearchResultTable() {
         )}
       </Table>
     </div>
-  );
-}
-
-function TableBodySkeleton() {
-  return (
-    <TableBody>
-      {[...Array(10)].map((_, index) => (
-        <TableRow key={index} className="animate-pulse">
-          <TableCell className="py-2">
-            <div className="flex items-center space-x-3">
-              <div className="w-5 h-5 bg-gray-200" />
-              <div className="flex flex-col space-y-1.5">
-                <div className="flex w-56 h-4 lg:w-48 bg-gray-200" />
-                <div className="flex w-20 h-3 lg:w-32 bg-gray-200" />
-              </div>
-            </div>
-          </TableCell>
-          <TableCell className="py-2 hidden lg:table-cell">
-            <div className="flex flex-col space-y-1.5">
-              <div className="flex w-16 h-4 bg-gray-200" />
-              <div className="flex w-20 h-2.5 bg-gray-200" />
-            </div>
-          </TableCell>
-          <TableCell />
-          <TableCell className="py-2 hidden lg:table-cell">
-            <div className="flex flex-col space-y-1.5">
-              <div className="flex w-16 h-4 bg-gray-200" />
-              <div className="flex w-28 h-2.5 bg-gray-200" />
-            </div>
-          </TableCell>
-          <TableCell className="py-2 hidden lg:table-cell">
-            <div className="flex  flex-col space-y-1.5">
-              <div className="flex w-16 h-4 bg-gray-200" />
-              <div className="flex w-36 h-2.5 bg-gray-200" />
-            </div>
-          </TableCell>
-          <TableCell className="py-2">
-            <div className="w-4 h-2 bg-gray-200" />
-          </TableCell>
-        </TableRow>
-      ))}
-    </TableBody>
   );
 }
