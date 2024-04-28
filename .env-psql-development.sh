395861867097219bd63b6aa444b9718e837ed1ac
Jørgen H. Fjeld <jorgen@veridit.no>
2024-04-27 20:04:17 -1000

    [dev] Remove unused file
    It is replaced by `eval $(./devops/manage-statbus.sh postgres-variables)`.

--- .env-psql-development.sh
+++
@@ -1,8 +0,0 @@
-WORKSPACE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-export PGHOST=localhost
-#export PGPORT=$(awk -F '=' '/POSTGRES_PORT/{print $2}' $WORKSPACE/.env)
-export PGPORT=3432
-export PGUSER=postgres
-export PGDATABASE=$(awk -F '=' '/POSTGRES_DB/{print $2}' $WORKSPACE/.env)
-export PGPASSWORD=$(awk -F '=' '/POSTGRES_PASSWORD/{print $2}' $WORKSPACE/.env)
