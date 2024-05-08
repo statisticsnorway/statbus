f338fa0d5ea8d642214c02dbfb98711976f184f3
Hedda Hubertz <hhz@ssb.no>
2024-05-08 13:45:10 +0200

    [ui] Add turnover to topology item on details page

--- app/src/components/statistical-unit-details/details-page-layout.tsx
+++ app/src/components/statistical-unit-details/details-page-layout.tsx
@@ -16,7 +16,7 @@ export const DetailsPageLayout = ({
   <main className="mx-auto w-full max-w-5xl space-y-6 px-2 py-2 lg:py-24">
     {header}
     <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-      <aside className="lg:w-4/12">
+      <aside className="lg:w-6/12">
         {nav}
         <div className="mt-6 p-2">{topology}</div>
       </aside>
