45678aa7f23f604dc679f6027f22327234f5dd88
Jørgen H. Fjeld <jorgen@veridit.no>
2024-05-07 21:45:12 -1000

    [auth] Fix user creation callback

--- dbseed/create-db-structure.sql
+++ dbseed/create-db-structure.sql
@@ -65,7 +65,7 @@ DECLARE
   role_id INTEGER;
 BEGIN
   -- Start with a minimal set of rights upon auto creation by trigger.
-  SELECT id INTO role_id FROM public.statbus_role WHERE role_type = 'external_user';
+  SELECT id INTO role_id FROM public.statbus_role WHERE type = 'external_user';
   INSERT INTO public.statbus_user (uuid, role_id) VALUES (new.id, role_id);
   RETURN new;
 END;
