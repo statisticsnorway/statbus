e79b02dd9d437f73923e646ac18a9ccb26d9f8d7
Hedda Hubertz <hhz@ssb.no>
2024-05-29 14:11:30 +0200

    [ui] Add 'Upload Establisments' link to command palette

--- app/src/components/command-palette/command-palette.tsx
+++ app/src/components/command-palette/command-palette.tsx
@@ -151,6 +151,13 @@ export function CommandPalette() {
             <Upload className="mr-2 h-4 w-4" />
             <span>Upload Legal Units</span>
           </CommandItem>
+          <CommandItem
+            onSelect={() => navigate("/getting-started/upload-establishments")}
+            value="Upload Establishments"
+          >
+            <Upload className="mr-2 h-4 w-4" />
+            <span>Upload Establishments</span>
+          </CommandItem>
           <CommandItem onSelect={() => navigate("/profile")} value="Profile">
             <User className="mr-2 h-4 w-4" />
             <span>Profile</span>
