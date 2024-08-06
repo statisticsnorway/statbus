b7088bd80533357aab020aff44bf19b9e570b50f
Jørgen H. Fjeld <jorgen@veridit.no>
2024-08-06 10:54:07 +0700

    db,test: Test and improve generate_mermaid_er_diagram
    As a first step towards having better test coverage, document
    and test generate_mermaid_er_diagram.
    In the process, also fix the obvious problems with the output.
    
    The syntax has been tested on "https://mermaid.live/edit" however
    it fails there due to mermaid not supporting types in double quotes,
    and postgresql having types with space.
    Ref. https://github.com/mermaid-js/mermaid/issues/1546
    
    I think a future better option is either to change to PlantUML that
    supports quoting with double quote.

--- dbseed/create-db-structure.sql
+++ dbseed/create-db-structure.sql
@@ -5563,6 +5563,44 @@ $$ LANGUAGE plpgsql;
 
 
 \echo public.generate_mermaid_er_diagram
+/*
+  Function: public.generate_mermaid_er_diagram()
+  Purpose: Generates a Mermaid syntax ER diagram representing the schema of the database.
+
+  Description:
+  This function constructs a textual representation of the database schema using the Mermaid ER diagram syntax. 
+  It lists tables with their columns and types and describes the relationships between tables through foreign keys.
+
+  Relationship Notation:
+  - The relationships are represented with the following cardinality symbols:
+    - Left-hand side (from the perspective of the right entity):
+      - "||": Exactly one
+      - "|o": Zero or one
+      - "}o": Zero or more (no upper limit)
+      - "}|": One or more (no upper limit)
+    - Right-hand side (from the perspective of the left entity):
+      - "||": Exactly one
+      - "o|": One or more (no upper limit)
+      - "o{": Zero or more (no upper limit)
+      - "|{": One or more (no upper limit)
+
+  Cardinality Representation:
+  - The notation is interpreted based on the perspective of the entities:
+    - For "EntityA ||--o{ EntityB":
+      - From EntityB to EntityA:
+        - Each instance of EntityB must be associated with exactly one instance of EntityA ("||" on EntityA side).
+      - From EntityA to EntityB:
+        - Each instance of EntityA can be associated with zero or more instances of EntityB ("o{" on EntityB side).
+
+  This interpretation is consistent with the Mermaid syntax rules, ensuring that the generated diagram accurately reflects 
+  the database schema's relationships and constraints.
+
+  Usage:
+  This function can be used to visualize the structure of the database schema, making it easier to understand the 
+  relationships and cardinalities between different tables.
+
+  Note: The output is a text-based ER diagram in Mermaid syntax, which can be rendered using Mermaid-compatible tools to produce a visual representation of the schema.
+*/
 CREATE OR REPLACE FUNCTION public.generate_mermaid_er_diagram()
 RETURNS text AS $$
 DECLARE
@@ -5570,10 +5608,23 @@ DECLARE
     result text := 'erDiagram';
 BEGIN
     -- First part of the query (tables and columns)
+    result := result || E'\n\t%% Entities (derived from tables)';
     FOR rec IN
-        SELECT format(E'\t%s{\n%s\n}',
-            c.relname,
-            string_agg(format(E'\t\t%s %s',
+        SELECT format(E'\t%s["%s"] {\n%s\n\t}',
+            -- Include the schema and a underscore if different than 'public' for the source table
+            -- since period is not valid syntax for an entity name.
+            CASE WHEN n.nspname <> 'public'
+                 THEN n.nspname || '_' || c.relname
+                 ELSE c.relname
+            END,
+            -- Provide the correct name with period as the label.
+            CASE WHEN n.nspname <> 'public'
+                 THEN n.nspname || '.' || c.relname
+                 ELSE c.relname
+            END,
+            -- Notice that mermaid uses the "attribute_type attribute_name" pattern
+            -- and that if there are spaces there must be double quoting.
+            string_agg(format(E'\t\t"%s" %s',
                 format_type(t.oid, a.atttypmod),
                 a.attname
             ), E'\n')
@@ -5585,18 +5636,61 @@ BEGIN
         WHERE c.relkind IN ('r', 'p')
           AND NOT c.relispartition
           AND n.nspname !~ '^pg_' AND n.nspname <> 'information_schema'
-        GROUP BY c.relname
+        GROUP BY n.nspname, c.relname
+        ORDER BY n.nspname, c.relname
     LOOP
         result := result || E'\n' || rec.format;
     END LOOP;
 
     -- Second part of the query (foreign key constraints)
+    result := result || E'\n\t%% Relationships (derived from foreign keys)';
+    -- Documentation of relationship syntax from https://mermaid.js.org/syntax/entityRelationshipDiagram.html#relationship-syntax
+    -- In particular:
+    --     Value (left)    Value (right)   Meaning
+    --     |o              o|              Zero or one
+    --     ||              ||              Exactly one
+    --     }o              o{              Zero or more (no upper limit)
+    --     }|              |{              One or more (no upper limit)
     FOR rec IN
-        SELECT format('%s }|..|| %s : %s', c1.relname, c2.relname, c.conname)
+        SELECT format(E'\t%s %s--%s %s : %s',
+            -- Include the schema and a underscore if different than 'public' for the source table
+            -- since period is not valid syntax for an entity name.
+            CASE WHEN n1.nspname <> 'public'
+                 THEN n1.nspname || '_' || c1.relname
+                 ELSE c1.relname
+            END,
+            -- The relationship cardinality from the referenced table (target) towards the referencing table (source).
+            CASE
+                WHEN EXISTS (
+                    SELECT 1
+                    FROM pg_constraint con
+                    WHERE con.conrelid = c.confrelid
+                    AND con.conkey = c.conkey
+                    AND con.contype IN ('p', 'u')
+                )
+                THEN '}|' -- Every instance in the target can have one or more instances in the source
+                ELSE '}o' -- Every instance in the target can have zero or more instances in the source
+            END,
+            -- The relationship cardinality from the referencing table (source) towards the referenced table (target).
+            CASE
+                WHEN a.attnotnull THEN '||' -- Every instance in the source must reference exactly one instance in the target
+                ELSE 'o|'                   -- Every instance in the source may reference zero or one instance in the target
+            END,
+            -- Include the schema and a period if different than 'public' for the target table
+            CASE WHEN n2.nspname <> 'public'
+                 THEN n2.nspname || '.' || c2.relname
+                 ELSE c2.relname
+            END,
+            c.conname
+        )
         FROM pg_constraint c
         JOIN pg_class c1 ON c.conrelid = c1.oid AND c.contype = 'f'
         JOIN pg_class c2 ON c.confrelid = c2.oid
+        JOIN pg_namespace n1 ON n1.oid = c1.relnamespace
+        JOIN pg_namespace n2 ON n2.oid = c2.relnamespace
+        JOIN pg_attribute a ON a.attnum = ANY (c.conkey) AND a.attrelid = c.conrelid
         WHERE NOT c1.relispartition AND NOT c2.relispartition
+        ORDER BY n1.nspname, c1.relname, n2.nspname, c2.relname
     LOOP
         result := result || E'\n' || rec.format;
     END LOOP;
@@ -5604,6 +5698,7 @@ BEGIN
     RETURN result;
 END;
 $$ LANGUAGE plpgsql;
+
 
 \echo public.sector_available
 CREATE VIEW public.sector_available(path, name, custom, description)
---
+++ test/sql/21_generate_mermaid_er_diagram.sql
@@ -0,0 +1,7 @@
+\t
+\a
+SELECT '########################################'
+UNION ALL
+SELECT public.generate_mermaid_er_diagram()
+UNION ALL
+SELECT '########################################';
