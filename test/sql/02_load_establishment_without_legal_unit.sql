512b2d6f71e0bcdb9a56145228ce485e9f1a9b4c
Jørgen H. Fjeld <jorgen@veridit.no>
2024-08-16 14:15:33 +0700

    test: Fix distinct count

--- test/sql/02_load_establishment_without_legal_unit.sql
+++ test/sql/02_load_establishment_without_legal_unit.sql
@@ -29,15 +29,15 @@ SELECT count(*) FROM public.legal_form_available;
 SELECT count(*) FROM public.sector_available;
 
 SELECT
-    (SELECT COUNT(id) FROM public.establishment) AS establishment_count,
-    (SELECT COUNT(id) FROM public.legal_unit) AS legal_unit_count,
-    (SELECT COUNT(id) FROM public.enterprise) AS enterprise_count;
+    (SELECT COUNT(DISTINCT id) AS distinct_unit_count FROM public.establishment) AS establishment_count,
+    (SELECT COUNT(DISTINCT id) AS distinct_unit_count FROM public.legal_unit) AS legal_unit_count,
+    (SELECT COUNT(DISTINCT id) AS distinct_unit_count FROM public.enterprise) AS enterprise_count;
 \echo "User uploads establishments without legal_unit"
 \copy public.import_establishment_current_without_legal_unit(tax_ident,name,birth_date,death_date,physical_address_part1,physical_postal_code,physical_postal_place,physical_region_code,physical_country_iso_2,postal_address_part1,postal_postal_code,postal_postal_place,postal_region_code,postal_country_iso_2,primary_activity_category_code,secondary_activity_category_code,employees) FROM 'test/data/02_norwegian-establishments-without-legal-unit.csv' WITH (FORMAT csv, DELIMITER ',', QUOTE '"', HEADER true);
 SELECT
-    (SELECT COUNT(id) FROM public.establishment) AS establishment_count,
-    (SELECT COUNT(id) FROM public.legal_unit) AS legal_unit_count,
-    (SELECT COUNT(id) FROM public.enterprise) AS enterprise_count;
+    (SELECT COUNT(DISTINCT id) AS distinct_unit_count FROM public.establishment) AS establishment_count,
+    (SELECT COUNT(DISTINCT id) AS distinct_unit_count FROM public.legal_unit) AS legal_unit_count,
+    (SELECT COUNT(DISTINCT id) AS distinct_unit_count FROM public.enterprise) AS enterprise_count;
 
 \echo "Refreshing materialized views"
 -- Exclude the refresh_time_ms as it will vary.
@@ -49,7 +49,7 @@ FROM statistical_unit ORDER BY name, unit_type;
 
 \echo "Checking statistics"
 SELECT unit_type
-     , count(*)
+     , COUNT(DISTINCT unit_id) AS distinct_unit_count
      , jsonb_pretty(jsonb_agg(DISTINCT invalid_codes) FILTER (WHERE invalid_codes IS NOT NULL)) AS invalid_codes
      , jsonb_pretty(jsonb_stats_summary_merge_agg(stats_summary)) AS stats_summary
  FROM statistical_unit
