4f424b7c1d7e0bef9dd0535fb0295f741e436bda
Jørgen H. Fjeld <jorgen@veridit.no>
2024-09-27 00:59:04 +0200

    db,ui: Fix default ordering of regions in search

--- dbseed/create-db-structure.sql
+++ dbseed/create-db-structure.sql
@@ -4860,7 +4860,7 @@ SELECT r.id
      , r.name
 FROM public.region AS r
 WHERE r.path OPERATOR(public.@>) (SELECT array_agg(DISTINCT physical_region_path) FROM public.statistical_unit WHERE physical_region_path IS NOT NULL)
-ORDER BY path;
+ORDER BY public.nlevel(path), path;
 
 CREATE UNIQUE INDEX "region_used_key"
     ON public.region_used (path);
