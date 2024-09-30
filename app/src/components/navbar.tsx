4eaacb855c3751039a3ee368cce23a6c587c45f6
Jørgen H. Fjeld <jorgen@veridit.no>
2024-09-30 11:36:19 +0200

    ui: Fix update of tc parameter for current time context

--- app/src/components/navbar.tsx
+++ app/src/components/navbar.tsx
@@ -2,7 +2,7 @@
 import ProfileAvatar from "@/components/profile-avatar";
 import Image from "next/image";
 import logo from "@/../public/statbus-logo.png";
-import { TimeContextLink } from "@/components/time-context-link";
+import Link from "next/link";
 import { BarChartHorizontal, Search } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
@@ -27,13 +27,13 @@ export default function Navbar() {
   return (
     <header className="bg-ssb-dark text-white">
       <div className="mx-auto flex max-w-screen-xl items-center justify-between gap-4 p-2 lg:px-4">
-        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <Image src={logo} alt="Statbus Logo" className="h-10 w-10" />
-        </a>
+        </Link>
         {isAuthenticated && (
           <div className="flex-1 space-x-3 flex items-center justify-end">
             <TimeContextSelector />
-            <TimeContextLink
+            <Link
               href="/reports"
               className={cn(
                 buttonVariants({ variant: "ghost", size: "sm" }),
@@ -42,8 +42,8 @@ export default function Navbar() {
             >
               <BarChartHorizontal size={16} />
               <span>Reports</span>
-            </TimeContextLink>
-            <TimeContextLink
+            </Link>
+            <Link
               href="/search"
               className={cn(
                 buttonVariants({ variant: "ghost", size: "sm" }),
@@ -52,7 +52,7 @@ export default function Navbar() {
             >
               <Search size={16} />
               <span>Statistical Units</span>
-            </TimeContextLink>
+            </Link>
             <ProfileAvatar className="w-8 h-8 text-ssb-dark hidden lg:flex" />
             <CommandPaletteTriggerMobileMenuButton className="lg:hidden" />
           </div>
