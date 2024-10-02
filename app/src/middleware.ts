c9f4085fa77a4dbac1a5dc323f40b53ce0cca3b9
Jørgen H. Fjeld <jorgen@veridit.no>
2024-10-02 21:40:08 +0200

    ui: Adapt middleware to updated supabase/ssr

--- app/src/middleware.ts
+++ app/src/middleware.ts
@@ -1,33 +1,9 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
-import { type CookieOptions, createServerClient } from "@supabase/ssr";
+import { createClient } from '@/utils/supabase/server';
 
 export async function middleware(request: NextRequest) {
-  const client = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return request.cookies.get(name)?.value;
-        },
-        set(name: string, value: string, options: CookieOptions) {
-          request.cookies.set({
-            name,
-            value,
-            ...options,
-          });
-        },
-        remove(name: string, options: CookieOptions) {
-          request.cookies.set({
-            name,
-            value: "",
-            ...options,
-          });
-        },
-      },
-    }
-  );
+  const {client} = createClient(request);
   const {
     data: { session },
   } = await client.auth.getSession();
