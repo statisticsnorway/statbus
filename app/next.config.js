2f0956125b34b870c9ea6d20861df08b3924a6d2
Jørgen H. Fjeld <jorgen@veridit.no>
2024-10-03 15:01:26 +0200

    ui: Document reason for next.js config

--- app/next.config.js
+++ app/next.config.js
@@ -1,7 +1,11 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Output a server running js file that does SSR (Server Side Rendering)
+  // as opposed to 'export' that exports static HTML files.
   output: 'standalone',
+  // Disable double rendering of everything, that is more annoying
+  // than the other checks/benefits that strict mode enables.
   reactStrictMode: false,
 };
 
