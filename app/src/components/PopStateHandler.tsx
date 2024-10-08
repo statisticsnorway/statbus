7393d60c932029bee991bea08bebf60fc182f86c
Jørgen H. Fjeld <jorgen@veridit.no>
2024-10-08 15:28:37 +0200

    ui: Make the back button work with selectedTimeContext

--- app/src/components/PopStateHandler.tsx
+++ app/src/components/PopStateHandler.tsx
@@ -1,17 +1,32 @@
 "use client";
 import { useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+import { useTimeContext } from "@/app/time-context";
 
 export default function PopStateHandler() {
+  const searchParams = useSearchParams();
+  const { setSelectedTimeContext, timeContexts } = useTimeContext();
+
   useEffect(() => {
     const handlePopState = () => {
-      window.location.reload();
+      const query = new URLSearchParams(searchParams.toString());
+      const tcQueryParam = query.get("tc");
+
+      if (tcQueryParam) {
+        const selectedContext = timeContexts.find(
+          (timeContext) => timeContext.ident === tcQueryParam
+        );
+        if (selectedContext) {
+          setSelectedTimeContext(selectedContext);
+        }
+      }
     };
 
     window.addEventListener('popstate', handlePopState);
     return () => {
       window.removeEventListener('popstate', handlePopState);
     };
-  }, []);
+  }, [searchParams, setSelectedTimeContext, timeContexts]);
 
   return null;
 }
