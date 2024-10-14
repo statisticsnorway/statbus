20562bb308383e367e37c22af07ff44b3d4d7a80
Jørgen H. Fjeld <jorgen@veridit.no>
2024-10-14 16:57:12 +0200

    ui: Adjust export to dynamic columns

--- app/src/app/api/search/export/route.ts
+++ app/src/app/api/search/export/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { getStatisticalUnits } from "@/app/search/search-requests";
 import { toCSV } from "@/lib/csv-utils";
 import { createSupabaseSSRClient } from "@/utils/supabase/server";
+import { getBaseData } from "@/app/BaseDataServer";
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
@@ -10,19 +11,38 @@ export async function GET(request: NextRequest) {
     searchParams.set("order", "tax_ident.desc");
   }
 
-  if (!searchParams.has("select")) {
-    searchParams.set("select", "*");
-  }
+  const client = await createSupabaseSSRClient();
+  const { externalIdentTypes, statDefinitions } = await getBaseData(client);
 
-  searchParams.set("limit", "100000");
+  const externalIdentColumns = externalIdentTypes.map(({ code }) => `${code}:external_idents->>${code}`);
+  const statDefinitionColumns = statDefinitions.map(({ code }) => `${code}:stats_summary->${code}->sum`);
 
   searchParams.set(
     "select",
-    "tax_ident, name, unit_type, primary_activity_category_id, physical_region_id, employees, turnover, physical_country_iso_2, sector_code, sector_name, legal_form_code, legal_form_name"
+    [
+      externalIdentColumns,
+      [
+        "name",
+        "unit_type",
+        "primary_activity_category_id",
+        "physical_region_id"
+      ],
+      statDefinitionColumns,
+      [
+        "physical_country_iso_2",
+        "sector_code",
+        "sector_name",
+        "legal_form_code",
+        "legal_form_name"
+      ]
+    ].flat().join(",")
   );
 
+  searchParams.set("limit", "100000");
+  searchParams.set("offset", "0");
+
+
   try {
-    const client = await createSupabaseSSRClient();
     const response = await getStatisticalUnits(client, searchParams);
     const { header, body } = toCSV(response.statisticalUnits);
     return new Response(header + body, {
@@ -34,12 +54,11 @@ export async function GET(request: NextRequest) {
   } catch (error) {
     if (error instanceof Error) {
       // Log the error message from the error instance
-      console.error('Error fetching statistical units:', error.message);
+      console.error("Error fetching statistical units:", error.message);
       return NextResponse.json({ error: error.message }, { status: 500 });
-    } else {
-      // Handle non-standard errors (if any other types could be thrown)
-      console.error('Unknown error fetching statistical units:', error);
-      return NextResponse.json({ error: 'An unexpected error occurred' }, { status: 500 });
     }
+    // Handle non-standard errors (if any other types could be thrown)
+    console.error("Unknown error fetching statistical units:", error);
+    return NextResponse.json({ error: "An unexpected error occurred" }, { status: 500 });
   }
 }
--- app/src/app/search/search-requests.ts
+++ app/src/app/search/search-requests.ts
@@ -3,16 +3,16 @@ import { SearchResult } from './search';
 
 export async function getStatisticalUnits(client: SupabaseClient, searchParams: URLSearchParams): Promise<SearchResult> {
   const apiFetcher = async (url: string, init: RequestInit) => {
-      const session = await client.auth.getSession();
-      return fetch(url, {
-        ...init,
-        headers: {
-          ...init.headers,
-          Authorization: `Bearer ${session.data.session?.access_token}`,
-          apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-        },
-      });
-    };
+    const session = await client.auth.getSession();
+    return fetch(url, {
+      ...init,
+      headers: {
+        ...init.headers,
+        Authorization: `Bearer ${session.data.session?.access_token}`,
+        apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      },
+    });
+  };
 
   var response = await apiFetcher(
     `${process.env.NEXT_PUBLIC_SUPABASE_URL}/rest/v1/statistical_unit?${searchParams}`,
@@ -34,7 +34,7 @@ export async function getStatisticalUnits(client: SupabaseClient, searchParams: 
   const count = parseInt(count_str ?? "0", 10);
 
   return {
-      statisticalUnits: data,
-      count: count,
-    };
+    statisticalUnits: data,
+    count: count,
+  };
 }
