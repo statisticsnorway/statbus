386aa34c0c05c7906c2dbb01e26021fee325c922
Jørgen H. Fjeld <jorgen@veridit.no>
2024-10-17 09:49:00 +0200

    dev: Load .env when developing
    To avoid mismatch between .env.local and ../.env

--- app/next.config.js
+++ app/next.config.js
@@ -1,4 +1,13 @@
 // next.config.js
+const dotenv = require('dotenv');
+
+if (process.env.NODE_ENV !== 'production' && !process.env.ENV_LOADED) {
+  const envFilePath = '../.env';
+  dotenv.config({ path: envFilePath });
+  process.env.ENV_LOADED = 'true';
+  console.log(`Development Environment file ${envFilePath} loaded successfully.`);
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Output a server running js file that does SSR (Server Side Rendering)
