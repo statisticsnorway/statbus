09a5f7c361cb46fb1eefe504ba355b538f8fb845
Jørgen H. Fjeld <jorgen@veridit.no>
2024-10-18 16:36:21 +0200

    ui: Add missing import

--- app/src/app/search/search-requests.ts
+++ app/src/app/search/search-requests.ts
@@ -1,11 +1,12 @@
 import { SupabaseClient } from '@supabase/supabase-js';
 import { SearchResult } from './search';
+import { Fetch } from '@supabase/auth-js/src/lib/fetch';
 
 export async function getStatisticalUnits(client: SupabaseClient, searchParams: URLSearchParams): Promise<SearchResult> {
   // Inspect inside the client and get the correct url,
   // as server side and client side code uses different urls.
   // due to running inside docker containers.
-  const apiFetcher = (client as any).rest.fetch as Fetcher;
+  const apiFetcher = (client as any).rest.fetch as Fetch;
   const supabase_url = (client as any).rest.url as String
   var response = await apiFetcher(
     `${supabase_url}/statistical_unit?${searchParams}`,
