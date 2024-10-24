ee546ca112d679dbf06771fb99da2d92f322d6d9
Jørgen H. Fjeld <jorgen@veridit.no>
2024-10-24 10:58:29 +0200

    devop: Add script to extract information about deployments

---
+++ devops/devops.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# devops/devops.sh
+set -euo pipefail
+
+if test -n "${DEBUG:-}"; then
+  set -x
+fi
+
+# TODO: if the `choose` command is not available suggest installation with `brew install choose-rust`
+
+# Get all branches matching the pattern devops/deploy-to-(.*) except production
+SUFFIXES=$(git branch -a | grep 'remotes/origin/devops/deploy-to-' | grep -v 'production' | sd 'remotes/origin/devops/deploy-to-(.*?)' '$1')
+
+# Function to extract information from environment files
+extract_info() {
+  local user=$1
+  ssh "$user@niue.statbus.org" bash <<'EOF'
+set -euo pipefail
+export $(egrep -v '^#' ~/statbus/.env | xargs)
+statbus_users=$(yq '.[] | "  " + .email + " " + .password' ~/statbus/.users.yml)
+cat <<EOS
+############################################################
+## ${STATBUS_URL} - ${NEXT_PUBLIC_DEPLOYMENT_SLOT_CODE} - ${NEXT_PUBLIC_DEPLOYMENT_SLOT_NAME}
+############################################################
+Frontend: ${STATBUS_URL}/
+Usernames and passwords:
+${statbus_users}
+############################################################
+API: ${NEXT_PUBLIC_BROWSER_SUPABASE_URL}
+API Username ${DASHBOARD_USERNAME}
+Shell Access: 'ssh ${USER}@libu.statbus.org'
+############################################################
+
+EOS
+EOF
+}
+
+# Iterate over each deployment slot
+for SUFFIX in $SUFFIXES; do
+  # Derive the user from the branch name
+  user="statbus_${SUFFIX}"
+  extract_info "$user"
+done
+
