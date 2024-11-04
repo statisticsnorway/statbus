74d12f8716888e8618b239517d93d264d0ffdeee
Jørgen H. Fjeld <jorgen@veridit.no>
2024-11-04 13:23:33 +0100

    ui: Use strict mode for Chrom React Tools to work

--- app/next.config.js
+++ app/next.config.js
@@ -13,9 +13,7 @@ const nextConfig = {
   // Output a server running js file that does SSR (Server Side Rendering)
   // as opposed to 'export' that exports static HTML files.
   output: 'standalone',
-  // Disable double rendering of everything, that is more annoying
-  // than the other checks/benefits that strict mode enables.
-  reactStrictMode: false,
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
