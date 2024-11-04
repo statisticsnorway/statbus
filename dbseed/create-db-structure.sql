8a82a6bf43e13b798528302106cbc1f5f5bf66eb
Jørgen H. Fjeld <jorgen@veridit.no>
2024-11-04 16:30:08 +0100

    db: Prevent overlapping external identifiers

--- dbseed/create-db-structure.sql
+++ dbseed/create-db-structure.sql
@@ -1645,7 +1645,6 @@ CREATE TABLE public.external_ident (
     enterprise_id INTEGER REFERENCES public.enterprise(id) ON DELETE CASCADE,
     enterprise_group_id INTEGER CHECK (admin.enterprise_group_id_exists(enterprise_group_id)),
     updated_by_user_id INTEGER NOT NULL REFERENCES public.statbus_user(id) ON DELETE CASCADE,
-    UNIQUE (ident, type_id),
     CONSTRAINT "One and only one statistical unit id must be set"
     CHECK( establishment_id IS NOT NULL AND legal_unit_id IS     NULL AND enterprise_id IS     NULL AND enterprise_group_id IS     NULL
         OR establishment_id IS     NULL AND legal_unit_id IS NOT NULL AND enterprise_id IS     NULL AND enterprise_group_id IS     NULL
@@ -1653,6 +1652,12 @@ CREATE TABLE public.external_ident (
         OR establishment_id IS     NULL AND legal_unit_id IS     NULL AND enterprise_id IS     NULL AND enterprise_group_id IS NOT NULL
         )
 );
+
+CREATE UNIQUE INDEX external_ident_type_for_ident ON public.external_ident(type_id, ident);
+CREATE UNIQUE INDEX external_ident_type_for_establishment ON public.external_ident(type_id, establishment_id) WHERE establishment_id IS NOT NULL;
+CREATE UNIQUE INDEX external_ident_type_for_legal_unit ON public.external_ident(type_id, legal_unit_id) WHERE legal_unit_id IS NOT NULL;
+CREATE UNIQUE INDEX external_ident_type_for_enterprise ON public.external_ident(type_id, enterprise_id) WHERE enterprise_id IS NOT NULL;
+CREATE UNIQUE INDEX external_ident_type_for_enterprise_group ON public.external_ident(type_id, enterprise_group_id) WHERE enterprise_group_id IS NOT NULL;
 CREATE INDEX external_ident_establishment_id_idx ON public.external_ident(establishment_id);
 CREATE INDEX external_ident_legal_unit_id_idx ON public.external_ident(legal_unit_id);
 CREATE INDEX external_ident_enterprise_id_idx ON public.external_ident(enterprise_id);
