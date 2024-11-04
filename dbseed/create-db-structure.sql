82ad46265fee5be6824d84711f30a1a1b6b1116d
Jørgen H. Fjeld <jorgen@veridit.no>
2024-11-04 21:30:28 +0100

    db: Fix handling of conflicting identifiers

--- dbseed/create-db-structure.sql
+++ dbseed/create-db-structure.sql
@@ -8457,6 +8457,109 @@ END; -- Process external identifiers
 $process_external_idents$ LANGUAGE plpgsql;
 
 
+\echo admin.insert_external_idents
+CREATE FUNCTION admin.insert_external_idents(
+    new_jsonb JSONB,
+    external_idents_to_add public.external_ident[],
+    p_legal_unit_id INTEGER,
+    p_establishment_id INTEGER,
+    p_updated_by_user_id INTEGER
+) RETURNS void LANGUAGE plpgsql AS $insert_external_idents$
+DECLARE
+  unit_type TEXT;
+BEGIN
+  -- Ensure that either legal_unit_id or establishment_id is provided, but not both.
+  IF (p_legal_unit_id IS NOT NULL AND p_establishment_id IS NOT NULL) OR
+      (p_legal_unit_id IS NULL AND p_establishment_id IS NULL) THEN
+      RAISE EXCEPTION 'Must provide either a p_legal_unit_id or an p_establishment_id, but not both.';
+  END IF;
+
+  IF array_length(external_idents_to_add, 1) > 0 THEN
+    BEGIN
+      IF p_legal_unit_id IS NOT NULL THEN
+        unit_type := 'legal_unit';
+        -- Insert for legal units if legal_unit_id is provided.
+        INSERT INTO public.external_ident
+            ( type_id
+            , ident
+            , legal_unit_id
+            , updated_by_user_id
+            )
+        SELECT type_id
+              , ident
+              , p_legal_unit_id
+              , p_updated_by_user_id
+        FROM unnest(external_idents_to_add);
+      ELSIF p_establishment_id IS NOT NULL THEN
+        unit_type := 'establishment';
+        -- Insert for establishments if establishment_id is provided.
+        INSERT INTO public.external_ident
+            ( type_id
+            , ident
+            , establishment_id
+            , updated_by_user_id
+            )
+        SELECT type_id
+              , ident
+              , p_establishment_id
+              , p_updated_by_user_id
+        FROM unnest(external_idents_to_add);
+      END IF;
+    EXCEPTION WHEN unique_violation THEN
+      IF SQLERRM LIKE '%external_ident_type_for_%' THEN
+        DECLARE
+          pg_exception_detail	TEXT;
+
+          extracted_values TEXT[];
+
+          extracted_conflict_unit_type TEXT;
+          extracted_foreign_column TEXT;
+          extracted_type_id INTEGER;
+          extracted_unit_id INTEGER;
+          offending_rows JSONB;
+
+        BEGIN
+          GET STACKED DIAGNOSTICS pg_exception_detail = PG_EXCEPTION_DETAIL;
+
+          -- pg_exception_detail='Key (type_id, establishment_id)=(1, 1) already exists.'
+          extracted_values := regexp_matches(
+              pg_exception_detail,
+              'Key \(type_id, ((.*?)_id)\)=\((\d+), (\d+)\)'
+          );
+
+          IF array_length(extracted_values, 1) = 4 THEN
+            extracted_foreign_column := extracted_values[1];
+            extracted_conflict_unit_type := extracted_values[2];
+            extracted_type_id := extracted_values[3]::INT;
+            extracted_unit_id := extracted_values[4]::INT;
+
+            EXECUTE format($$
+              SELECT jsonb_object_agg(eit.code,ei.ident)
+              FROM public.external_ident AS ei
+              JOIN public.external_ident_type AS eit
+              ON ei.type_id = eit.id
+              WHERE ei.%s = %L
+            $$, extracted_foreign_column, extracted_unit_id)
+            INTO offending_rows;
+
+            RAISE EXCEPTION 'Another % % already uses the same identier(s) as the % in row %', extracted_conflict_unit_type, offending_rows, unit_type, new_jsonb
+            USING ERRCODE = 'unique_violation',
+              HINT = 'Check for other units already using the same identifier',
+              DETAIL = 'Key constraint (type_id, '||unit_type||'_id) is violated.';
+          ELSE
+              RAISE; -- Re-raise unexpected exceptions
+          END IF;
+        END;
+      ELSE
+        RAISE; -- Re-raise unexpected exceptions
+      END IF;
+    END;
+  END IF;
+END;
+$insert_external_idents$;
+
+
+
 -- Find a connected legal_unit - i.e. a field with a `legal_unit`
 -- prefix that points to an external identifier.
 \echo admin.process_linked_legal_unit_external_idents
@@ -8965,21 +9068,13 @@ BEGIN
         SET CONSTRAINTS ALL DEFERRED;
     END IF;
 
-    -- Store external identifiers
-    IF array_length(external_idents_to_add, 1) > 0 THEN
-        INSERT INTO public.external_ident
-            ( type_id
-            , ident
-            , legal_unit_id
-            , updated_by_user_id
-            )
-         SELECT type_id
-              , ident
-              , inserted_legal_unit.id
-              , edited_by_user.id
-         FROM unnest(external_idents_to_add);
-    END IF;
-
+    PERFORM admin.insert_external_idents(
+      new_jsonb,
+      external_idents_to_add,
+      p_legal_unit_id => inserted_legal_unit.id,
+      p_establishment_id => null::INTEGER,
+      p_updated_by_user_id => edited_by_user.id
+      );
 
     IF physical_region.id IS NOT NULL OR physical_country.id IS NOT NULL THEN
         INSERT INTO public.location_era
@@ -9711,19 +9806,13 @@ BEGIN
         SET CONSTRAINTS ALL DEFERRED;
     END IF;
 
-    IF array_length(external_idents_to_add, 1) > 0 THEN
-        INSERT INTO public.external_ident
-            ( type_id
-            , ident
-            , establishment_id
-            , updated_by_user_id
-            )
-         SELECT type_id
-              , ident
-              , inserted_establishment.id
-              , edited_by_user.id
-         FROM unnest(external_idents_to_add);
-    END IF;
+    PERFORM admin.insert_external_idents(
+      new_jsonb,
+      external_idents_to_add,
+      p_legal_unit_id => null::INTEGER,
+      p_establishment_id => inserted_establishment.id,
+      p_updated_by_user_id => edited_by_user.id
+      );
 
     IF physical_region.id IS NOT NULL OR physical_country.id IS NOT NULL THEN
         INSERT INTO public.location_era
--- test/sql/31_load_units_errors.sql
+++ test/sql/31_load_units_errors.sql
@@ -37,12 +37,6 @@ SELECT
 \echo "User uploads formal establishments with same stat_ident as legal units"
 \copy public.import_establishment_current_for_legal_unit(tax_ident,stat_ident,name,physical_region_code,physical_country_iso_2,primary_activity_category_code,employees,turnover,legal_unit_tax_ident,data_source_code) FROM 'test/data/31_formal_establishments_errors.csv' WITH (FORMAT csv, DELIMITER ',', QUOTE '"', HEADER true);
 
-SELECT
-    (SELECT COUNT(DISTINCT id) AS distinct_unit_count FROM public.establishment) AS establishment_count,
-    (SELECT COUNT(DISTINCT id) AS distinct_unit_count FROM public.legal_unit) AS legal_unit_count,
-    (SELECT COUNT(DISTINCT id) AS distinct_unit_count FROM public.enterprise) AS enterprise_count;
-
-
 
 ROLLBACK TO before_loading_units;
 
@@ -52,7 +46,6 @@ SELECT
     (SELECT COUNT(DISTINCT id) AS distinct_unit_count FROM public.enterprise) AS enterprise_count;
 
 \echo "Same external ident for formal establishment and informal establishment"
-
 
 \echo "User uploads legal units"
 \copy public.import_legal_unit_current(tax_ident,stat_ident,name,birth_date,physical_region_code,physical_country_iso_2,primary_activity_category_code,legal_form_code,sector_code,employees,turnover,data_source_code) FROM 'test/data/31_legal_units.csv' WITH (FORMAT csv, DELIMITER ',', QUOTE '"', HEADER true);
@@ -65,12 +58,5 @@ SELECT
 
 \echo "User uploads informal establishments with same stat_idents as formal establishments"
 \copy public.import_establishment_current_without_legal_unit(tax_ident,stat_ident,name,physical_region_code,physical_country_iso_2,primary_activity_category_code,employees,turnover,data_source_code) FROM 'test/data/31_informal_establishments_errors.csv' WITH (FORMAT csv, DELIMITER ',', QUOTE '"', HEADER true);
-
-SELECT
-    (SELECT COUNT(DISTINCT id) AS distinct_unit_count FROM public.establishment) AS establishment_count,
-    (SELECT COUNT(DISTINCT id) AS distinct_unit_count FROM public.legal_unit) AS legal_unit_count,
-    (SELECT COUNT(DISTINCT id) AS distinct_unit_count FROM public.enterprise) AS enterprise_count;
-
-
 
 ROLLBACK;
