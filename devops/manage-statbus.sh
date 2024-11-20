e697707dfba7a4873d282a6bcf33b33d0f6d5b53
Jørgen H. Fjeld <jorgen@veridit.no>
2024-11-20 12:55:47 +0100

    cli: Use migrate down all

--- devops/manage-statbus.sh
+++ devops/manage-statbus.sh
@@ -253,7 +253,9 @@ case "$action" in
         popd
       ;;
     'delete-db-structure' )
-        ./devops/manage-statbus.sh psql < dbseed/delete-db-structure.sql 2>&1
+        pushd cli
+          shards build && ./bin/statbus migrate down all
+        popd
       ;;
     'reset-db-structure' )
         ./devops/manage-statbus.sh delete-db-structure
