a44fa0c182d264048bf51323996049e514b67068
Jørgen H. Fjeld <jorgen@veridit.no>
2024-11-29 15:46:14 +0100

    db: Make turnover a float

--- migrations/0001_create/049_insert_into_stat_definition.up.sql
+++ migrations/0001_create/049_insert_into_stat_definition.up.sql
@@ -1,7 +1,6 @@
 BEGIN;
 
 INSERT INTO public.stat_definition(code, type, frequency, name, description, priority) VALUES
-  ('employees','int','yearly','Number of people employed','The number of people receiving an official salary with government reporting.',1),
-  ('turnover','int','yearly','Turnover','The amount (EUR)',2);
-
+  ('employees','int','yearly','Employees','The number of people receiving an official salary with government reporting.',1),
+  ('turnover','float','yearly','Turnover','The amount (Local Currency)',2);
 END;
