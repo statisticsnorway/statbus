caa4541c298d4ad85736e5c4ed0bc543c5c012a3
Jørgen H. Fjeld <jorgen@veridit.no>
2024-12-16 12:39:47 +0100

    db,auth: Fix rights for acitivity

--- migrations/0001_create/157_create_policy_activity_employee_manage.up.sql
+++ migrations/0001_create/157_create_policy_activity_employee_manage.up.sql
@@ -1,7 +1,7 @@
 BEGIN;
 
--- The employees can only update the tables designated by their assigned region or activity_category
-CREATE POLICY activity_employee_manage ON public.activity FOR ALL TO authenticated
+-- The restricted users can only update the tables designated by their assigned region or activity_category
+CREATE POLICY restricted_user_activity_access ON public.activity FOR ALL TO authenticated
 USING (auth.has_statbus_role(auth.uid(), 'restricted_user'::public.statbus_role_type)
        AND auth.has_activity_category_access(auth.uid(), category_id)
       )
@@ -9,6 +9,8 @@ WITH CHECK (auth.has_statbus_role(auth.uid(), 'restricted_user'::public.statbus_
        AND auth.has_activity_category_access(auth.uid(), category_id)
       );
 
---CREATE POLICY "premium and admin view access" ON premium_records FOR ALL TO authenticated USING (has_one_of_statbus_roles(auth.uid(), array['super_user', 'restricted_user']::public.statbus_role_type[]));
+CREATE POLICY "regular_and_super_user_activity_access" ON public.activity FOR ALL TO authenticated
+USING (auth.has_one_of_statbus_roles(auth.uid(), array['super_user', 'regular_user']::public.statbus_role_type[]))
+WITH CHECK (auth.has_one_of_statbus_roles(auth.uid(), array['super_user', 'regular_user']::public.statbus_role_type[]));
 
 END;
