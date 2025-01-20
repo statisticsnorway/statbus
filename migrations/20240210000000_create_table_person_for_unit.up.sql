ec800c50687554ed8ab20684315efe68342b1978
Jørgen H. Fjeld <jorgen@veridit.no>
2025-01-20 13:15:32 +0100

    db:Update naming of person_type to person_role
    To match table name.

--- migrations/20240210000000_create_table_person_for_unit.up.sql
+++ migrations/20240210000000_create_table_person_for_unit.up.sql
@@ -4,7 +4,7 @@ BEGIN;
 CREATE TABLE public.person_for_unit (
     id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     person_id integer NOT NULL REFERENCES public.person(id) ON DELETE CASCADE,
-    person_type_id integer REFERENCES public.person_role(id),
+    person_role_id integer REFERENCES public.person_role(id),
     establishment_id integer check (admin.establishment_id_exists(establishment_id)),
     legal_unit_id integer check (admin.legal_unit_id_exists(legal_unit_id)),
     CONSTRAINT "One and only one of establishment_id legal_unit_id  must be set"
@@ -15,6 +15,6 @@ CREATE TABLE public.person_for_unit (
 CREATE INDEX ix_person_for_unit_legal_unit_id ON public.person_for_unit USING btree (legal_unit_id);
 CREATE INDEX ix_person_for_unit_establishment_id ON public.person_for_unit USING btree (establishment_id);
 CREATE INDEX ix_person_for_unit_person_id ON public.person_for_unit USING btree (person_id);
-CREATE UNIQUE INDEX ix_person_for_unit_person_type_id_establishment_id_legal_unit_id_ ON public.person_for_unit USING btree (person_type_id, establishment_id, legal_unit_id, person_id);
+CREATE UNIQUE INDEX ix_person_for_unit_person_role_id_establishment_id_legal_unit_id_ ON public.person_for_unit USING btree (person_role_id, establishment_id, legal_unit_id, person_id);
 
 END;
