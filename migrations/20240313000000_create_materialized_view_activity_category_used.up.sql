874753727294802b6011f7b974f12fc932778944
Jørgen H. Fjeld <jorgen@veridit.no>
2025-01-23 16:09:54 +0100

    db: Include secondary in activity_category_used

--- migrations/20240313000000_create_materialized_view_activity_category_used.up.sql
+++ migrations/20240313000000_create_materialized_view_activity_category_used.up.sql
@@ -14,7 +14,21 @@ JOIN public.activity_category_standard AS acs ON ac.standard_id = acs.id
 LEFT JOIN public.activity_category AS acp ON ac.parent_id = acp.id
 WHERE acs.id = (SELECT activity_category_standard_id FROM public.settings)
   AND ac.active
-  AND ac.path OPERATOR(public.@>) (SELECT array_agg(DISTINCT primary_activity_category_path) FROM public.statistical_unit WHERE primary_activity_category_path IS NOT NULL)
+  AND (
+         ac.path OPERATOR(public.@>)
+         (
+         SELECT array_agg(DISTINCT primary_activity_category_path)
+         FROM public.statistical_unit
+         WHERE primary_activity_category_path IS NOT NULL
+         )
+         OR
+         ac.path OPERATOR(public.@>)
+         (
+         SELECT array_agg(DISTINCT secondary_activity_category_path)
+         FROM public.statistical_unit
+         WHERE secondary_activity_category_path IS NOT NULL
+         )
+      )
 ORDER BY path;
 
 CREATE UNIQUE INDEX "activity_category_used_key"
