3606a96b23d96846b7abe4c3e79e01ab59aac10b
Jørgen H. Fjeld <jorgen@veridit.no>
2025-01-29 17:55:56 +0100

    db: Improve region _itude error messages
    And test for it?

--- migrations/20240213000000_create_region_upload_and_related_functions.down.sql
+++ migrations/20240213000000_create_region_upload_and_related_functions.down.sql
@@ -3,4 +3,7 @@ BEGIN;
 DROP VIEW public.region_upload;
 DROP FUNCTION admin.region_upload_upsert();
 
+DROP FUNCTION admin.type_numeric_field(jsonb, text, int, int, jsonb);
+DROP FUNCTION admin.type_ltree_field(jsonb, text, jsonb);
+
 END;
