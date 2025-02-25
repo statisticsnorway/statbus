392006372127292dc1f777b1ff55b0a9b3fe605a
Jørgen H. Fjeld <jorgen@veridit.no>
2025-02-25 13:13:57 +0100

    worker: Adapt to running worker inside tests
    * Move logic to plpgsql
    * Make each test run independently with setup.sql
    * Adjust tests to run the worker notifications at specific times.
    * Remove statistical_unit_refresh_now as it now solved by the worker

--- migrations/20240314000000_create_materialized_view_public_region_used.up.sql
+++ migrations/20240314000000_create_materialized_view_public_region_used.up.sql
@@ -1,6 +1,7 @@
 BEGIN;
 
-CREATE MATERIALIZED VIEW public.region_used AS
+-- Create view that defines the regions in use
+CREATE VIEW public.region_used_def AS
 SELECT r.id
      , r.path
      , r.level
@@ -11,7 +12,27 @@ FROM public.region AS r
 WHERE r.path OPERATOR(public.@>) (SELECT array_agg(DISTINCT physical_region_path) FROM public.statistical_unit WHERE physical_region_path IS NOT NULL)
 ORDER BY public.nlevel(path), path;
 
+-- Create unlogged table from the view definition
+CREATE UNLOGGED TABLE public.region_used AS
+SELECT * FROM public.region_used_def;
+
 CREATE UNIQUE INDEX "region_used_key"
     ON public.region_used (path);
+
+-- Create function to populate the unlogged table
+CREATE FUNCTION public.region_used_derive() 
+    RETURNS void 
+    LANGUAGE plpgsql 
+    SECURITY DEFINER AS $region_used_derive$
+BEGIN
+    RAISE DEBUG 'Running region_used_derive()';
+    TRUNCATE TABLE public.region_used;
+    INSERT INTO public.region_used 
+    SELECT * FROM public.region_used_def;
+END;
+$region_used_derive$;
+
+-- Initial population
+SELECT public.region_used_derive();
 
 END;
--- migrations/20240324000000_create_materialized_view_public_statistical_history.down.sql
+++ migrations/20240324000000_create_materialized_view_public_statistical_history.down.sql
@@ -1,5 +1,6 @@
 BEGIN;
 
-DROP MATERIALIZED VIEW public.statistical_history;
+DROP FUNCTION IF EXISTS public.statistical_history_derive(date, date);
+DROP TABLE IF EXISTS public.statistical_history;
 
 END;
