67063da326021d7beab3bf356ce223cd5f4ad898
hhssb <hhz@ssb.no>
2025-03-05 14:35:14 +0100

    test: Update test data

--- test/sql/04_legal_units_with_statistics.sql
+++ test/sql/04_legal_units_with_statistics.sql
@@ -7,11 +7,9 @@ BEGIN;
 -- A Super User configures statbus.
 CALL test.set_user_from_email('test.super@statbus.org');
 
-\echo "This is the use case for Morocco, so sample data from them"
-
 \echo "User selected the Activity Category Standard"
 INSERT INTO settings(activity_category_standard_id, only_one_setting)
-SELECT id, true FROM activity_category_standard WHERE code = 'isic_v4'
+SELECT id, true FROM activity_category_standard WHERE code = 'nace_v2.1'
 ON CONFLICT (only_one_setting)
 DO UPDATE SET
     activity_category_standard_id = EXCLUDED.activity_category_standard_id;
@@ -21,8 +19,8 @@ SELECT acs.code
   JOIN activity_category_standard AS acs
     ON s.activity_category_standard_id = acs.id;
 
-\echo "User uploads the activity categories"
-\copy public.activity_category_available_custom(path,name,description) FROM 'test/data/04_morocco-activity_category.csv' WITH (FORMAT csv, DELIMITER ',', QUOTE '"', HEADER true);
+\echo "User uploads the sample activity categories"
+\copy public.activity_category_available_custom(path,name,description) FROM 'samples/norway/activity_category/activity_category_norway.csv' WITH (FORMAT csv, DELIMITER ',', QUOTE '"', HEADER true);
 SELECT count(*) FROM public.activity_category_available;
 
 SELECT standard_code
@@ -34,16 +32,16 @@ SELECT standard_code
 FROM public.activity_category_available
 ORDER BY standard_code, path;
 
-\echo "User uploads the regions"
-\copy public.region_upload(path, name) FROM 'test/data/04_morocco-regions.csv' WITH (FORMAT csv, DELIMITER ',', QUOTE '"', HEADER true);
+\echo "User uploads the sample regions"
+\copy public.region_upload(path, name) FROM 'samples/norway/regions/norway-regions-2024.csv' WITH (FORMAT csv, DELIMITER ',', QUOTE '"', HEADER true);
 SELECT count(*) FROM public.region;
 
 \echo "User uploads the sample legal forms"
-\copy public.legal_form_custom_only(code,name) FROM 'test/data/04_morocco-legal_form.csv' WITH (FORMAT csv, DELIMITER ',', QUOTE '"', HEADER true);
+\copy public.legal_form_custom_only(code,name) FROM 'samples/norway/legal_form/legal_form_norway.csv' WITH (FORMAT csv, DELIMITER ',', QUOTE '"', HEADER true);
 SELECT count(*) FROM public.legal_form_available;
 
 \echo "User uploads the sample sectors"
-\copy public.sector_custom_only(path,name,description) FROM 'test/data/04_morocco-sector.csv' WITH (FORMAT csv, DELIMITER ',', QUOTE '"', HEADER true);
+\copy public.sector_custom_only(path,name,description) FROM 'samples/norway/sector/sector_norway.csv' WITH (FORMAT csv, DELIMITER ',', QUOTE '"', HEADER true);
 SELECT count(*) FROM public.sector_available;
 
 SELECT
@@ -51,7 +49,7 @@ SELECT
     (SELECT COUNT(DISTINCT id) AS distinct_unit_count FROM public.legal_unit) AS legal_unit_count,
     (SELECT COUNT(DISTINCT id) AS distinct_unit_count FROM public.enterprise) AS enterprise_count;
 \echo "User uploads legal_units with statistics"
-\copy public.import_legal_unit_current(stat_ident,legal_form_code,name,birth_date,physical_postplace,physical_address_part1,primary_activity_category_code,physical_country_iso_2,physical_region_code,sector_code,employees,turnover) FROM 'test/data/04_morocco-legal-units-with-stats-small.csv' WITH (FORMAT csv, DELIMITER ',', QUOTE '"', HEADER true);
+\copy public.import_legal_unit_current(tax_ident,name,birth_date,physical_address_part1,physical_postcode,physical_postplace,physical_region_code,physical_country_iso_2,postal_address_part1,postal_postplace,postal_postcode,postal_region_code,postal_country_iso_2,primary_activity_category_code,sector_code,legal_form_code,employees,turnover) FROM 'test/data/04_legal-units-with-stats.csv' WITH (FORMAT csv, DELIMITER ',', QUOTE '"', HEADER true);
 SELECT
     (SELECT COUNT(DISTINCT id) AS distinct_unit_count FROM public.establishment) AS establishment_count,
     (SELECT COUNT(DISTINCT id) AS distinct_unit_count FROM public.legal_unit) AS legal_unit_count,
