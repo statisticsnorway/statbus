4705eeed7262758663c1caa2c94508548a84acb7
hhssb <hhz@ssb.no>
2025-03-12 11:35:36 +0100

    [ui] Add statistical variables to details page for all units

---
+++ app/src/app/enterprises/[id]/statistical-variables/statistical-variables-form.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { useBaseData } from "@/app/BaseDataClient";
+import { FormField } from "@/components/form/form-field";
+
+export default function StatisticalVariablesForm({
+  enterpriseStats,
+}: {
+  readonly enterpriseStats: StatisticalUnitStats;
+}) {
+  const { statDefinitions } = useBaseData();
+
+  return (
+    <form className="space-y-4">
+      {statDefinitions.map((statDefinition) => {
+        const value =
+          enterpriseStats?.stats_summary?.[statDefinition.code]?.sum;
+        return (
+          <FormField
+            key={statDefinition.code}
+            label={statDefinition.name ?? statDefinition.code!}
+            name={`stats.${statDefinition.code}`}
+            value={value}
+            response={null}
+            readonly
+          />
+        );
+      })}
+    </form>
+  );
+}
---
+++ app/src/app/establishments/[id]/statistical-variables/statistical-variables-form.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { useBaseData } from "@/app/BaseDataClient";
+import { FormField } from "@/components/form/form-field";
+
+export default function StatisticalVariablesForm({
+  establishmentStats,
+}: {
+  readonly establishmentStats: StatisticalUnitStats;
+}) {
+  const { statDefinitions } = useBaseData();
+
+  return (
+    <form className="space-y-4">
+      {statDefinitions.map((statDefinition) => {
+        const value = establishmentStats.stats?.[statDefinition.code!];
+        return (
+          <FormField
+            key={statDefinition.code}
+            label={statDefinition.name ?? statDefinition.code!}
+            name={`stats.${statDefinition.code}`}
+            value={value}
+            response={null}
+            readonly
+          />
+        );
+      })}
+    </form>
+  );
+}
