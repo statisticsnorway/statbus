e26a78ef30ac12fbd32c3821e9cff2b2878afcd7
Jørgen H. Fjeld <jorgen@veridit.no>
2025-03-13 13:06:30 +0100

    test: Document why the datestyle must be set for all tests

--- test/setup.sql
+++ test/setup.sql
@@ -1,8 +1,5 @@
-
-SELECT * FROM public.statbus_user_create('test.super@statbus.org', 'super_user'::statbus_role_type, 'Super#123!');
-SELECT * FROM public.statbus_user_create('test.regular@statbus.org', 'regular_user'::statbus_role_type, 'Regular#123!');
-SELECT * FROM public.statbus_user_create('test.restricted@statbus.org', 'restricted_user'::statbus_role_type, 'Restricted#123!');
-
+-- While the datestyle is set for the database, the pg_regress tool sets the MDY format
+-- to ensure consistent date formatting, so we must manually override this
 SET datestyle TO 'ISO, DMY';
 
 \if :{?DEBUG}
@@ -10,3 +7,8 @@ SET client_min_messages TO debug1;
 \else
 SET client_min_messages TO NOTICE;
 \endif
+
+\echo Add users for testing purposes
+SELECT * FROM public.statbus_user_create('test.super@statbus.org', 'super_user'::statbus_role_type, 'Super#123!');
+SELECT * FROM public.statbus_user_create('test.regular@statbus.org', 'regular_user'::statbus_role_type, 'Regular#123!');
+SELECT * FROM public.statbus_user_create('test.restricted@statbus.org', 'restricted_user'::statbus_role_type, 'Restricted#123!');
