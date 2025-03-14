d404e1cd327766162de5c2d522761d1ab0990042
Jørgen H. Fjeld <jorgen@veridit.no>
2025-03-14 09:44:23 +0100

    import: Adapt brreg imports to import jobs
    And split into different import definitions by years 2024 and 2025,
    since the columns of brreg data have changed.

---
+++ samples/norway/brreg/create-import-definition-underenhet-2024.sql
@@ -0,0 +1,126 @@
+-- Pretend the user has clicked and created import definition for BRREG Underenhet (establishment for establishment)
+
+WITH it AS (
+    SELECT * FROM public.import_target
+    WHERE schema_name = 'public'
+      AND table_name = 'import_establishment_era_for_legal_unit'
+), def AS (
+    INSERT INTO public.import_definition
+        ( target_id
+        , slug
+        , name
+        , note
+        )
+    SELECT it.id
+        , 'brreg_underenhet_2024'
+        , 'Import of BRREG Underenhet using 2024 columns'
+        , 'Easy upload of the CSV file found at brreg.'
+    FROM it
+    RETURNING *
+), raw_mapping(source_column_name, source_expression, target_column_name) AS (
+VALUES
+      (NULL, 'default'::public.import_source_expression, 'valid_from')
+    , (NULL, 'default'::public.import_source_expression, 'valid_to')
+    , ('organisasjonsnummer', NULL, 'tax_ident')
+    , ('navn', NULL, 'name')
+    , ('organisasjonsform.kode', NULL, NULL)
+    , ('organisasjonsform.beskrivelse', NULL, NULL)
+    , ('naeringskode1.kode', NULL, 'primary_activity_category_code')
+    , ('naeringskode1.beskrivelse', NULL, NULL)
+    , ('naeringskode2.kode', NULL, 'secondary_activity_category_code')
+    , ('naeringskode2.beskrivelse', NULL, NULL)
+    , ('naeringskode3.kode', NULL, NULL)
+    , ('naeringskode3.beskrivelse', NULL, NULL)
+    , ('hjelpeenhetskode.kode', NULL, NULL)
+    , ('hjelpeenhetskode.beskrivelse', NULL, NULL)
+    , ('harRegistrertAntallAnsatte', NULL, NULL)
+    , ('antallAnsatte', NULL, NULL)
+    , ('hjemmeside', NULL, NULL)
+    , ('postadresse.adresse', NULL, 'postal_address_part1')
+    , ('postadresse.poststed', NULL, 'postal_postplace')
+    , ('postadresse.postnummer', NULL, 'postal_postcode')
+    , ('postadresse.kommune', NULL, NULL)
+    , ('postadresse.kommunenummer', NULL, 'postal_region_code')
+    , ('postadresse.land', NULL, NULL)
+    , ('postadresse.landkode', NULL, 'postal_country_iso_2')
+    , ('beliggenhetsadresse.adresse', NULL, 'physical_address_part1')
+    , ('beliggenhetsadresse.poststed', NULL, 'physical_postplace')
+    , ('beliggenhetsadresse.postnummer', NULL, 'physical_postcode')
+    , ('beliggenhetsadresse.kommune', NULL, NULL)
+    , ('beliggenhetsadresse.kommunenummer', NULL, 'physical_region_code')
+    , ('beliggenhetsadresse.land', NULL, NULL)
+    , ('beliggenhetsadresse.landkode', NULL, 'physical_country_iso_2')
+    , ('registreringsdatoIEnhetsregisteret', NULL, NULL)
+    , ('frivilligMvaRegistrertBeskrivelser', NULL, NULL)
+    , ('registrertIMvaregisteret', NULL, NULL)
+    , ('oppstartsdato', NULL, 'birth_date')
+    , ('datoEierskifte', NULL, NULL)
+    , ('overordnetEnhet', NULL, 'legal_unit_tax_ident')
+    , ('nedleggelsesdato', NULL, 'death_date')
+), name_mapping AS (
+    SELECT
+        ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) as priority,
+        source_column_name,
+        source_expression,
+        target_column_name
+    FROM raw_mapping
+), inserted_source_column AS (
+    INSERT INTO public.import_source_column (definition_id,column_name, priority)
+    SELECT def.id, name_mapping.source_column_name, name_mapping.priority
+    FROM def, name_mapping
+    WHERE source_column_name IS NOT NULL
+    ORDER BY priority
+    RETURNING *
+), mapping AS (
+    SELECT def.id
+          , isc.id
+          , nm.source_expression
+          , itc.id
+    FROM def
+        , name_mapping AS nm
+        LEFT JOIN inserted_source_column AS isc
+          ON isc.column_name = nm.source_column_name
+        LEFT JOIN public.import_target_column AS itc
+        ON itc.column_name = nm.target_column_name
+        WHERE itc.target_id IS NULL OR itc.target_id = def.target_id
+), mapped AS (
+  INSERT INTO public.import_mapping
+      ( definition_id
+      , source_column_id
+      , source_expression
+      , target_column_id
+      )
+      SELECT * FROM mapping
+  RETURNING *
+)
+--SELECT * FROM mapped;
+SELECT d.slug as definition_slug,
+        sc.column_name as source_column,
+        m.source_value,
+        m.source_expression,
+        sc.priority AS source_column_priority,
+        tc.column_name AS target_column
+FROM mapped m
+LEFT JOIN def d ON d.id = m.definition_id
+LEFT JOIN inserted_source_column sc ON sc.id = m.source_column_id
+LEFT JOIN public.import_target_column tc ON tc.id = m.target_column_id
+ORDER BY source_column_priority, target_column;
+
+SELECT d.slug,
+        d.name,
+        t.table_name as target_table,
+        d.note,
+        ds.code as data_source,
+        d.time_context_ident,
+        d.draft,
+        d.valid,
+        d.validation_error
+FROM public.import_definition d
+JOIN public.import_target t ON t.id = d.target_id
+LEFT JOIN public.data_source ds ON ds.id = d.data_source_id
+WHERE d.slug = 'brreg_underenhet_2024';
+
+UPDATE public.import_definition
+SET draft = false
+WHERE draft
+  AND slug = 'brreg_underenhet_2024';
