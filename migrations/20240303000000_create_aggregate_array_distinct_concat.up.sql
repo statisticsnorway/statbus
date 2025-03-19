61710431f1530939f29a3346742c6ea53ba3830f
Jørgen H. Fjeld <jorgen@veridit.no>
2025-03-19 14:08:29 +0100

    Improve speed of analysis functions
    By letting PostgreSQL know that they can be optimised.

--- migrations/20240303000000_create_aggregate_array_distinct_concat.up.sql
+++ migrations/20240303000000_create_aggregate_array_distinct_concat.up.sql
@@ -1,14 +1,14 @@
 BEGIN;
 
 -- Final function to remove duplicates from concatenated arrays
-CREATE FUNCTION public.array_distinct_concat_final(anycompatiblearray)
-RETURNS anycompatiblearray LANGUAGE sql AS $$
+CREATE OR REPLACE FUNCTION public.array_distinct_concat_final(anycompatiblearray)
+RETURNS anycompatiblearray LANGUAGE sql IMMUTABLE PARALLEL SAFE AS $$
 SELECT array_agg(DISTINCT elem)
   FROM unnest($1) as elem;
 $$;
 
 -- Aggregate function using array_cat for concatenation and public.array_distinct_concat_final to remove duplicates
-CREATE AGGREGATE public.array_distinct_concat(anycompatiblearray) (
+CREATE OR REPLACE AGGREGATE public.array_distinct_concat(anycompatiblearray) (
   SFUNC = pg_catalog.array_cat,
   STYPE = anycompatiblearray,
   FINALFUNC = public.array_distinct_concat_final,
