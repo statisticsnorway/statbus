80dfb9b09e9cae7de44b026762b8e4c8f4eb2ea4
Jørgen H. Fjeld <jorgen@veridit.no>
2025-04-06 19:51:17 +0100

    ui: Adjust to new attribute names

--- app/src/app/search/components/statistical-unit-table-row.tsx
+++ app/src/app/search/components/statistical-unit-table-row.tsx
@@ -312,22 +312,22 @@ export const StatisticalUnitTableRow = ({
                 className={getCellClassName(column)}
               >
                 <div className="flex flex-col space-y-0.5 leading-tight whitespace-nowrap">
-                  {unit.enterprise_count != null &&
-                    unit.enterprise_count > 0 && (
+                  {unit.included_enterprise_count != null &&
+                    unit.included_enterprise_count > 0 && (
                       <small className="text-gray-700">
-                        Enterprises: {unit.enterprise_count}
+                        Enterprises: {unit.included_enterprise_count}
                       </small>
                     )}
-                  {unit.legal_unit_count != null &&
-                    unit.legal_unit_count > 0 && (
+                  {unit.included_legal_unit_count != null &&
+                    unit.included_legal_unit_count > 0 && (
                       <small className="text-gray-700">
-                        Legal Units: {unit.legal_unit_count}
+                        Legal Units: {unit.included_legal_unit_count}
                       </small>
                     )}
-                  {unit.establishment_count != null &&
-                    unit.establishment_count > 0 && (
+                  {unit.included_establishment_count != null &&
+                    unit.included_establishment_count > 0 && (
                       <small className="text-gray-700">
-                        Establishments: {unit.establishment_count}
+                        Establishments: {unit.included_establishment_count}
                       </small>
                     )}
                 </div>
