a720afb2d61153a79f4f2156d36cd1f8c60f644c
Erik Søberg <136451001+rsbssbno@users.noreply.github.com>
2025-04-08 14:42:36 +0200

    Create ug.sql

---
+++ custom/ug.sql
@@ -0,0 +1,28 @@
+
+
+Select 'Runs Uganda Kampala'  "StatbusStatus", now() "AT";
+
+
+delete from external_ident_type where id >2;
+
+
+INSERT INTO external_ident_type (code, name, priority)
+VALUES 
+('coin_ident', 'CoinId', 3);
+
+
+delete from data_source_custom ;
+
+INSERT INTO data_source_custom (code, name)
+VALUES 
+    ('tst', 'Test');
+
+
+delete from stat_definition where id >2;
+INSERT INTO stat_definition (code, type,frequency, name, priority)
+VALUES 
+('female', 'int', 'yearly', 'Female', 5),
+('male', 'int', 'yearly', 'Male', 6);
+
+
+Select 'Done Uganda'  "StatbusStatus", now() "AT";
