ab7f241564cad89f9e7b81bf5180ba4b81ba91f0
Erik Søberg <136451001+rsbssbno@users.noreply.github.com>
2025-04-08 14:44:18 +0200

    Add files via upload
    custom contries

---
+++ custom/ma.sql
@@ -0,0 +1,78 @@
+
+
+Select 'Runs MA'  "StatbusStatus", now() "AT";
+
+
+delete from external_ident_type where id >2;
+
+
+INSERT INTO external_ident_type (code, name, priority)
+VALUES 
+('cnss_ident', 'Cnss', 3),
+('hcp_ident', 'Hcp', 4),
+('ice_ident', 'Ice', 5);
+
+
+delete from data_source_custom ;
+
+INSERT INTO data_source_custom (code, name)
+VALUES 
+    ('Base2014', 'Base2014'),
+    ('CNSS2014', 'CNSS2014'),
+    ('CNSS2015', 'CNSS2015'),
+    ('CNSS2016', 'CNSS2016'),
+    ('CNSS2017', 'CNSS2017'),
+    ('CNSS2018', 'CNSS2018'),
+    ('CNSS2019', 'CNSS2019'),
+    ('CNSS2020', 'CNSS2020'),
+    ('CNSS2021', 'CNSS2021'),
+    ('CNSS2022', 'CNSS2022');
+
+
+update data_source
+set active = TRUE;
+
+
+
+delete from stat_definition where id >2;
+INSERT INTO stat_definition (code, type,frequency, name, priority)
+VALUES 
+('share_capital', 'float', 'yearly', 'Share_capital', 3);
+
+update activity_category_standard
+set code_pattern = 'digits'
+where code = 'nace_v2.1';
+
+
+
+delete from unit_size where id >4 and custom is TRUE;
+
+INSERT INTO unit_size (code, name, active, custom)
+VALUES 
+(1, 'Tiny', TRUE, TRUE),
+(2, 'PetitSmall', TRUE, TRUE),
+(3, 'Moyen', TRUE, TRUE),
+(4, 'Grande', TRUE, TRUE);
+
+
+
+update status
+set active = FALSE where custom=FALSE;
+
+
+delete from status where id >2 and custom=TRUE;
+INSERT INTO status (code, name, assigned_by_default, include_unit_in_reports,priority, active, custom)
+VALUES 
+('act', 'Active', TRUE, TRUE,3,TRUE,TRUE),
+('inact', 'Inactive', FALSE, TRUE,4,TRUE,TRUE),
+('veil', 'En veilleuse', FALSE, TRUE,3,TRUE,TRUE),
+('cess', 'Cessation', FALSE, TRUE,4,TRUE,TRUE),
+('fus-abs', 'Fusion-Absrption', FALSE, TRUE,5,TRUE,TRUE),
+('rad', 'Radiee', FALSE, FALSE,6,TRUE,TRUE);
+
+
+
+
+
+
+Select 'Done MA'  "StatbusStatus", now() "AT";
