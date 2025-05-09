c8232e310f7d78693dab33cec7ffbc31872ee9f0
Erik Soberg <erik.soberg@ssb.no>
2025-05-09 14:06:35 +0200

    Update ug.sql

--- custom/ug.sql
+++ custom/ug.sql
@@ -7,22 +7,24 @@ delete from external_ident_type where id >2;
 
 
 INSERT INTO external_ident_type (code, name, priority)
-VALUES 
+VALUES
 ('coin_ident', 'CoinId', 3);
 
 
-delete from data_source_custom ;
+--custom datasources can be listed here:
+--delete from data_source_custom ;
+--INSERT INTO data_source_custom (code, name)
+--VALUES
+  --  ('tst', 'Test'),
+ --   ('cen25', 'Census2025');
 
-INSERT INTO data_source_custom (code, name)
-VALUES 
-    ('tst', 'Test');
 
 
 delete from stat_definition where id >2;
 INSERT INTO stat_definition (code, type,frequency, name, priority)
-VALUES 
-('female', 'int', 'yearly', 'Female', 5),
-('male', 'int', 'yearly', 'Male', 6);
+VALUES
+('female', 'int', 'yearly', 'Female', 3),
+('male', 'int', 'yearly', 'Male', 4);
 
 
 Select 'Done Uganda'  "StatbusStatus", now() "AT";
