3c7ac70509d900046431350e1d14eda6a85a3750
Jørgen H. Fjeld <jorgen@veridit.no>
2025-06-16 16:37:04 +0200

    app:Limit uploads for getting started to csv files

--- app/src/app/getting-started/upload-csv-form.tsx
+++ app/src/app/getting-started/upload-csv-form.tsx
@@ -77,6 +77,7 @@ export const UploadCSVForm = ({
         id={filename}
         type="file"
         name={filename}
+        accept=".csv"
         className="mb-4"
       />
       <UploadFormButtons error={state.error} nextPage={nextPage} />
